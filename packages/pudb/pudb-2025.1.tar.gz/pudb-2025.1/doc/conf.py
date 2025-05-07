05b0ab65c95db4dfb06c6b0a968eadf559ec557e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:03:43 +0000

    Add package pudb/2025.1/pudb-2025.1.tar.gz

---
+++ packages/pudb/pudb-2025.1.tar.gz/pudb-2025.1/doc/conf.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from urllib.request import urlopen
+
+
+_conf_url = \
+        "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
+with urlopen(_conf_url) as _inf:
+    exec(compile(_inf.read(), _conf_url, "exec"), globals())
+
+sys.path.insert(0, os.path.abspath(".."))
+
+copyright = "2017-21, Andreas Kloeckner and contributors"
+author = "Andreas Kloeckner and contributors"
+
+ver_dic = {}
+with open("../pudb/__init__.py") as ver_file:
+    ver_src = ver_file.read()
+exec(compile(ver_src, "../pudb/__init__.py", "exec"), ver_dic)
+version = ver_dic["VERSION"]
+
+# The full version, including alpha/beta/rc tags.
+release = version
+
+intersphinx_mapping = {
+        "python": ("https://docs.python.org/3", None),
+        "urwid": ("https://urwid.org/", None),
+        }
