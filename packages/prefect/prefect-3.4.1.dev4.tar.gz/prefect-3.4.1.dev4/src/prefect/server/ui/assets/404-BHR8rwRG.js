1b244ec5879a375c74c7a459781d8da259766f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:05:27 +0000

    Add package prefect/3.4.1.dev4/prefect-3.4.1.dev4.tar.gz

---
+++ packages/prefect/prefect-3.4.1.dev4.tar.gz/prefect-3.4.1.dev4/src/prefect/server/ui/assets/404-BHR8rwRG.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,m as t,dv as r}from"./index-BTKE0aAH.js";import{u as s}from"./usePageTitle-C4XW_JS5.js";const _=e({__name:"404",setup(n){return s("Page Not Found"),(c,m)=>(a(),o(t(r)))}});export{_ as default};
+//# sourceMappingURL=404-BHR8rwRG.js.map
---
+++ packages/prefect/prefect-3.4.1.dev4.tar.gz/prefect-3.4.1.dev4/src/prefect/server/ui/assets/usePageTitle-C4XW_JS5.js
@@ -0,0 +1,2 @@
+import{h as a,m as c,bw as n}from"./index-BTKE0aAH.js";function i(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>c(e)).filter(e=>e!==null).join(" • "));n(()=>document.title=o.value)}export{i as u};
+//# sourceMappingURL=usePageTitle-C4XW_JS5.js.map
