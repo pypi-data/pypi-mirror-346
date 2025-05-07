3c70e098f5f11eada61d6feb466f84d757c8f353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:21:12 +0000

    Add package gcore/0.1.0a2/gcore-0.1.0a2-py3-none-any.whl

---
+++ packages/gcore/gcore-0.1.0a2-py3-none-any.whl/gcore/resources/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .cloud import (
+    CloudResource,
+    AsyncCloudResource,
+    CloudResourceWithRawResponse,
+    AsyncCloudResourceWithRawResponse,
+    CloudResourceWithStreamingResponse,
+    AsyncCloudResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "CloudResource",
+    "AsyncCloudResource",
+    "CloudResourceWithRawResponse",
+    "AsyncCloudResourceWithRawResponse",
+    "CloudResourceWithStreamingResponse",
+    "AsyncCloudResourceWithStreamingResponse",
+]
