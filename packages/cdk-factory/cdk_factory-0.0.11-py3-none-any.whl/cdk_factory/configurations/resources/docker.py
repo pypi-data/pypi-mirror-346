cbd41f51545de6c7ad5d3f559655be1ce4096fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:03:42 +0000

    Add package cdk-factory/0.0.11/cdk_factory-0.0.11-py3-none-any.whl

---
+++ packages/cdk-factory/cdk_factory-0.0.11-py3-none-any.whl/cdk_factory/configurations/resources/docker.py
@@ -0,0 +1,64 @@
+"""
+Geek Cafe, LLC
+Maintainers: Eric Wilson
+MIT License.  See Project Root for the license information.
+"""
+
+
+class DockerConfig:
+    """
+    DockerConfig Container Information
+
+    """
+
+    def __init__(self, config: dict) -> None:
+        self.__config = config
+
+    @property
+    def name(self) -> str:
+        """Name (Repository Name)"""
+
+        if self.__config and isinstance(self.__config, dict):
+            value = self.__config.get("name")
+            if isinstance(value, str):
+                return value
+
+        return ""
+
+    @property
+    def uri(self) -> str:
+        """uri"""
+        if self.__config and isinstance(self.__config, dict):
+            value = self.__config.get("uri")
+            if isinstance(value, str):
+                return value
+
+        return ""
+
+    @property
+    def arn(self) -> str:
+        """arn"""
+        if self.__config and isinstance(self.__config, dict):
+            value = self.__config.get("arn")
+            if isinstance(value, str):
+                return value
+
+        return ""
+
+    @property
+    def file(self) -> str:
+        """file"""
+        if self.__config and isinstance(self.__config, dict):
+            value = self.__config.get("file")
+            if isinstance(value, str):
+                return value
+
+        return ""
+
+    @property
+    def image(self) -> bool:
+        """image"""
+        if self.__config and isinstance(self.__config, dict):
+            return str(self.__config.get("image")).lower() == "true"
+
+        return False
