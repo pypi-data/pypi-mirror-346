0234c795961dd9f02d0736e1b7b0a8d2eb463979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 17:05:20 +0000

    Add package uagents-adapter/0.2.0/uagents_adapter-0.2.0.tar.gz

---
+++ packages/uagents-adapter/uagents_adapter-0.2.0.tar.gz/uagents_adapter-0.2.0/src/uagents_adapter/langchain/__init__.py
@@ -0,0 +1,19 @@
+"""LangChain integration for registering agents on Agentverse."""
+
+from importlib import metadata
+
+from .tools import LangchainRegisterTool, QueryMessage
+
+try:
+    __version__ = metadata.version(__package__.split(".")[0])
+except metadata.PackageNotFoundError:
+    # Case where package metadata is not available.
+    __version__ = ""
+del metadata  # optional, avoids polluting the results of dir(__package__)
+
+
+__all__ = [
+    "LangchainRegisterTool",
+    "QueryMessage",
+    "__version__",
+]
