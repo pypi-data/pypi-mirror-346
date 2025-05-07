0ebf937ab312ecc89d1f0f21c693784167fd4bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:21:43 +0000

    Add package JsonDot/0.7.1/jsondot-0.7.1.tar.gz

---
+++ packages/JsonDot/jsondot-0.7.1.tar.gz/jsondot-0.7.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='JsonDot',
+    version='0.7.1',
+    description='Load JSON files and use the data with dot notation then dump to the file',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Mits-Soft/JsonDot.git',
+    author='Marco Antonio Calviño Coira',
+    author_email='mits.soft.main@gmail.com',
+    license='MIT',
+    packages=['jsondot'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',        
+        'License :: OSI Approved :: MIT License',       
+        'Programming Language :: Python :: 3.12'
+    ],
+)
