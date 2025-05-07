785126578b4f82cdebf38e6a7fd4f8d74dd81202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 15:04:16 +0000

    Add package caqui/3.0.4/caqui-3.0.4-py3-none-any.whl

---
+++ packages/caqui/caqui-3.0.4-py3-none-any.whl/caqui/easy/__init__.py
@@ -0,0 +1,4 @@
+from caqui.easy.page import AsyncPage
+from caqui.easy.action_chains import ActionChains
+
+__all__ = ["AsyncPage", "ActionChains"]
---
+++ packages/caqui/caqui-3.0.4-py3-none-any.whl/caqui/helper.py
@@ -0,0 +1,22 @@
+import base64
+
+
+def save_picture(session, path, file_name, response):
+    with open(f"{path}/{file_name}-{session}.png", "wb") as f:
+        f.write(base64.b64decode((response)))
+
+
+def get_elements(response):
+    values = response.get("value")
+    return [list(value.values())[0] for value in values]
+
+
+def get_element(response) -> dict:
+    value = response.get("value")
+    # Google Chrome
+    element = value.get("ELEMENT")
+    if element:
+        return element
+
+    # Firefox
+    return list(value.values())[0]
