7bcaab92bf65795da2af96434165a0910e60e14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:05:55 +0000

    Add package fotoobo/2.3.0/fotoobo-2.3.0-py3-none-any.whl

---
+++ packages/fotoobo/fotoobo-2.3.0-py3-none-any.whl/fotoobo/cli/fmg/fmg.py
@@ -0,0 +1,119 @@
+"""
+The FortiManager commands
+"""
+
+import logging
+from pathlib import Path
+
+import typer
+
+from fotoobo.helpers import cli_path
+from fotoobo.helpers.config import config as fotoobo_config
+from fotoobo.inventory import Inventory
+from fotoobo.tools import fmg
+
+from . import get
+
+app = typer.Typer(no_args_is_help=True, rich_markup_mode="rich")
+log = logging.getLogger("fotoobo")
+
+
+@app.callback()
+def callback(context: typer.Context) -> None:
+    """
+    The fmg subcommand callback
+
+    Args:
+        context: The context object of the typer app
+    """
+    cli_path.append(str(context.invoked_subcommand))
+    log.debug("About to execute command: '%s'", context.invoked_subcommand)
+
+
+@app.command(no_args_is_help=True)
+def assign(
+    adoms: str = typer.Argument(
+        ...,
+        help="The ADOMs to assign the global policy/objects to. Use 'fotoobo fmg get adoms' to "
+        "get a list of available ADOMs. Separate multiple ADOMs by comma (no spaces).",
+        metavar="[adoms]",
+        show_default=False,
+    ),
+    policy: str = typer.Argument(
+        ...,
+        help="The global policy to assign",
+        metavar="[policy]",
+        show_default=False,
+    ),
+    host: str = typer.Argument(
+        "fmg",
+        help="The FortiManager to access (must be defined in the inventory).",
+        metavar="[host]",
+    ),
+    smtp_server: str = typer.Option(
+        None,
+        "--smtp",
+        "-s",
+        help="The smtp configuration from the inventory to send potential errors to.",
+        metavar="[server]",
+    ),
+    timeout: int = typer.Option(
+        60,
+        "--timeout",
+        "-t",
+        help="The timeout to wait for the FortiManager task to finish.",
+        metavar="[timeout]",
+    ),
+) -> None:
+    """
+    Assign a global policy to a specified ADOM or to a list of ADOMs.
+    """
+    inventory = Inventory(fotoobo_config.inventory_file)
+    result = fmg.assign(adoms=adoms, policy=policy, host=host, timeout=timeout)
+
+    if smtp_server:
+        if smtp_server in inventory.assets:
+            result.send_messages_as_mail(inventory.assets[smtp_server], "error")
+
+        else:
+            log.warning("SMTP server '%s' not in found in inventory.", smtp_server)
+
+
+@app.command(no_args_is_help=True)
+def post(
+    file: Path = typer.Argument(
+        ..., help="JSON file with payload(s).", show_default=False, metavar="[file]"
+    ),
+    adom: str = typer.Argument(
+        ..., help="The ADOM to issue the set command(s).", metavar="[adom]", show_default=False
+    ),
+    host: str = typer.Argument(
+        "fmg",
+        help="The FortiManager to access (must be defined in the inventory).",
+        metavar="[host]",
+    ),
+    smtp_server: str = typer.Option(
+        None,
+        "--smtp",
+        "-s",
+        help="The smtp configuration from the inventory to send potential errors to.",
+        metavar="[server]",
+    ),
+) -> None:
+    """
+    Post any valid JSON request to the FortiManager.
+
+    Configure the FortiManager with any valid API call(s) given within the JSON file.
+    """
+    inventory = Inventory(fotoobo_config.inventory_file)
+    result = fmg.post(file=file, adom=adom, host=host)
+
+    if smtp_server:
+        if smtp_server in inventory.assets:
+            result.send_messages_as_mail(inventory.assets[smtp_server], "error", command=True)
+
+        else:
+            log.warning("SMTP server '%s' not in found in inventory.", smtp_server)
+
+
+app.add_typer(get.app, name="get", help="FortiManager get commands.")
---
+++ packages/fotoobo/fotoobo-2.3.0-py3-none-any.whl/fotoobo/tools/ems/get.py
@@ -0,0 +1,57 @@
+"""
+FortiClient EMS get module
+"""
+
+import logging
+from typing import Dict, List
+
+from fotoobo.fortinet.forticlientems import FortiClientEMS
+from fotoobo.helpers.config import config
+from fotoobo.helpers.result import Result
+from fotoobo.inventory import Inventory
+
+log = logging.getLogger("fotoobo")
+
+
+def version(host: str) -> Result[str]:
+    """
+    ems get version
+
+    Args:
+        host: Host defined in inventory
+
+    Returns:
+        Version data in a dict with keys: host, version
+    """
+    result = Result[str]()
+    inventory = Inventory(config.inventory_file)
+    ems: FortiClientEMS = inventory.get_item(host, "forticlientems")
+    log.debug("FortiClient EMS get version ...")
+    ems.login()
+    ems_version = ems.get_version()
+    result.push_result(host, f"v{ems_version}")
+    return result
+
+
+def workgroups(host: str, custom: bool = False) -> Result[List[Dict[str, str]]]:
+    """
+    ems get workgroups
+
+    Args:
+        host:   Host defined in inventory
+        custom: If true it only returns custom groups
+
+    Returns:
+        Workgroups data in a list of dict with keys: name, id, total_devices
+    """
+    result = Result[List[Dict[str, str]]]()
+    inventory = Inventory(config.inventory_file)
+    ems: FortiClientEMS = inventory.get_item(host, "forticlientems")
+    log.debug("FortiClient EMS get workgroups ...")
+    ems.login()
+    raw_data = ems.api("get", f"/workgroups/index?custom={custom}").json()["data"]
+
+    for entry in raw_data:
+        result.push_result(entry["name"], entry["total_devices"])
+
+    return result
