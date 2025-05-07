0842e352a81c3e287c487aa44b272f7918673f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:04:30 +0000

    Add package BloodQ/1.4/bloodq-1.4.tar.gz

---
+++ packages/BloodQ/bloodq-1.4.tar.gz/bloodq-1.4/BLooDNiNjA/build/include/BLooDNiNjA/helper/richcomparisons.h
@@ -0,0 +1,22 @@
+//     Copyright 2025, BLOOD, tnmn4219@gmail.com find license text at end of file
+
+#ifndef __BloodSx_HELPER_RICHCOMPARISONS_H__
+#define __BloodSx_HELPER_RICHCOMPARISONS_H__
+
+#include "BLooDNiNjA/helper/comparisons_eq.h"
+#include "BLooDNiNjA/helper/comparisons_ge.h"
+#include "BLooDNiNjA/helper/comparisons_gt.h"
+#include "BLooDNiNjA/helper/comparisons_le.h"
+#include "BLooDNiNjA/helper/comparisons_lt.h"
+#include "BLooDNiNjA/helper/comparisons_ne.h"
+
+#include "BLooDNiNjA/helper/comparisons_dual_eq.h"
+#include "BLooDNiNjA/helper/comparisons_dual_ge.h"
+#include "BLooDNiNjA/helper/comparisons_dual_gt.h"
+#include "BLooDNiNjA/helper/comparisons_dual_le.h"
+#include "BLooDNiNjA/helper/comparisons_dual_lt.h"
+#include "BLooDNiNjA/helper/comparisons_dual_ne.h"
+
+#endif
+
+
---
+++ packages/BloodQ/bloodq-1.4.tar.gz/bloodq-1.4/BLooDNiNjA/tools/commercial/__init__.py
@@ -0,0 +1,8 @@
+#     Copyright 2025, BLOOD, tnmn4219@gmail.com find license text at end of file
+
+
+""" Commercial tools package.
+
+"""
+
+
