f7d2ff2f2716a286052ba65c9c000c5134d500d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:04:32 +0000

    Add package ora3/0.1.1/ora3-0.1.1.tar.gz

---
+++ packages/ora3/ora3-0.1.1.tar.gz/ora3-0.1.1/ora3/__init__.py
@@ -0,0 +1,1 @@
+from .ora3 import accounts
---
+++ packages/ora3/ora3-0.1.1.tar.gz/ora3-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ora3",
+    version="0.1.1",
+    author="ealchemist",
+    author_email="",
+    description="Tools for tiime and library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
