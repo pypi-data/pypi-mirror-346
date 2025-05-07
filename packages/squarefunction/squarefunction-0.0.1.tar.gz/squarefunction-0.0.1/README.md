041c5eb7f7e2e608fec66b25f9f9937b22ab8954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 15:04:16 +0000

    Add package squarefunction/0.0.1/squarefunction-0.0.1.tar.gz

---
+++ packages/squarefunction/squarefunction-0.0.1.tar.gz/squarefunction-0.0.1/README.md
@@ -0,0 +1,1 @@
+this is a program find area calculation
---
+++ packages/squarefunction/squarefunction-0.0.1.tar.gz/squarefunction-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+name="squarefunction",
+version="0.0.1",
+author="Noora",
+author_email="noorajasmine82@gmail.com",
+packages=find_packages()
+)
