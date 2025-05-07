d12614ed104633c63f126836f6c1804560187f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 14:04:21 +0000

    Add package tytodb-pyclient/0.1.1/tytodb_pyclient-0.1.1.tar.gz

---
+++ packages/tytodb-pyclient/tytodb_pyclient-0.1.1.tar.gz/tytodb_pyclient-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tytodb-pyclient",
+    version="0.1.1",
+    author="ttd3v",
+    description="A TytoDB client for python",
+    packages=find_packages(include=["tytodb_client", "tytodb_client.*"]),
+    install_requires=[
+        "blake3",
+        "cffi",
+        "cryptography"
+    ],
+    python_requires=">=3.6",
+)
