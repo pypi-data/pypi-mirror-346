cbc82047777a24477ecc26ca71efc159e4e0e01e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:20:07 +0000

    Add package vassureai/0.1.1/vassureai-0.1.1-py3-none-any.whl

---
+++ packages/vassureai/vassureai-0.1.1-py3-none-any.whl/actions/__init__.py
@@ -0,0 +1,16 @@
+"""
+-----------------------
+Author: Sukumar Kutagulla (Read-only)
+Designation: Test Automation Architect (Read-only)
+Copyright (c) 2025. All Rights Reserved.
+-----------------------
+
+"""
+
+__author__ = "Sukumar Kutagulla"
+__author_title__ = "Test Automation Architect"
+__copyright__ = f"Copyright (c) 2025, {__author__}. All Rights Reserved."
+
+from .custom_actions import TestController
+
+__all__ = ['TestController']
---
+++ packages/vassureai/vassureai-0.1.1-py3-none-any.whl/vassureai/__init__.py
@@ -0,0 +1,8 @@
+ÿþ"""
+-----------------------
+Author: Sukumar Kutagulla (Read-only)
+Designation: Test Automation Architect (Read-only)
+Copyright (c) 2025. All Rights Reserved.
+-----------------------
+
+"""
