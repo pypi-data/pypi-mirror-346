f1431b5872629c80486e2d8c05ec5edd64ced4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:04:17 +0000

    Add package obsws-cli/0.10.2/obsws_cli-0.10.2.tar.gz

---
+++ packages/obsws-cli/obsws_cli-0.10.2.tar.gz/obsws_cli-0.10.2/obsws_cli/studiomode.py
@@ -0,0 +1,46 @@
+"""module containing commands for manipulating studio mode in OBS."""
+
+import typer
+
+from .alias import AliasGroup
+
+app = typer.Typer(cls=AliasGroup)
+
+
+@app.callback()
+def main():
+    """Control studio mode in OBS."""
+
+
+@app.command('enable | on')
+def enable(ctx: typer.Context):
+    """Enable studio mode."""
+    ctx.obj.set_studio_mode_enabled(True)
+
+
+@app.command('disable | off')
+def disable(ctx: typer.Context):
+    """Disable studio mode."""
+    ctx.obj.set_studio_mode_enabled(False)
+
+
+@app.command('toggle | tg')
+def toggle(ctx: typer.Context):
+    """Toggle studio mode."""
+    resp = ctx.obj.get_studio_mode_enabled()
+    if resp.studio_mode_enabled:
+        ctx.obj.set_studio_mode_enabled(False)
+        typer.echo('Studio mode is now disabled.')
+    else:
+        ctx.obj.set_studio_mode_enabled(True)
+        typer.echo('Studio mode is now enabled.')
+
+
+@app.command('status | ss')
+def status(ctx: typer.Context):
+    """Get the status of studio mode."""
+    resp = ctx.obj.get_studio_mode_enabled()
+    if resp.studio_mode_enabled:
+        typer.echo('Studio mode is enabled.')
+    else:
+        typer.echo('Studio mode is disabled.')
---
+++ packages/obsws-cli/obsws_cli-0.10.2.tar.gz/obsws_cli-0.10.2/tests/test_studiomode.py
@@ -0,0 +1,27 @@
+"""Unit tests for the studio mode command in the OBS WebSocket CLI."""
+
+from typer.testing import CliRunner
+
+from obsws_cli.app import app
+
+runner = CliRunner()
+
+
+def test_studio_enable():
+    """Test the studio enable command."""
+    result = runner.invoke(app, ['studiomode', 'enable'])
+    assert result.exit_code == 0
+
+    result = runner.invoke(app, ['studiomode', 'status'])
+    assert result.exit_code == 0
+    assert 'Studio mode is enabled.' in result.stdout
+
+
+def test_studio_disable():
+    """Test the studio disable command."""
+    result = runner.invoke(app, ['studiomode', 'disable'])
+    assert result.exit_code == 0
+
+    result = runner.invoke(app, ['studiomode', 'status'])
+    assert result.exit_code == 0
+    assert 'Studio mode is disabled.' in result.stdout
