adf140f15e8e98056dc52914ebb50e884cb82682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 14:04:11 +0000

    Add package zenif/0.5.2b2/zenif-0.5.2b2.tar.gz

---
+++ packages/zenif/zenif-0.5.2b2.tar.gz/zenif-0.5.2b2/zenif/__init__.py
@@ -0,0 +1,15 @@
+"""
+    zenif
+    ~~~~~
+
+    Zenif is a powerful, highly customizable, and versatile Python module designed to enhance the efficiency and performance of your programs. Whether you're a seasoned developer or just starting out, Zenif offers a suite of tools to streamline your workflow and improve code management. Zenif currently enables developers with a simple yet highly customizable logger with support for multiple streams, a set of utility decorators, like @cache and @rate_limiter, and a handful of tools to create a command-line interface with interactive prompts and argument handling.
+"""
+
+from . import log, decorators, cli, schema
+
+# initialize colorama *once*
+from colorama import init
+init(autoreset=True)
+
+__version__ = "0.4.2"
+__all__ = ["log", "decorators", "cli", "schema"]
---
+++ packages/zenif/zenif-0.5.2b2.tar.gz/zenif-0.5.2b2/zenif/cli/applets/__init__.py
@@ -0,0 +1,7 @@
+from .core import Applet
+from .custom_parser import parse_command_args
+
+__all__ = [
+    "Applet", 
+    "parse_command_args",
+]
