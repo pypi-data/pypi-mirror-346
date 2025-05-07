1074d84cfcd336a38ce5cdd2211c5afc61f5f934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 14:04:06 +0000

    Add package ttk-epay/0.1.2/ttk_epay-0.1.2.tar.gz

---
+++ packages/ttk-epay/ttk_epay-0.1.2.tar.gz/ttk_epay-0.1.2/src/ttk_epay/app.py
@@ -0,0 +1,10 @@
+
+class ttk_epay: 
+    """
+    A class to represent the ttk_epay application.
+    """
+    def __init__(self) -> None:
+        pass
+
+    def say_hello(self) -> str:
+        return "hello world"
