0e30efbd14d1882022c40f26b5eaf57d927f4880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:04:26 +0000

    Add package ob-metaflow-stubs/6.0.3.164/ob_metaflow_stubs-6.0.3.164-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.164-py2.py3-none-any.whl/metaflow-stubs/plugins/aws/batch/__init__.pyi
@@ -0,0 +1,13 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.11.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-05-07T07:24:19.693440                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import batch_client as batch_client
+from . import batch as batch
+from . import batch_decorator as batch_decorator
+
