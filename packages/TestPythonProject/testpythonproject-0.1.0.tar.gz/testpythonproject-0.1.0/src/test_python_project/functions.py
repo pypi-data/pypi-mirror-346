9e4831186c9882c17b72d7db37b13dae0f7000da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 17:05:12 +0000

    Add package TestPythonProject/0.1.0/testpythonproject-0.1.0.tar.gz

---
+++ packages/TestPythonProject/testpythonproject-0.1.0.tar.gz/testpythonproject-0.1.0/src/test_python_project/functions.py
@@ -0,0 +1,35 @@
+# Standard library imports
+import os
+
+# Third party imports
+import opengeode
+
+
+# Local application imports
+
+
+def sum(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def string_upper(string="Hello world"):
+    string_upper = string.upper()
+    print(string_upper, flush=True)
+    return string_upper
+
+
+def load_brep(filename):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    return opengeode.load_brep(path)
