55e3ec3516de1024497ca1788d8202cf89c6a94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:04:28 +0000

    Add package oelint-adv/7.2.7/oelint_adv-7.2.7.tar.gz

---
+++ packages/oelint-adv/oelint_adv-7.2.7.tar.gz/oelint_adv-7.2.7/tests/test_class_oelint_newline_consecutive.py
@@ -0,0 +1,61 @@
+import pytest  # noqa: I900
+
+from .base import TestBaseClass
+
+
+class TestClassOelintNewlineConsecutive(TestBaseClass):
+
+    @pytest.mark.parametrize('id_', ['oelint.newline.consecutive'])
+    @pytest.mark.parametrize('occurrence', [1])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     A = "1"
+
+
+                                     B = "1"
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_bad(self, input_, id_, occurrence):
+        self.check_for_id(self._create_args(input_), id_, occurrence)
+
+    @pytest.mark.parametrize('id_', ['oelint.newline.consecutive'])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     A = "1"
+
+
+                                     B = "1"
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_fix(self, input_, id_):
+        self.fix_and_check(self._create_args_fix(input_), id_)
+
+    @pytest.mark.parametrize('id_', ['oelint.newline.consecutive'])
+    @pytest.mark.parametrize('occurrence', [0])
+    @pytest.mark.parametrize('input_',
+                             [
+                                 {
+                                     'oelint_adv_test.bb':
+                                     '''
+                                     A = "1"
+
+                                     B = "1"
+                                     C = "1"
+
+                                     D = "1"
+                                     ''',
+                                 },
+                             ],
+                             )
+    def test_good(self, input_, id_, occurrence):
+        self.check_for_id(self._create_args(input_), id_, occurrence)
