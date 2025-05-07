b2d3479dede8caed08d244ad38733306ecc418bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:03:36 +0000

    Add package foamlib/0.8.11/foamlib-0.8.11.tar.gz

---
+++ packages/foamlib/foamlib-0.8.11.tar.gz/foamlib-0.8.11/foamlib/_files/_parsing.py
@@ -0,0 +1,469 @@
+from __future__ import annotations
+
+import re
+import sys
+from typing import Tuple, Union, cast
+
+if sys.version_info >= (3, 9):
+    from collections.abc import Iterator, Mapping, MutableMapping, Sequence
+else:
+    from typing import Iterator, Mapping, MutableMapping, Sequence
+
+if sys.version_info >= (3, 10):
+    from types import EllipsisType
+else:
+    EllipsisType = type(...)
+
+import numpy as np
+from pyparsing import (
+    CaselessKeyword,
+    Combine,
+    Dict,
+    Forward,
+    Group,
+    Keyword,
+    LineEnd,
+    Literal,
+    Located,
+    Opt,
+    ParserElement,
+    ParseResults,
+    Regex,
+    Word,
+    common,
+    counted_array,
+    dbl_quoted_string,
+    identchars,
+    printables,
+)
+
+from ._types import Data, Dimensioned, DimensionSet, File, TensorKind
+
+
+def _tensor(tensor_kind: TensorKind, *, ignore: Regex | None = None) -> Regex:
+    float_pattern = r"(?i:[+-]?(?:(?:\d+\.?\d*(?:e[+-]?\d+)?)|nan|inf(?:inity)?))"
+
+    if tensor_kind == TensorKind.SCALAR:
+        ret = Regex(float_pattern)
+        ret.add_parse_action(lambda tks: [float(tks[0])])
+        return ret
+
+    ignore_pattern = rf"(?:\s|{ignore.re.pattern})+" if ignore is not None else r"\s+"
+
+    ret = Regex(
+        rf"\((?:{ignore_pattern})?(?:{float_pattern}{ignore_pattern}){{{tensor_kind.size - 1}}}{float_pattern}(?:{ignore_pattern})?\)"
+    )
+    ret.add_parse_action(
+        lambda tks: np.fromstring(
+            re.sub(ignore.re, " ", tks[0][1:-1])
+            if ignore is not None
+            else tks[0][1:-1],
+            sep=" ",
+        )
+    )
+    return ret
+
+
+def _list_of(entry: ParserElement) -> ParserElement:
+    return Opt(
+        Literal("List") + Literal("<") + _IDENTIFIER + Literal(">")
+    ).suppress() + (
+        (
+            counted_array(entry, common.integer + Literal("(").suppress())
+            + Literal(")").suppress()
+        ).set_parse_action(lambda tks: [tks.as_list()])
+        | (
+            Literal("(").suppress()
+            + Group((entry)[...], aslist=True)
+            + Literal(")").suppress()
+        )
+        | (
+            common.integer + Literal("{").suppress() + entry + Literal("}").suppress()
+        ).set_parse_action(lambda tks: [[tks[1]] * tks[0]])
+    )
+
+
+def _parse_ascii_field(
+    s: str, tensor_kind: TensorKind, *, ignore: Regex | None
+) -> np.ndarray[tuple[int] | tuple[int, int], np.dtype[np.float64]]:
+    if ignore is not None:
+        s = re.sub(ignore.re, " ", s)
+    s = s.replace("(", " ").replace(")", " ")
+
+    return np.fromstring(s, sep=" ").reshape(-1, *tensor_kind.shape)  # type: ignore [return-value]
+
+
+def _unpack_binary_field(
+    b: bytes, tensor_kind: TensorKind, *, length: int
+) -> np.ndarray[tuple[int] | tuple[int, int], np.dtype[np.float64 | np.float32]]:
+    float_size = len(b) / tensor_kind.size / length
+    assert float_size in (4, 8)
+
+    dtype = np.float32 if float_size == 4 else float
+    return np.frombuffer(b, dtype=dtype).reshape(-1, *tensor_kind.shape)  # type: ignore [return-value]
+
+
+def _tensor_list(
+    tensor_kind: TensorKind, *, ignore: Regex | None = None
+) -> ParserElement:
+    tensor = _tensor(tensor_kind, ignore=ignore)
+    ignore_pattern = rf"(?:\s|{ignore.re.pattern})+" if ignore is not None else r"\s+"
+
+    list_ = Forward()
+
+    list_ <<= Regex(
+        rf"\((?:{ignore_pattern})?(?:{tensor.re.pattern}{ignore_pattern})*{tensor.re.pattern}(?:{ignore_pattern})?\)"
+    ).add_parse_action(
+        lambda tks: [_parse_ascii_field(tks[0], tensor_kind, ignore=ignore)]
+    ) | (
+        (Literal("(") + Literal(")"))
+        .suppress()
+        .add_parse_action(lambda: np.array([]).reshape(0, *tensor_kind.shape))
+    )
+
+    def count_parse_action(tks: ParseResults) -> None:
+        nonlocal list_
+        length = tks[0]
+        assert isinstance(length, int)
+
+        if not length:
+            list_ <<= (
+                ((Literal("(") + Literal(")")) | (Literal("{") + Literal("}")))
+                .suppress()
+                .add_parse_action(lambda: np.array([]).reshape(0, *tensor_kind.shape))
+            )
+            return
+
+        list_ <<= (
+            Regex(
+                rf"\((?:{ignore_pattern})?(?:{tensor.re.pattern}{ignore_pattern}){{{length - 1}}}{tensor.re.pattern}(?:{ignore_pattern})?\)"
+            ).add_parse_action(
+                lambda tks: [_parse_ascii_field(tks[0], tensor_kind, ignore=ignore)]
+            )
+            | Regex(
+                rf"\((?s:({'.' * 8 * tensor_kind.size}|{'.' * 4 * tensor_kind.size}){{{length}}})\)"
+            ).add_parse_action(
+                lambda tks: [
+                    _unpack_binary_field(
+                        tks[0][1:-1].encode("latin-1"), tensor_kind, length=length
+                    )
+                ]
+            )
+            | (
+                Literal("{").suppress() + tensor + Literal("}").suppress()
+            ).add_parse_action(
+                lambda tks: [np.full((length, *tensor_kind.shape), tks[0], dtype=float)]
+            )
+        )
+
+    count = common.integer.copy().add_parse_action(count_parse_action)
+
+    return (
+        Opt(Literal("List") + Literal("<") + str(tensor_kind) + Literal(">")).suppress()
+        + Opt(count).suppress()
+        + list_
+    )
+
+
+def _dict_of(
+    keyword: ParserElement,
+    data: ParserElement,
+    *,
+    directive: ParserElement | None = None,
+    data_entry: ParserElement | None = None,
+    located: bool = False,
+) -> ParserElement:
+    dict_ = Forward()
+
+    keyword_entry = keyword + (dict_ | (data + Literal(";").suppress()))
+
+    if directive is not None:
+        assert data_entry is not None
+        keyword_entry |= directive + data_entry + LineEnd().suppress()
+
+    if located:
+        keyword_entry = Located(keyword_entry)
+
+    dict_ <<= (
+        Literal("{").suppress()
+        + Dict(Group(keyword_entry)[...], asdict=not located)
+        + Literal("}").suppress()
+    )
+
+    return dict_
+
+
+def _keyword_entry_of(
+    keyword: ParserElement,
+    data: ParserElement,
+    *,
+    directive: ParserElement | None = None,
+    data_entry: ParserElement | None = None,
+    located: bool = False,
+) -> ParserElement:
+    keyword_entry = keyword + (
+        _dict_of(
+            keyword, data, directive=directive, data_entry=data_entry, located=located
+        )
+        | (data + Literal(";").suppress())
+    )
+
+    if directive is not None:
+        assert data_entry is not None
+        keyword_entry |= directive + data_entry + LineEnd().suppress()
+
+    if located:
+        keyword_entry = Located(keyword_entry)
+    else:
+        keyword_entry = keyword_entry.copy().set_parse_action(lambda tks: tuple(tks))
+
+    return keyword_entry
+
+
+# https://github.com/pyparsing/pyparsing/pull/584
+_COMMENT = Regex(r"(?:/\*(?:[^*]|\*(?!/))*\*/)|(?://(?:\\\n|[^\n])*)")
+
+_IDENTCHARS = identchars + "$"
+_IDENTBODYCHARS = (
+    printables.replace(";", "")
+    .replace("(", "")
+    .replace(")", "")
+    .replace("{", "")
+    .replace("}", "")
+    .replace("[", "")
+    .replace("]", "")
+)
+
+_SWITCH = (
+    Keyword("yes", _IDENTBODYCHARS)
+    | Keyword("true", _IDENTBODYCHARS)
+    | Keyword("on", _IDENTBODYCHARS)
+    | Keyword("y", _IDENTBODYCHARS)
+    | Keyword("t", _IDENTBODYCHARS)
+).set_parse_action(lambda: True) | (
+    Keyword("no", _IDENTBODYCHARS)
+    | Keyword("false", _IDENTBODYCHARS)
+    | Keyword("off", _IDENTBODYCHARS)
+    | Keyword("n", _IDENTBODYCHARS)
+    | Keyword("f", _IDENTBODYCHARS)
+).set_parse_action(lambda: False)
+_DIMENSIONS = (
+    Literal("[").suppress() + common.number[0, 7] + Literal("]").suppress()
+).set_parse_action(lambda tks: DimensionSet(*tks))
+_TENSOR = (
+    _tensor(TensorKind.SCALAR)
+    | _tensor(TensorKind.VECTOR)
+    | _tensor(TensorKind.SYMM_TENSOR)
+    | _tensor(TensorKind.TENSOR)
+)
+_PARENTHESIZED = Forward()
+_IDENTIFIER = Combine(Word(_IDENTCHARS, _IDENTBODYCHARS) + Opt(_PARENTHESIZED))
+_PARENTHESIZED <<= Combine(
+    Literal("(")
+    + (_PARENTHESIZED | Word(_IDENTBODYCHARS) + Opt(_PARENTHESIZED))
+    + Literal(")")
+)
+
+_DIMENSIONED = (Opt(_IDENTIFIER) + _DIMENSIONS + _TENSOR).set_parse_action(
+    lambda tks: Dimensioned(*reversed(tks.as_list()))
+)
+_FIELD = (Keyword("uniform", _IDENTBODYCHARS).suppress() + _TENSOR) | (
+    Keyword("nonuniform", _IDENTBODYCHARS).suppress()
+    + (
+        _tensor_list(TensorKind.SCALAR, ignore=_COMMENT)
+        | _tensor_list(TensorKind.VECTOR, ignore=_COMMENT)
+        | _tensor_list(TensorKind.SYMM_TENSOR, ignore=_COMMENT)
+        | _tensor_list(TensorKind.TENSOR, ignore=_COMMENT)
+    )
+)
+_DIRECTIVE = Word("#", _IDENTBODYCHARS)
+_TOKEN = dbl_quoted_string | _DIRECTIVE | _IDENTIFIER
+_DATA = Forward()
+_KEYWORD_ENTRY = _keyword_entry_of(_TOKEN | _list_of(_IDENTIFIER), _DATA)
+_DICT = _dict_of(_TOKEN, _DATA)
+_DATA_ENTRY = Forward()
+_LIST_ENTRY = _DICT | _KEYWORD_ENTRY | _DATA_ENTRY
+_LIST = _list_of(_LIST_ENTRY)
+_NUMBER = (
+    common.number
+    | CaselessKeyword("nan").set_parse_action(lambda: np.nan)
+    | (CaselessKeyword("inf") | CaselessKeyword("infinity")).set_parse_action(
+        lambda: np.inf
+    )
+    | (CaselessKeyword("-inf") | CaselessKeyword("-infinity")).set_parse_action(
+        lambda: -np.inf
+    )
+)
+_DATA_ENTRY <<= _FIELD | _LIST | _DIMENSIONED | _DIMENSIONS | _NUMBER | _SWITCH | _TOKEN
+
+_DATA <<= (
+    _DATA_ENTRY[1, ...]
+    .set_parse_action(lambda tks: [tuple(tks)] if len(tks) > 1 else [tks[0]])
+    .ignore(_COMMENT)
+    .parse_with_tabs()
+)
+
+
+def parse_data(s: str) -> Data:
+    if not s.strip():
+        return ""
+    return cast("Data", _DATA.parse_string(s, parse_all=True)[0])
+
+
+_LOCATED_DICTIONARY = Group(
+    _keyword_entry_of(
+        _TOKEN,
+        Opt(_DATA, default=""),
+        directive=_DIRECTIVE,
+        data_entry=_DATA_ENTRY,
+        located=True,
+    )
+)[...]
+_LOCATED_DATA = Group(Located(_DATA.copy().add_parse_action(lambda tks: ["", tks[0]])))
+
+_FILE = (
+    Dict(_LOCATED_DICTIONARY + Opt(_LOCATED_DATA) + _LOCATED_DICTIONARY)
+    .ignore(_COMMENT)
+    .parse_with_tabs()
+)
+
+
+class Parsed(Mapping[Tuple[str, ...], Union[Data, EllipsisType]]):
+    def __init__(self, contents: bytes) -> None:
+        self._parsed: MutableMapping[
+            tuple[str, ...],
+            tuple[int, Data | EllipsisType, int],
+        ] = {}
+        for parse_result in _FILE.parse_string(
+            contents.decode("latin-1"), parse_all=True
+        ):
+            self._parsed.update(self._flatten_result(parse_result))
+
+        self.contents = contents
+        self.modified = False
+
+    @staticmethod
+    def _flatten_result(
+        parse_result: ParseResults, *, _keywords: tuple[str, ...] = ()
+    ) -> Mapping[tuple[str, ...], tuple[int, Data | EllipsisType, int]]:
+        ret: MutableMapping[
+            tuple[str, ...],
+            tuple[int, Data | EllipsisType, int],
+        ] = {}
+        start = parse_result.locn_start
+        assert isinstance(start, int)
+        item = parse_result.value
+        assert isinstance(item, Sequence)
+        end = parse_result.locn_end
+        assert isinstance(end, int)
+        keyword, *data = item
+        assert isinstance(keyword, str)
+        if not keyword:
+            assert not _keywords
+            assert len(data) == 1
+            assert not isinstance(data[0], ParseResults)
+            ret[()] = (start, data[0], end)
+        else:
+            assert isinstance(keyword, str)
+            ret[(*_keywords, keyword)] = (start, ..., end)
+            for d in data:
+                if isinstance(d, ParseResults):
+                    ret.update(
+                        Parsed._flatten_result(d, _keywords=(*_keywords, keyword))
+                    )
+                else:
+                    ret[(*_keywords, keyword)] = (start, d, end)
+        return ret
+
+    def __getitem__(self, keywords: tuple[str, ...]) -> Data | EllipsisType:
+        _, data, _ = self._parsed[keywords]
+        return data
+
+    def put(
+        self,
+        keywords: tuple[str, ...],
+        data: Data | EllipsisType,
+        content: bytes,
+    ) -> None:
+        start, end = self.entry_location(keywords, missing_ok=True)
+
+        diff = len(content) - (end - start)
+        for k, (s, d, e) in self._parsed.items():
+            if s > end:
+                self._parsed[k] = (s + diff, d, e + diff)
+            elif e > start:
+                self._parsed[k] = (s, d, e + diff)
+
+        self._parsed[keywords] = (start, data, end + diff)
+
+        self.contents = self.contents[:start] + content + self.contents[end:]
+        self.modified = True
+
+        for k in list(self._parsed):
+            if keywords != k and keywords == k[: len(keywords)]:
+                del self._parsed[k]
+
+    def __delitem__(self, keywords: tuple[str, ...]) -> None:
+        start, end = self.entry_location(keywords)
+        del self._parsed[keywords]
+
+        for k in list(self._parsed):
+            if keywords == k[: len(keywords)]:
+                del self._parsed[k]
+
+        diff = end - start
+        for k, (s, d, e) in self._parsed.items():
+            if s > end:
+                self._parsed[k] = (s - diff, d, e - diff)
+            elif e > start:
+                self._parsed[k] = (s, d, e - diff)
+
+        self.contents = self.contents[:start] + self.contents[end:]
+        self.modified = True
+
+    def __contains__(self, keywords: object) -> bool:
+        return keywords in self._parsed
+
+    def __iter__(self) -> Iterator[tuple[str, ...]]:
+        return iter(self._parsed)
+
+    def __len__(self) -> int:
+        return len(self._parsed)
+
+    def entry_location(
+        self, keywords: tuple[str, ...], *, missing_ok: bool = False
+    ) -> tuple[int, int]:
+        try:
+            start, _, end = self._parsed[keywords]
+        except KeyError:
+            if missing_ok:
+                if len(keywords) > 1:
+                    assert self[keywords[:-1]] is ...
+                    start, end = self.entry_location(keywords[:-1])
+                    end = self.contents.rindex(b"}", start, end)
+                else:
+                    end = len(self.contents)
+
+                start = end
+            else:
+                raise
+
+        return start, end
+
+    def as_dict(self) -> File:
+        ret: File = {}
+        for keywords, (_, data, _) in self._parsed.items():
+            r = ret
+            for k in keywords[:-1]:
+                v = r[k]
+                assert isinstance(v, dict)
+                r = cast("File", v)
+
+            assert isinstance(r, dict)
+            if keywords:
+                r[keywords[-1]] = {} if data is ... else data
+            else:
+                assert data is not ...
+                r[None] = data
+
+        return ret
---
+++ packages/foamlib/foamlib-0.8.11.tar.gz/foamlib-0.8.11/tests/test_files/test_parsing/test_basic.py
@@ -0,0 +1,119 @@
+import numpy as np
+from foamlib import FoamFile
+from foamlib._files._parsing import Parsed
+
+
+def test_parse_value() -> None:
+    assert Parsed(b"1")[()] == 1
+    assert Parsed(b"1.0")[()] == 1.0
+    assert Parsed(b"1.0e-3")[()] == 1.0e-3
+    assert Parsed(b"yes")[()] is True
+    assert Parsed(b"no")[()] is False
+    assert Parsed(b"word")[()] == "word"
+    assert Parsed(b"inference")[()] == "inference"
+    assert Parsed(b"word word")[()] == ("word", "word")
+    assert Parsed(b'"a string"')[()] == '"a string"'
+    assert Parsed(b"uniform 1")[()] == 1
+    assert Parsed(b"uniform 1.0")[()] == 1.0
+    assert Parsed(b"uniform 1.0e-3")[()] == 1.0e-3
+    assert Parsed(b"(1.0 2.0 3.0)")[()] == [1.0, 2.0, 3.0]
+    field = Parsed(b"uniform (1 2 3)")[()]
+    assert isinstance(field, np.ndarray)
+    assert np.array_equal(field, [1, 2, 3])
+    field = Parsed(b"nonuniform List<scalar> 2(1 2)")[()]
+    assert isinstance(field, np.ndarray)
+    assert np.array_equal(field, [1, 2])
+    field = Parsed(b"nonuniform List<scalar> 2{1}")[()]
+    assert isinstance(field, np.ndarray)
+    assert np.array_equal(field, [1, 1])
+    field = Parsed(b"nonuniform List<symmTensor> 0()")[()]
+    assert isinstance(field, np.ndarray)
+    assert field.shape == (0, 6)
+    field = Parsed(b"nonuniform List<tensor> ()")[()]
+    assert isinstance(field, np.ndarray)
+    assert field.shape == (0, 9)
+    assert Parsed(b"3(1 2 3)")[()] == [1, 2, 3]
+    assert Parsed(b"2((1 2 3) (4 5 6))")[()] == [
+        [1, 2, 3],
+        [4, 5, 6],
+    ]
+    assert Parsed(b"2{(1 2 3)}")[()] == [
+        [1, 2, 3],
+        [1, 2, 3],
+    ]
+    field = Parsed(b"nonuniform List<vector> 2((1 2 3) (4 5 6))")[()]
+    assert isinstance(field, np.ndarray)
+    assert np.array_equal(
+        field,
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+        ],
+    )
+    field = Parsed(b"nonuniform List<vector> 2{(1 2 3)}")[()]
+    assert isinstance(field, np.ndarray)
+    assert np.array_equal(
+        field,
+        [
+            [1, 2, 3],
+            [1, 2, 3],
+        ],
+    )
+    field = Parsed(
+        b"nonuniform List<scalar> 2(\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00@)"
+    )[()]
+    assert isinstance(field, np.ndarray)
+    assert np.array_equal(field, [1, 2])
+    field = Parsed(
+        b"nonuniform List<vector> 2(\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00\x08@\x00\x00\x00\x00\x00\x00\x10@\x00\x00\x00\x00\x00\x00\x14@\x00\x00\x00\x00\x00\x00\x18@)"
+    )[()]
+    assert isinstance(field, np.ndarray)
+    assert np.array_equal(field, [[1, 2, 3], [4, 5, 6]])
+    field = Parsed(b"nonuniform List<scalar> 2(\x00\x00\x80?\x00\x00\x00@)")[()]
+    assert isinstance(field, np.ndarray)
+    assert np.array_equal(field, [1, 2])
+    assert Parsed(b"[1 1 -2 0 0 0 0]")[()] == FoamFile.DimensionSet(
+        mass=1, length=1, time=-2
+    )
+    assert Parsed(b"g [1 1 -2 0 0 0 0] (0 0 -9.81)")[()] == FoamFile.Dimensioned(
+        name="g",
+        dimensions=FoamFile.DimensionSet(mass=1, length=1, time=-2),
+        value=[0, 0, -9.81],
+    )
+    assert Parsed(b"[1 1 -2 0 0 0 0] 9.81")[()] == FoamFile.Dimensioned(
+        dimensions=FoamFile.DimensionSet(mass=1, length=1, time=-2), value=9.81
+    )
+    assert Parsed(b"hex (0 1 2 3 4 5 6 7) (1 1 1) simpleGrading (1 1 1)")[()] == (
+        "hex",
+        [0, 1, 2, 3, 4, 5, 6, 7],
+        [1, 1, 1],
+        "simpleGrading",
+        [1, 1, 1],
+    )
+    assert Parsed(b"(a b; c d;)")[()] == [("a", "b"), ("c", "d")]
+    assert Parsed(b"(a {b c;} d {e g;})")[()] == [
+        ("a", {"b": "c"}),
+        ("d", {"e": "g"}),
+    ]
+    assert Parsed(b"(a (0 1 2); b {})")[()] == [("a", [0, 1, 2]), ("b", {})]
+    assert Parsed(b"({a b; c d;} {e g;})")[()] == [{"a": "b", "c": "d"}, {"e": "g"}]
+    assert Parsed(b"(water oil mercury air)")[()] == ["water", "oil", "mercury", "air"]
+    assert Parsed(b"div(phi,U)")[()] == "div(phi,U)"
+    assert Parsed(b"U.component(1)")[()] == "U.component(1)"
+    assert Parsed(b"div(nuEff*dev(T(grad(U))))")[()] == "div(nuEff*dev(T(grad(U))))"
+    assert Parsed(b"div((nuEff*dev(T(grad(U)))))")[()] == "div((nuEff*dev(T(grad(U)))))"
+    assert Parsed(b"((air and water) { type constant; sigma 0.07; })")[()] == [
+        (["air", "and", "water"], {"type": "constant", "sigma": 0.07})
+    ]
+    assert Parsed(b"[]")[()] == FoamFile.DimensionSet()
+    assert Parsed(b"object f.1;")[("object",)] == "f.1"
+
+
+def test_parse_directive() -> None:
+    assert Parsed(b'#include "filename"')[("#include",)] == '"filename"'
+    assert (
+        Parsed(b"functions\n{\n#includeFunc funcName\nsubdict{}}")[
+            ("functions", "#includeFunc")
+        ]
+        == "funcName"
+    )
