56918fd8421f29cf0a6d00638a9e4f8af15c64a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:40 +0000

    Add package replicate-stainless/0.1.0a10/replicate_stainless-0.1.0a10.tar.gz

---
+++ packages/replicate-stainless/replicate_stainless-0.1.0a10.tar.gz/replicate_stainless-0.1.0a10/src/replicate/types/__init__.py
@@ -0,0 +1,24 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .prediction import Prediction as Prediction
+from .prediction_output import PredictionOutput as PredictionOutput
+from .model_create_params import ModelCreateParams as ModelCreateParams
+from .model_list_response import ModelListResponse as ModelListResponse
+from .model_search_params import ModelSearchParams as ModelSearchParams
+from .account_get_response import AccountGetResponse as AccountGetResponse
+from .training_get_response import TrainingGetResponse as TrainingGetResponse
+from .hardware_list_response import HardwareListResponse as HardwareListResponse
+from .prediction_list_params import PredictionListParams as PredictionListParams
+from .training_create_params import TrainingCreateParams as TrainingCreateParams
+from .training_list_response import TrainingListResponse as TrainingListResponse
+from .deployment_get_response import DeploymentGetResponse as DeploymentGetResponse
+from .deployment_create_params import DeploymentCreateParams as DeploymentCreateParams
+from .deployment_list_response import DeploymentListResponse as DeploymentListResponse
+from .deployment_update_params import DeploymentUpdateParams as DeploymentUpdateParams
+from .prediction_create_params import PredictionCreateParams as PredictionCreateParams
+from .training_cancel_response import TrainingCancelResponse as TrainingCancelResponse
+from .training_create_response import TrainingCreateResponse as TrainingCreateResponse
+from .deployment_create_response import DeploymentCreateResponse as DeploymentCreateResponse
+from .deployment_update_response import DeploymentUpdateResponse as DeploymentUpdateResponse
