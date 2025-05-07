d7c26e31ce940482c5ce51e47d876ec02b9cf59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 17:05:21 +0000

    Add package spark-sink-connector/0.10.0/spark_sink_connector-0.10.0.tar.gz

---
+++ packages/spark-sink-connector/spark_sink_connector-0.10.0.tar.gz/spark_sink_connector-0.10.0/spark_sink_connector/enums.py
@@ -0,0 +1,20 @@
+from enum import Enum, auto
+
+
+class SchemaKind(Enum):
+    AVRO = auto()
+    PROTOBUF = auto()
+
+
+class ConnectorOutputMode(str, Enum):
+    APPEND = "append"
+    OVERWRITE = "overwrite"
+    UPSERT = "upsert"
+
+class OpenTableFormat(str, Enum):
+    HUDI = "hudi"
+    DELTA = "delta"
+
+class ConnectorMode(Enum):
+    STREAM = auto()
+    BATCH = auto()
