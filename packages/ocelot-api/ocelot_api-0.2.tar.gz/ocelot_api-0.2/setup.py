4da702c6020530d169fcbc5c5c17c8418446717d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:04:53 +0000

    Add package ocelot-api/0.2/ocelot_api-0.2.tar.gz

---
+++ packages/ocelot-api/ocelot_api-0.2.tar.gz/ocelot_api-0.2/setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+setup(
+    name='ocelot_api',
+    version='0.2',
+    description="",
+    author="Ai, Qianxiang",
+    author_email='qai222@uky.edu',
+    packages=['ocelot', 'ocelot.schema', 'ocelot.routines', 'ocelot.task', 'ocelot.curator'],
+    keywords='ocelot',
+)
