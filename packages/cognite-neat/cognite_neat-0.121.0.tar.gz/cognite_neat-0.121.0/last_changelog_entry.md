c32cd16c5ccf738d660ae3a4a5079c5f541260b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:05:02 +0000

    Add package cognite-neat/0.121.0/cognite_neat-0.121.0.tar.gz

---
+++ packages/cognite-neat/cognite_neat-0.121.0.tar.gz/cognite_neat-0.121.0/last_changelog_entry.md
@@ -0,0 +1,5 @@
+
+### Added
+
+- Support for reading file metadata.
+`neat.read.cdf.classic.file_metdata(...)`
---
+++ packages/cognite-neat/cognite_neat-0.121.0.tar.gz/cognite_neat-0.121.0/tests/tests_unit/test_rules/test_models/test_base_rules.py
@@ -0,0 +1,42 @@
+from cognite.neat.core._rules.models.information import (
+    InformationClass,
+    InformationInputClass,
+    InformationMetadata,
+)
+
+
+class TestBaseRules:
+    def test_strip_whitespace_metadata(
+        self,
+    ) -> None:
+        meta = InformationMetadata.model_validate(
+            {
+                "role": "  information architect  ",
+                "space": "  my_space  ",
+                "external_id": "  my_external_id  ",
+                "version": "  0.1.0  ",
+                "name": "  My Data Model  ",
+                "description": "  My Data Model Description  ",
+                "created": "  2022-07-01T00:00:00Z  ",
+                "updated": "  2022-07-01T00:00:00Z  ",
+                "creator": " Me, Myself, I  ",
+            }
+        )
+
+        assert meta.space == "my_space"
+        assert meta.external_id == "my_external_id"
+        assert meta.role == "information architect"
+        assert meta.creator == ["Me", "Myself", "I"]
+
+    def test_strip_whitespace_input_class(self) -> None:
+        row = InformationInputClass(
+            class_="  MyClass  ",
+            name="  My Class Name  ",
+            description="  My Class Description  ",
+        )
+
+        class_ = InformationClass.model_validate(row.dump(default_prefix="neat"))
+
+        assert class_.class_.suffix == "MyClass"
+        assert class_.name == "My Class Name"
+        assert class_.description == "My Class Description"
