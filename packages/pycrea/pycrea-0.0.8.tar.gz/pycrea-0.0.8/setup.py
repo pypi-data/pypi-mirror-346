6ace62bd9b115c1c7902ad96f8133f1eb5ec5d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:03:41 +0000

    Add package pycrea/0.0.8/pycrea-0.0.8.tar.gz

---
+++ packages/pycrea/pycrea-0.0.8.tar.gz/pycrea-0.0.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PyCREA",
+    version="0.0.8",
+    packages=find_packages(),
+    install_requires=[ "scipy", "scikit-learn", "numpy"],  
+    include_package_data=True,
+    description="A Python package to parse and manage CREA word vectors",
+    author="Alex Skitowski",
+    author_email="askitowski@mcw.edu",
+    url="https://github.com/askitowski1/CREA-Vectors/tree/main/crea_library",
+)
+ 
