0604fa89271152f568504d4fe5324cd4d901072e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 17:04:58 +0000

    Add package mayutils/1.0.1/mayutils-1.0.1-py3-none-any.whl

---
+++ packages/mayutils/mayutils-1.0.1-py3-none-any.whl/mayutils/__init__.py
@@ -0,0 +1,23 @@
+from mayutils.environment.logging import setup_logging
+from mayutils.visualisation.notebook import setup_notebooks
+from mayutils.objects.dataframes import (
+    setup_dataframes,
+)
+
+
+class Setup(object):
+    initialised: bool = False
+
+    @staticmethod
+    def initialise() -> None:
+        if Setup.initialised:
+            return
+
+        setup_logging()
+        setup_notebooks()
+        setup_dataframes()
+
+        Setup.initialised = True
+
+
+Setup.initialise()
