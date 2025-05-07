1008234f0577b8dc47830a7a428d84ef7dad394a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:04:23 +0000

    Add package mcp-yiqijian/0.5/mcp_yiqijian-0.5.tar.gz

---
+++ packages/mcp-yiqijian/mcp_yiqijian-0.5.tar.gz/mcp_yiqijian-0.5/README.md
@@ -0,0 +1,3 @@
+# MCP Server
+
+The Model Context Protocol (MCP) server provides access to the Yiqijian API. This server allows the use of the Yiqijian open interface to implement the function of providing video copy and generating videos
---
+++ packages/mcp-yiqijian/mcp_yiqijian-0.5.tar.gz/mcp_yiqijian-0.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mcp_yiqijian',
+    version='0.5',
+    packages=find_packages(),
+    description='Yiqijian mcp plugin',
+    long_description=open('README.md').read(),
+    # python3，readme文件中文报错
+    # long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    # url='',
+    author='Yiqijian',
+    author_email='',
+    license='MIT',
+    install_requires=[
+        "fastmcp>=2.2.10",
+        "httpx>=0.28.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "mcp_yiqijian=mcp_yiqijian.main:main",
+        ],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',  # 目标用户群体
+        'License :: OSI Approved :: MIT License',  # 许可证类型
+        'Programming Language :: Python :: 3',  # 支持的 Python 版本
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ]
+)
