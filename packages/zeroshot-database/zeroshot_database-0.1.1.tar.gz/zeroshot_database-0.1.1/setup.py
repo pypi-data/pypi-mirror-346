20d908153523273188b22d9a1db138a8f5ebde3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:04:28 +0000

    Add package zeroshot-database/0.1.1/zeroshot_database-0.1.1.tar.gz

---
+++ packages/zeroshot-database/zeroshot_database-0.1.1.tar.gz/zeroshot_database-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="zeroshot-database",
+    version="0.1.1",
+    description="Stream zeroshot dataset",
+    author="Sheel Taksar",
+    author_email="sheel@zeroshotdata.com",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
