7df776f37c7cd27422555deeede2322cf05a74f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 15:05:12 +0000

    Add package PreKO/0.2.4/preko-0.2.4-py3-none-any.whl

---
+++ packages/PreKO/preko-0.2.4-py3-none-any.whl/PreKO/__init__.py
@@ -0,0 +1,11 @@
+from PreKO.indel import InDelSearcher
+
+
+## For publishing ##
+# hatch version patch
+# or hatch version minor
+# or hatch version major
+
+# hatch build
+# hatch publish --user __token__ -a <your_api_key>
+__version__ = '0.2.4'
