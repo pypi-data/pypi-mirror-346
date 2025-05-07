af2ecf4f4eb49e5a78e0dac5982118bb452fb5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 14:04:27 +0000

    Add package liblaf-cherries/0.0.12/liblaf_cherries-0.0.12-py3-none-any.whl

---
+++ packages/liblaf-cherries/liblaf_cherries-0.0.12-py3-none-any.whl/liblaf/cherries/info/__init__.pyi
@@ -0,0 +1,11 @@
+from ._exp_name import exp_name
+from ._git import git_auto_commit, git_branch, git_commit_sha, git_commit_url, git_info
+
+__all__ = [
+    "exp_name",
+    "git_auto_commit",
+    "git_branch",
+    "git_commit_sha",
+    "git_commit_url",
+    "git_info",
+]
