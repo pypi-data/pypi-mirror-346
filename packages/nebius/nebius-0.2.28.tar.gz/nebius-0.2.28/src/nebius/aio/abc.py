488cdfa39d2066e5361d576b2b91504ba11d686d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 17:04:37 +0000

    Add package nebius/0.2.28/nebius-0.2.28.tar.gz

---
+++ packages/nebius/nebius-0.2.28.tar.gz/nebius-0.2.28/src/nebius/aio/abc.py
@@ -0,0 +1,22 @@
+from collections.abc import Awaitable
+from typing import Protocol, TypeVar
+
+from .base import AddressChannel
+
+T = TypeVar("T")
+
+
+class SyncronizerInterface(Protocol):
+    def run_sync(self, awaitable: Awaitable[T], timeout: float | None = None) -> T: ...
+
+
+class ClientChannelInterface(Protocol):
+    def get_channel_by_method(self, method_name: str) -> AddressChannel: ...
+    def return_channel(self, chan: AddressChannel | None) -> None: ...
+    def discard_channel(self, chan: AddressChannel | None) -> None: ...
+
+    def run_sync(self, awaitable: Awaitable[T], timeout: float | None = None) -> T: ...
+
+
+class GracefulInterface(Protocol):
+    async def close(self, grace: float | None = None) -> None: ...
---
+++ packages/nebius/nebius-0.2.28.tar.gz/nebius-0.2.28/src/nebius/aio/constant_channel.py
@@ -0,0 +1,30 @@
+from collections.abc import Awaitable
+from typing import TypeVar
+
+from nebius.aio.abc import ClientChannelInterface
+
+from .base import AddressChannel
+
+T = TypeVar("T")
+
+
+class Constant(ClientChannelInterface):
+    def __init__(
+        self,
+        method: str,
+        source: ClientChannelInterface,
+    ) -> None:
+        self._method = method
+        self._source = source
+
+    def return_channel(self, chan: AddressChannel | None) -> None:
+        return self._source.return_channel(chan)
+
+    def discard_channel(self, chan: AddressChannel | None) -> None:
+        return self._source.discard_channel(chan)
+
+    def get_channel_by_method(self, method_name: str) -> AddressChannel:
+        return self._source.get_channel_by_method(self._method)
+
+    def run_sync(self, awaitable: Awaitable[T], timeout: float | None = None) -> T:
+        return self._source.run_sync(awaitable, timeout)
