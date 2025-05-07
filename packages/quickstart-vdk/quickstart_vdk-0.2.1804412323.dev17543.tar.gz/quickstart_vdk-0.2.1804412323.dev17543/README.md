38ee755aee393099bdfa32f7c84993d45d19a9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 15:05:12 +0000

    Add package quickstart-vdk/0.2.1804412323.dev17543/quickstart_vdk-0.2.1804412323.dev17543.tar.gz

---
+++ packages/quickstart-vdk/quickstart_vdk-0.2.1804412323.dev17543.tar.gz/quickstart_vdk-0.2.1804412323.dev17543/README.md
@@ -0,0 +1,14 @@
+# Quickstart-VDK
+
+<a href="https://pypistats.org/packages/quickstart-vdk" alt="Monthly Downloads">
+        <img src="https://img.shields.io/pypi/dm/quickstart-vdk.svg" alt="monthly download count for quickstart-vdk"></a>
+
+This is the first VDK packaging that users would install to play around with.
+
+It packages:
+
+* Plugin for a local database to get started quickly
+* Plugin for job management (vdk-control-cli)
+* Plugin for local Control Service installation (vdk-server)
+
+See also [Versatile Data Kit Getting Started](https://github.com/vmware/versatile-data-kit/wiki/Getting-Started).
---
+++ packages/quickstart-vdk/quickstart_vdk-0.2.1804412323.dev17543.tar.gz/quickstart_vdk-0.2.1804412323.dev17543/setup.py
@@ -0,0 +1,38 @@
+# Copyright 2023-2025 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+
+__version__ = "0.2.1804412323.dev17543"
+
+setuptools.setup(
+    name="quickstart-vdk",
+    version=__version__,
+    url="https://github.com/vmware/versatile-data-kit",
+    description="Versatile Data Kit SDK packaging containing common plugins to get started quickly using it.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    # If you add a new dependency to quickstart-vdk, add its location to quickstart-vdk/.plugin-ci.yml in
+    # quickstart_vdk_locations, so that quickstart-vdk would be tested when the new dependency is upgraded
+    install_requires=[
+        "vdk-core",
+        "vdk-plugin-control-cli",
+        "vdk-sqlite",
+        "vdk-ingest-http",
+        "vdk-ingest-file",
+        "vdk-server",
+        "vdk-structlog",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
