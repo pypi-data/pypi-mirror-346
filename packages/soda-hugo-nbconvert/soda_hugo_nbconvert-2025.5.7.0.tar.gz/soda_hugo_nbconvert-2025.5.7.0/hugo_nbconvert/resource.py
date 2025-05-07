9f940848e1a301d78666b437ef40378c3db8b225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 15:05:29 +0000

    Add package soda-hugo-nbconvert/2025.5.7.0/soda_hugo_nbconvert-2025.5.7.0.tar.gz

---
+++ packages/soda-hugo-nbconvert/soda_hugo_nbconvert-2025.5.7.0.tar.gz/soda_hugo_nbconvert-2025.5.7.0/hugo_nbconvert/resource.py
@@ -0,0 +1,24 @@
+content = r"""
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "ec2a032c",
+   "metadata": {},
+   "source": [
+    "---\n",
+    "date: {date}\n",
+    "title: {title}\n",
+    "---"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
+"""
