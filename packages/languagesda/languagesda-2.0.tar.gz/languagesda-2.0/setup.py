2cebcbf247a1d5365305c887d5699563703f8c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:17 +0000

    Add package languagesda/2.0/languagesda-2.0.tar.gz

---
+++ packages/languagesda/languagesda-2.0.tar.gz/languagesda-2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='languagesda',
+    version='2.0',
+    packages = ['languagesda'],
+    install_requires=[],
+    python_requires=">=3.11",
+    author="development",
+    author_email="agcs-development@rambler.ru"
+)
