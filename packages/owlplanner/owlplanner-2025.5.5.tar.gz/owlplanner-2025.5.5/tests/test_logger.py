295403c494d7691121fca22e16540611d05134f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 15:04:24 +0000

    Add package owlplanner/2025.5.5/owlplanner-2025.5.5.tar.gz

---
+++ packages/owlplanner/owlplanner-2025.5.5.tar.gz/owlplanner-2025.5.5/tests/test_logger.py
@@ -0,0 +1,21 @@
+from io import StringIO
+
+from owlplanner import logging
+
+
+def test_logger1():
+    strio = StringIO()
+    mylog = logging.Logger(True, [strio])
+    msg1 = 'Hello'
+    mylog.vprint(msg1)
+    msg2 = strio.getvalue().splitlines()
+    assert msg1 == msg2[-1]
+
+
+def test_logger2():
+    strio = StringIO()
+    mylog = logging.Logger(False, [strio])
+    msg1 = 'Hello'
+    mylog.vprint(msg1)
+    msg2 = strio.getvalue()
+    assert '' == msg2
