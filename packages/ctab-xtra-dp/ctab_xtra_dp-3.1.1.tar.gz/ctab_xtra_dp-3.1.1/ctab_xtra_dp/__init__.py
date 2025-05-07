5d28c4738fecf8d92a23105a1ccececaed08e382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:04:14 +0000

    Add package ctab-xtra-dp/3.1.1/ctab_xtra_dp-3.1.1.tar.gz

---
+++ packages/ctab-xtra-dp/ctab_xtra_dp-3.1.1.tar.gz/ctab_xtra_dp-3.1.1/ctab_xtra_dp/__init__.py
@@ -0,0 +1,17 @@
+from .model.ctabgan import CTAB_XTRA_DP
+
+
+from .model import evaluation as _evaluation
+
+from .datasets.dataset_loader import load_demo , display_demo
+
+
+__all__ = [
+    "CTAB_XTRA_DP",
+    "evaluation",
+    "load_demo",
+    "display_demo",
+]
+
+
+__version__ = "0.1.0"
