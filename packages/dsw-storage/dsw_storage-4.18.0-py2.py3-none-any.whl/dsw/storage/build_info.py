b97f7ba309ae9a77bd4ed24b4778b06723d64aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:04:31 +0000

    Add package dsw-storage/4.18.0/dsw_storage-4.18.0-py2.py3-none-any.whl

---
+++ packages/dsw-storage/dsw_storage-4.18.0-py2.py3-none-any.whl/dsw/storage/build_info.py
@@ -0,0 +1,17 @@
+# Generated file
+# - do not overwrite
+# - do not include in git
+from collections import namedtuple
+
+BuildInfo = namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v4.18.0~c60f053',
+    built_at='2025-05-07 06:19:19Z',
+    sha='c60f0537838e0c1d79e267298dbdd40c8ec26fd3',
+    branch='HEAD',
+    tag='v4.18.0',
+)
