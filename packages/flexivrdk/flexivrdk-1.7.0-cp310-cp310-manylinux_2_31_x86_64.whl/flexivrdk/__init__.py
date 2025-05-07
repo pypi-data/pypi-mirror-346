f17aeb8f95174ff35ebfb5c77d63c8580d965cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:21:27 +0000

    Add package flexivrdk/1.7.0/flexivrdk-1.7.0-cp310-cp310-manylinux_2_31_x86_64.whl

---
+++ packages/flexivrdk/flexivrdk-1.7.0-cp310-cp310-manylinux_2_31_x86_64.whl/flexivrdk/__init__.py
@@ -0,0 +1,15 @@
+# flexivrdk/__init__.py
+
+"""
+Python wrapper of Flexiv RDK C++ library (https://github.com/flexivrobotics/flexiv_rdk)
+
+- Main website: https://www.flexiv.com/software/rdk
+- User Manual: https://www.flexiv.com/software/rdk/manual
+- API Doc: https://www.flexiv.com/software/rdk/api
+
+"""
+
+__version__ = "1.7.0"
+__author__ = "Flexiv Ltd"
+
+from .flexivrdk import *
