90aba33133c34f4d0b2c7b1f54359856cd94d708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 15:04:40 +0000

    Add package meshagent-cli/0.0.19/meshagent_cli-0.0.19.tar.gz

---
+++ packages/meshagent-cli/meshagent_cli-0.0.19.tar.gz/meshagent_cli-0.0.19/meshagent/cli/projects.py
@@ -0,0 +1,31 @@
+import typer
+from rich import print
+from meshagent.cli import async_typer
+from meshagent.cli.helper import get_client, print_json_table, set_active_project
+
+app = async_typer.AsyncTyper()
+
+
+@app.async_command("list")
+async def list():
+    client = await get_client()
+    projects = await client.list_projects()
+    print_json_table(projects["projects"], "id", "name")
+    await client.close()
+
+@app.async_command("activate")
+async def activate(project_id: str):
+    client = await get_client()
+    try:
+        projects = await client.list_projects()
+        projects = projects["projects"]
+        for project in projects:
+            if project["id"] == project_id:
+                await set_active_project(project_id=project_id)
+                return
+
+        print(f"[red]Invalid project id: {project_id}[/red]")
+        raise typer.Exit(code=1)
+    finally:
+        await client.close()
+
