2e4dbe928de06536dfe851077c5c849ff9520408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:24 +0000

    Add package polars-features/0.1.0/polars_features-0.1.0-py3-none-any.whl

---
+++ packages/polars-features/polars_features-0.1.0-py3-none-any.whl/functime/base/__init__.py
@@ -0,0 +1,5 @@
+from functime.base.forecaster import Forecaster
+from functime.base.metric import metric
+from functime.base.transformer import Transformer, transformer
+
+__all__ = ["Forecaster", "Transformer", "transformer", "metric"]
