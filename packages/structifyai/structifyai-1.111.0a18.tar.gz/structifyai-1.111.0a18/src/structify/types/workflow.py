fe2f8e5499efb5e5438a1d3ff2e5d7db16350a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 14:04:39 +0000

    Add package structifyai/1.111.0a18/structifyai-1.111.0a18.tar.gz

---
+++ packages/structifyai/structifyai-1.111.0a18.tar.gz/structifyai-1.111.0a18/src/structify/types/workflow.py
@@ -0,0 +1,54 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Union
+from typing_extensions import Literal, TypeAlias
+
+from pydantic import Field as FieldInfo
+
+from .._models import BaseModel
+
+__all__ = [
+    "Workflow",
+    "Step",
+    "StepOperation",
+    "StepOperationEnhanceProperties",
+    "StepOperationEnhanceRelationship",
+    "StepOperationDeriveProperty",
+]
+
+
+class StepOperationEnhanceProperties(BaseModel):
+    enhance_properties: List[str] = FieldInfo(alias="EnhanceProperties")
+
+
+class StepOperationEnhanceRelationship(BaseModel):
+    enhance_relationship: str = FieldInfo(alias="EnhanceRelationship")
+
+
+class StepOperationDeriveProperty(BaseModel):
+    derive_property: List[str] = FieldInfo(alias="DeriveProperty")
+
+
+StepOperation: TypeAlias = Union[
+    StepOperationEnhanceProperties, StepOperationEnhanceRelationship, StepOperationDeriveProperty, Literal["IngestData"]
+]
+
+
+class Step(BaseModel):
+    id: str
+
+    children: List[str]
+
+    operation: StepOperation
+
+    table_name: str
+
+
+class Workflow(BaseModel):
+    name: str
+
+    starting_step: str
+
+    starting_table: str
+
+    steps: List[Step]
