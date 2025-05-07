a9d3160926ddaa7af6561a5cf0aa1cb47388fc40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 14:04:36 +0000

    Add package dark-matter/5.0.36/dark_matter-5.0.36.tar.gz

---
+++ packages/dark-matter/dark_matter-5.0.36.tar.gz/dark_matter-5.0.36/bin/read-blast-json.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Read simplified JSON BLAST records and report the elapsed time.
+"""
+
+from time import time
+import sys
+
+from dark.blast.conversion import JSONRecordsReader
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: %s file.json" % sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+    else:
+        start = time()
+        jsonReader = JSONRecordsReader(sys.argv[1])
+        records = jsonReader.records()
+        for count, record in enumerate(records, start=1):
+            pass
+        elapsed = time() - start
+        print(
+            "Read %d JSON BLAST records in %.3f secs (%.0f records/sec)"
+            % (count, elapsed, float(count) / float(elapsed))
+        )
---
+++ packages/dark-matter/dark_matter-5.0.36.tar.gz/dark_matter-5.0.36/bin/sff-to-fastq.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from Bio import SeqIO
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: %s file.sff > file.fastq" % sys.argv[0], file=sys.stderr)
+    sys.exit(1)
+else:
+    SeqIO.convert(sys.argv[1], "sff", sys.stdout, "fastq")
