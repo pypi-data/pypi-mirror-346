531efd84c98a1773d52095a8e4e7fcadc5136111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:06 +0000

    Add package amsdal_models/0.4.15/amsdal_models-0.4.15-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/amsdal_models/amsdal_models-0.4.15-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/amsdal_models/errors.py
@@ -0,0 +1,15 @@
+from amsdal_utils.errors import AmsdalError
+
+
+class AmsdalValidationError(AmsdalError):
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+
+class MigrationsError(AmsdalError): ...
+
+
+class AmsdalTypeError(AmsdalError):
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(message)
