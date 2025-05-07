bdef619f8093da9855dd10c6db6d30cad31fe822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:04:54 +0000

    Add package dbt-sqlx/1.3.0/dbt_sqlx-1.3.0-py3-none-any.whl

---
+++ packages/dbt-sqlx/dbt_sqlx-1.3.0-py3-none-any.whl/dbt_sqlx/support.py
@@ -0,0 +1,26 @@
+SUPPORTED_SQL_TYPES = {
+    'oracle', 
+    'sql-server', 
+    'snowflake', 
+    'redshift', 
+    'postgres', 
+    'mysql', 
+    'bigquery',
+    'spark-sql', 
+    'clickhouse',
+    'db2',
+    'duckdb',
+    'databrick-sql',
+    'trino',
+    'vertica',
+    'athena',
+    'presto',
+    'google-alloy-db',
+    'mariadb',
+    'presto',
+    'apache-hive',
+    'sqlite'}
+SUPPORTED_PROVIDERS = ['OpenAI', 'Groq','Anthropic','MistralAI','Google_GenAI']
+PROVIDER_API_KEY_PREFIX = {
+    'mistralai': 'mistralai'
+}
