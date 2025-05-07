5c796c3c58155841f579c05307e85dd51ee6a25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 15:04:56 +0000

    Add package pyenvrunner/0.1.2/pyenvrunner-0.1.2-py3-none-any.whl

---
+++ packages/pyenvrunner/pyenvrunner-0.1.2-py3-none-any.whl/core/config.py
@@ -0,0 +1,12 @@
+# src/core/config.py
+
+DEFAULT_IMPORT_TO_PACKAGE_MAP = {
+    "yaml": "PyYAML",
+    "cv2": "opencv-python",
+    "sklearn": "scikit-learn",
+    "PIL": "Pillow",
+    "dotenv": "python-dotenv",
+    # Add more mappings as needed
+}
+
+DEFAULT_REQUIREMENTS_FILE = "pyenvrunner_requirements.txt"
