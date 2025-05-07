c4a4e5bbadd5735cc4f1bb778150c70c502e535b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:04:18 +0000

    Add package VegasAfterglow/0.1.2/vegasafterglow-0.1.2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/VegasAfterglow/vegasafterglow-0.1.2-cp311-cp311-macosx_11_0_arm64.whl/VegasAfterglow/__init__.py
@@ -0,0 +1,13 @@
+from .types import ModelParams, Setups, ObsData, VegasMC, FitResult, ParamDef, Scale
+from .runner import Fitter
+
+__all__ = [
+    "ModelParams",
+    "Setups",
+    "ObsData",
+    "VegasMC",
+    "FitResult",
+    "Fitter",
+    "ParamDef",
+    "Scale",
+]
