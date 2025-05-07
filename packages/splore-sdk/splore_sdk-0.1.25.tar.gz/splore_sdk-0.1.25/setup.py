93e6b632dfeaabfb230108d3b0e4f4838a6be2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:04:55 +0000

    Add package splore-sdk/0.1.25/splore_sdk-0.1.25.tar.gz

---
+++ packages/splore-sdk/splore_sdk-0.1.25.tar.gz/splore_sdk-0.1.25/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="splore-sdk",
+    version="0.1.25",
+    author="DilipCoder",
+    author_email="dilips.ven@splore.com",
+    description="Python SDK for Splore",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/splorehq/splore-sdk-py",
+    project_urls={
+        "Homepage": "https://github.com/splorehq/splore-sdk-py",
+        "Documentation": "https://github.com/splorehq/splore-sdk-py",
+        "Changelog": "https://github.com/splorehq/splore-sdk-py/blob/main/CHANGELOG.md",
+        "Issues": "https://github.com/splorehq/splore-sdk-py/issues",
+        "Releases": "https://github.com/splorehq/splore-sdk-py/releases"
+    },
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.7',
+)
