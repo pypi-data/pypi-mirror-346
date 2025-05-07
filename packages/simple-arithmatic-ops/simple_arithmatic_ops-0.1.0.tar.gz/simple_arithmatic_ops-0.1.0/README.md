6ad36ec07a1d5ee1d4dba28bc1398c8a1cb7e398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 16:04:30 +0000

    Add package simple-arithmatic-ops/0.1.0/simple_arithmatic_ops-0.1.0.tar.gz

---
+++ packages/simple-arithmatic-ops/simple_arithmatic_ops-0.1.0.tar.gz/simple_arithmatic_ops-0.1.0/README.md
@@ -0,0 +1,32 @@
+# Simple Arithmatic Ops
+
+A simple Python package for integer arithmetic operations.
+
+## Installation
+
+You can install the package using pip:
+
+```bash
+pip install simple-arithmatic-ops
+```
+
+## Usage
+
+```python
+from arithmatic import add, subtract
+
+# Addition
+result = add(5, 3)  # Returns 8
+
+# Subtraction
+result = subtract(10, 4)  # Returns 6
+```
+
+## Features
+
+- Simple integer addition
+- Simple integer subtraction
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details. 
---
+++ packages/simple-arithmatic-ops/simple_arithmatic_ops-0.1.0.tar.gz/simple_arithmatic_ops-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simple-arithmatic-ops",
+    version="0.1.0",
+    author="Zilberguy",
+    author_email="Some_random_email@gmail.com",
+    description="A simple Python package for integer arithmetic operations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
