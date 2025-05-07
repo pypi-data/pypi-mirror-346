ea68deb2451fe17a3fcb62f6db8349f436ac8c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 17:05:23 +0000

    Add package kaniwork/0.0.1/kaniwork-0.0.1.tar.gz

---
+++ packages/kaniwork/kaniwork-0.0.1.tar.gz/kaniwork-0.0.1/README.md
@@ -0,0 +1,1 @@
+this is a fibonacci series program
---
+++ packages/kaniwork/kaniwork-0.0.1.tar.gz/kaniwork-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+name="kaniwork",
+version="0.0.1",
+author="kanishka",
+author_email="k60413617@gmail.com",
+packages=find_packages()
+)
