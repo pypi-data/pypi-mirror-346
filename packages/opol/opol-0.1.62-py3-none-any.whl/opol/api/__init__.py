43e46dafa3cdc58727294b3bea2344a9e388015d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:40 +0000

    Add package opol/0.1.62/opol-0.1.62-py3-none-any.whl

---
+++ packages/opol/opol-0.1.62-py3-none-any.whl/opol/api/__init__.py
@@ -0,0 +1,9 @@
+from .articles import Articles
+from .entities import Entities
+from .geo import Geo
+from .classification import Classification
+from .models import *
+from .client_base import BaseClient
+from .scraping import Scraping
+
+__all__ = ["Articles", "Entities", "Geo", "Classification", "BaseClient", "Scraping", "models"]
