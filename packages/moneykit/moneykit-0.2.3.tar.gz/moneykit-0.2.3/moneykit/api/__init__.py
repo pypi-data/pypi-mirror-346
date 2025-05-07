11a36656f3593efefabc2b6b424f326fa0ecd52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:20:54 +0000

    Add package moneykit/0.2.3/moneykit-0.2.3.tar.gz

---
+++ packages/moneykit/moneykit-0.2.3.tar.gz/moneykit-0.2.3/moneykit/api/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+
+# import apis into api package
+from moneykit.api.access_token_api import AccessTokenApi
+from moneykit.api.account_numbers_api import AccountNumbersApi
+from moneykit.api.accounts_api import AccountsApi
+from moneykit.api.identity_api import IdentityApi
+from moneykit.api.institutions_api import InstitutionsApi
+from moneykit.api.investments_api import InvestmentsApi
+from moneykit.api.link_session_api import LinkSessionApi
+from moneykit.api.links_api import LinksApi
+from moneykit.api.products_api import ProductsApi
+from moneykit.api.transactions_api import TransactionsApi
+from moneykit.api.users_api import UsersApi
+from moneykit.api.webhooks_api import WebhooksApi
