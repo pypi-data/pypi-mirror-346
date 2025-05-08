add6cf19ade3f946e384512c169468beb6551347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:22 +0000

    Add package shapeout2/2.22.0/shapeout2-2.22.0-py3-none-any.whl

---
+++ packages/shapeout2/shapeout2-2.22.0-py3-none-any.whl/shapeout2/plot_cache.py
@@ -0,0 +1,78 @@
+"""Facilitate caching of plot data"""
+from dclab.kde import KernelDensityEstimator
+
+from . import util
+
+
+def get_contour_data(rtdc_ds, xax, yax, xacc, yacc, xscale, yscale,
+                     kde_type="histogram", kde_kwargs=None):
+    if kde_kwargs is None:
+        kde_kwargs = {}
+    rtdc_ds.apply_filter()
+    cfg = rtdc_ds.config
+    tohash = [
+        rtdc_ds.identifier, rtdc_ds.filter.all,
+        cfg.get("calculation", ""),
+        xax, yax, xacc, yacc, xscale, yscale,
+        kde_type, kde_kwargs]
+    shash = util.hashobj(tohash)
+    if shash in cache_data:
+        x, y, den = cache_data[shash]
+    else:
+        # compute scatter plot data
+        kde_instance = KernelDensityEstimator(rtdc_ds=rtdc_ds)
+        x, y, den = kde_instance.get_contour(
+            xax=xax,
+            yax=yax,
+            xacc=xacc,
+            yacc=yacc,
+            xscale=xscale,
+            yscale=yscale,
+            kde_type=kde_type,
+            kde_kwargs=kde_kwargs)
+        # save in cache
+        cache_data[shash] = x, y, den
+    return x, y, den
+
+
+def get_scatter_data(rtdc_ds, downsample, xax, yax, xscale, yscale,
+                     kde_type="histogram", kde_kwargs=None):
+    if kde_kwargs is None:
+        kde_kwargs = {}
+    rtdc_ds.apply_filter()
+    cfg = rtdc_ds.config
+    tohash = [
+        rtdc_ds.identifier, rtdc_ds.filter.all, downsample,
+        cfg.get("calculation", ""),
+        xax, yax, xscale, yscale, kde_type, kde_kwargs]
+    shash = util.hashobj(tohash)
+    if shash in cache_data:
+        x, y, kde, idx = cache_data[shash]
+    else:
+        # compute scatter plot data
+        x, y, idx = rtdc_ds.get_downsampled_scatter(
+            xax=xax,
+            yax=yax,
+            downsample=downsample,
+            xscale=xscale,
+            yscale=yscale,
+            remove_invalid=True,
+            ret_mask=True)
+        # kde
+        kde = rtdc_ds.get_kde_scatter(
+            xax=xax,
+            yax=yax,
+            positions=(x, y),
+            kde_type=kde_type,
+            kde_kwargs=kde_kwargs,
+            xscale=xscale,
+            yscale=yscale)
+        if kde.size and kde.min() != kde.max():
+            kde -= kde.min()
+            kde /= kde.max()
+        # save in cache
+        cache_data[shash] = x, y, kde, idx
+    return x, y, kde, idx
+
+
+cache_data = {}
