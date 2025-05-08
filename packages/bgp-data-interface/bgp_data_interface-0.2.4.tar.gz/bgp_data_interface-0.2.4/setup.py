4ddfcf1cbb9006c8d36840ce2a85d63b17a15072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:10 +0000

    Add package bgp-data-interface/0.2.4/bgp_data_interface-0.2.4.tar.gz

---
+++ packages/bgp-data-interface/bgp_data_interface-0.2.4.tar.gz/bgp_data_interface-0.2.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+setup(
+    name='bgp_data_interface',
+    version=VERSION,
+    author='Anurat Chapanond',
+    author_email='anurat.c@bgrimmpower.com',
+    packages=find_packages(),
+    scripts=[],
+    url='http://pypi.python.org/pypi/bgp_data_interface/',
+    license='LICENSE.txt',
+    description='This is a python library for accessing internal and public data e.g. PI, AMR, openmeteo, etc.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'pandas', 'numpy', 'requests', 'pytz', 'urllib3'
+    ],
+)
