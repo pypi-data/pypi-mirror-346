dfb12ad52d563daf220eeeded0631fa07b2ecb0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 17:05:03 +0000

    Add package smartpush/1.4.2/smartpush-1.4.2.tar.gz

---
+++ packages/smartpush/smartpush-1.4.2.tar.gz/smartpush-1.4.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smartpush',
+    version='1.4.2',
+    description='用于smartpush自动化测试工具包',
+    author='lulu、felix、long',
+    packages=find_packages(),
+    install_requires=[
+        # List your package's dependencies here
+        # TODO
+    ],
+)
