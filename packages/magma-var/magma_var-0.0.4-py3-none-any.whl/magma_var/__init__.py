5e69915b37283f7f2e38a990ff08b8a480d16f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:36 +0000

    Add package magma-var/0.0.4/magma_var-0.0.4-py3-none-any.whl

---
+++ packages/magma-var/magma_var-0.0.4-py3-none-any.whl/magma_var/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pkg_resources import get_distribution
+from magma_var import resources
+from magma_var.plot import Plot
+
+__version__ = get_distribution("magma-var").version
+__author__ = "Martanto"
+__author_email__ = "martanto@live.com"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2024, MAGMA Indonesia"
+__url__ = "https://github.com/martanto/magma-var"
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__license__",
+    "__copyright__",
+    "resources",
+    "Plot",
+]
