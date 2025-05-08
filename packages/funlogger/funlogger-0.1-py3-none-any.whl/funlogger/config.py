9d0145b50aa1f1672f434f5e370a071fcd37a3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 19:04:56 +0000

    Add package funlogger/0.1/funlogger-0.1-py3-none-any.whl

---
+++ packages/funlogger/funlogger-0.1-py3-none-any.whl/funlogger/config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+
+# SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+
+# SPDX-License-Identifier: Apache Software License
+import os
+from getpass import getuser
+from platform import machine
+
+
+class _Setting:
+
+    SYS_ARCH = machine()
+    USERNAME = getuser()
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    # 日志文件生成的路径
+    LOG_FILE_PATH = os.path.join(ROOT_DIR, "logs")
+    # 本机IP
+    HOST_IP = str(os.popen("hostname -I |awk '{print $1}'").read()).strip("\n").strip()
+    TAG = "DEFAULT"
+
+    LOG_LEVEL = "DEBUG"
+    CLASS_NAME_STARTSWITH: list = []
+    CLASS_NAME_ENDSWITH: list = []
+    CLASS_NAME_CONTAIN: list = []
+
+config = _Setting()
