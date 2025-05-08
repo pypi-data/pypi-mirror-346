e254cbc1ba3f5eb48b50dc399a1eee5923565045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 04:06:45 +0000

    Add package trs-google-lib/0.1.2/trs_google_lib-0.1.2.tar.gz

---
+++ packages/trs-google-lib/trs_google_lib-0.1.2.tar.gz/trs_google_lib-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="trs_google_lib",
+    version="0.1.2",
+    py_modules=["google_trans_simple"],
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+        "loguru",
+    ],
+    author="Ivan Khomich",
+    description="Асинхронная библиотека для перевода через Google с поддержкой прокси",
+)
