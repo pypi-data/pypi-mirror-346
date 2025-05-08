9b73ecd162c6e4fd00e332aef292876df413dc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 17:05:32 +0000

    Add package curlifier/0.2.3/curlifier-0.2.3-py3-none-any.whl

---
+++ packages/curlifier/curlifier-0.2.3-py3-none-any.whl/curlifier/structures/types.py
@@ -0,0 +1,6 @@
+from typing import TypeAlias
+
+CurlCommandShort: TypeAlias = str
+CurlCommandLong: TypeAlias = str
+CurlCommand: TypeAlias = CurlCommandShort | CurlCommandLong
+CurlCommandTitle: TypeAlias = str
