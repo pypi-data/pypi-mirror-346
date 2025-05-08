eaa954e836c4b1b89fb7746d4b30dad8f6caafe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:28 +0000

    Add package urnc/2.0.12/urnc-2.0.12-py3-none-any.whl

---
+++ packages/urnc/urnc-2.0.12-py3-none-any.whl/urnc/__init__.py
@@ -0,0 +1,10 @@
+"""
+University of Regensburg Notebook Converter
+
+Package for converting jupyter notebook files within a course directory into a
+format suitable for student use. It simplifies the process of preparing and
+maintaining course materials by providing easy-to-use commands to check,
+convert, and manage course versions.
+"""
+
+from urnc import ci, convert, logger, pull, util, version, format, config, git, init
