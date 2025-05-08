13131a3bc140685f7bbeb044a687988e27f13658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 06:07:49 +0000

    Add package GoodByeSyntax/1.3/goodbyesyntax-1.3.tar.gz

---
+++ packages/GoodByeSyntax/goodbyesyntax-1.3.tar.gz/goodbyesyntax-1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GoodByeSyntax',
+    version='1.3',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pyperclip'
+    ],
+    extras_require={
+        "email_support": ["smtplib"],
+    },
+    author='GoodByeSyntax',
+    author_email='goodbyesyntax.official@gmail.com',
+    description='GoodByeSyntax Python Module',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/GoodByeSyntax/GBS',
+    classifiers=[  
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+    ],
+)
