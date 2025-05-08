66769c1db100d52adcabb69e077c386a9785643e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:35 +0000

    Add package conda-subprocess/0.0.7/conda_subprocess-0.0.7.tar.gz

---
+++ packages/conda-subprocess/conda_subprocess-0.0.7.tar.gz/conda_subprocess-0.0.7/tests/test_conda_function.py
@@ -0,0 +1,76 @@
+import os
+import sys
+import unittest
+
+try:
+    from conda_subprocess.decorator import conda
+    from executorlib import SingleNodeExecutor
+    from executorlib.standalone.serialize import cloudpickle_register
+except ImportError:
+
+    def conda(prefix_name=None, prefix_path=None):
+        def wrap_function(funct):
+            def function_out(*args, **kwargs):
+                return None
+
+            return function_out
+
+        return wrap_function
+
+
+@conda(prefix_name="py313")
+def add_function(parameter_1, parameter_2):
+    import os
+
+    return parameter_1 + parameter_2, os.environ["CONDA_PREFIX"]
+
+
+@conda(prefix_name="py313")
+def get_exe(parameter_1, parameter_2):
+    import sys
+
+    return parameter_1 + parameter_2, sys.executable
+
+
+@conda(prefix_name="py313")
+def error_funct(parameter_1):
+    raise ValueError
+
+
+@unittest.skipIf(
+    sys.version_info.minor != 13 or os.name == "nt",
+    "Test environment has to be Python 3.13 for consistency and Windows is currently not supported.",
+)
+class TestCondaFunction(unittest.TestCase):
+    def test_conda_function(self):
+        cloudpickle_register(ind=1)
+        number, prefix = add_function(parameter_1=1, parameter_2=2)
+        self.assertTrue("py313" in prefix.split(os.sep))
+        self.assertEqual(number, 3)
+
+    def test_conda_exe_function(self):
+        cloudpickle_register(ind=1)
+        number, prefix = get_exe(parameter_1=1, parameter_2=2)
+        self.assertTrue("py313" in prefix.split(os.sep))
+        self.assertEqual(number, 3)
+
+    def test_conda_function_error(self):
+        cloudpickle_register(ind=1)
+        with self.assertRaises(ValueError):
+            error_funct(parameter_1=1)
+
+    def test_conda_function_with_executorlib(self):
+        cloudpickle_register(ind=1)
+        with SingleNodeExecutor(max_cores=1, hostname_localhost=True) as exe:
+            future = exe.submit(add_function, 1, 2)
+            number, prefix = future.result()
+        self.assertTrue("py313" in prefix.split(os.sep))
+        self.assertEqual(number, 3)
+
+    def test_conda_exe_with_executorlib(self):
+        cloudpickle_register(ind=1)
+        with SingleNodeExecutor(max_cores=1, hostname_localhost=True) as exe:
+            future = exe.submit(get_exe, 1, 2)
+            number, prefix = future.result()
+        self.assertTrue("py313" in prefix.split(os.sep))
+        self.assertEqual(number, 3)
---
+++ packages/conda-subprocess/conda_subprocess-0.0.7.tar.gz/conda_subprocess-0.0.7/tests/test_conda_subprocess.py
@@ -0,0 +1,201 @@
+import os
+from subprocess import (
+    PIPE,
+    CalledProcessError,
+    TimeoutExpired,
+    check_output as subprocess_check_output,
+)
+import unittest
+
+from conda.base.context import context
+from conda_subprocess import call, check_call, check_output, run, Popen
+
+
+class TestCondaSubprocess(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.env_name = "py313"
+        cls.env_path = os.path.join(context.root_prefix, "..", cls.env_name)
+
+    def test_call_path(self):
+        self.assertEqual(call("python --version", prefix_path=self.env_path), 0)
+
+    def test_call_name(self):
+        self.assertEqual(call("python --version", prefix_name=self.env_name), 0)
+
+    def test_check_call_path(self):
+        self.assertEqual(check_call("python --version", prefix_path=self.env_path), 0)
+
+    def test_check_call_name(self):
+        self.assertEqual(check_call("python --version", prefix_name=self.env_name), 0)
+
+    @unittest.skipIf(
+        os.name == "nt",
+        "Windows is currently not supported.",
+    )
+    def test_check_output_path(self):
+        expected_output = (
+            b"Python 3.13.2\r\n" if os.name == "nt" else b"Python 3.13.2\n"
+        )
+        self.assertEqual(
+            check_output("python --version", prefix_path=self.env_path, input=None),
+            expected_output,
+        )
+
+    @unittest.skipIf(
+        os.name == "nt",
+        "Windows is currently not supported.",
+    )
+    def test_check_output_name(self):
+        expected_output = (
+            b"Python 3.13.2\r\n" if os.name == "nt" else b"Python 3.13.2\n"
+        )
+        self.assertEqual(
+            check_output("python --version", prefix_name=self.env_name),
+            expected_output,
+        )
+
+    @unittest.skipIf(
+        os.name == "nt",
+        "Windows is currently not supported.",
+    )
+    def test_check_output_env_name(self):
+        output_new = check_output("which python", prefix_name=self.env_name, universal_newlines=True)
+        output_classic = subprocess_check_output(["conda", "run", "-p", self.env_path, "which", "python"], universal_newlines=True)
+        self.assertTrue(
+            "py313" in output_new.split("/")
+        )
+        self.assertEqual(output_new.rstrip(), output_classic.rstrip())
+
+    @unittest.skipIf(
+        os.name == "nt",
+        "Windows is currently not supported.",
+    )
+    def test_check_output_universal_newlines(self):
+        self.assertEqual(
+            check_output(
+                "python --version", prefix_path=self.env_path, universal_newlines=True, input=None
+            ),
+            "Python 3.13.2\n",
+        )
+
+    def test_run_path(self):
+        self.assertEqual(
+            run("python --version", prefix_path=self.env_path).returncode, 0
+        )
+
+    def test_run_name(self):
+        self.assertEqual(
+            run("python --version", prefix_name=self.env_name).returncode, 0
+        )
+
+    @unittest.skipIf(
+        os.name == "nt",
+        "Windows is currently not supported.",
+    )
+    def test_popen_path(self):
+        expected_output = (
+            b"Python 3.13.2\r\n" if os.name == "nt" else b"Python 3.13.2\n"
+        )
+        process = Popen("python --version", prefix_path=self.env_path, stdout=PIPE)
+        output = process.communicate()
+        self.assertEqual(output[0], expected_output)
+        self.assertIsNone(output[1])
+
+    @unittest.skipIf(
+        os.name == "nt",
+        "Windows is currently not supported.",
+    )
+    def test_popen_name(self):
+        expected_output = (
+            b"Python 3.13.2\r\n" if os.name == "nt" else b"Python 3.13.2\n"
+        )
+        process = Popen("python --version", prefix_name=self.env_name, stdout=PIPE)
+        output = process.communicate()
+        self.assertEqual(output[0], expected_output)
+        self.assertIsNone(output[1])
+
+    def test_environment_variable(self):
+        self.assertTrue(
+            "TESTVAR=test"
+            in check_output(
+                "env",
+                prefix_path=self.env_path,
+                env={"TESTVAR": "test"},
+                universal_newlines=True,
+            ).split("\n"),
+        )
+
+    def test_check_output_error(self):
+        with self.assertRaises(CalledProcessError):
+            check_output(
+                "exit 1",
+                prefix_path=self.env_path,
+                universal_newlines=True,
+            )
+
+    def test_check_output_kwargs_error(self):
+        with self.assertRaises(ValueError):
+            check_output(
+                "exit 1",
+                prefix_path=self.env_path,
+                universal_newlines=True,
+                stdout="test.out"
+            )
+
+    def test_check_call_kwargs_error(self):
+        with self.assertRaises(CalledProcessError):
+            check_call(
+                "exit 1",
+                prefix_path=self.env_path,
+                universal_newlines=True,
+            )
+
+    def test_call_timeout(self):
+        with self.assertRaises(TimeoutExpired):
+            call(
+                "sleep 5",
+                timeout=1,
+                prefix_path=self.env_path,
+                universal_newlines=True,
+            )
+
+    def test_run_timeout(self):
+        with self.assertRaises(TimeoutExpired):
+            run(
+                "sleep 5",
+                timeout=1,
+                prefix_path=self.env_path,
+                universal_newlines=True,
+            )
+
+    def test_run_timeout_error(self):
+        with self.assertRaises(ValueError):
+            run(
+                "sleep 5",
+                timeout=1,
+                prefix_path=self.env_path,
+                universal_newlines=True,
+                stdout="test.out",
+                capture_output=True,
+            )
+
+    def test_check_call_timeout(self):
+        with self.assertRaises(CalledProcessError):
+            check_output(
+                "exit 1",
+                prefix_path=self.env_path,
+                universal_newlines=True,
+            )
+
+    def test_environment_variable_run(self):
+        self.assertTrue(
+            "TESTVAR=test"
+            in run(
+                "env",
+                prefix_path=self.env_path,
+                capture_output=True,
+                env={"TESTVAR": "test"},
+                universal_newlines=True,
+            ).stdout.split("\n"),
+        )
