cba22b260927868e0cf68c5859f4cdccae212f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 04:05:51 +0000

    Add package h-ai-brain/0.0.22/h_ai_brain-0.0.22.tar.gz

---
+++ packages/h-ai-brain/h_ai_brain-0.0.22.tar.gz/h_ai_brain-0.0.22/src/h_ai/domain/llm_config.py
@@ -0,0 +1,6 @@
+class LLMConfig:
+    def __init__(self, url: str, model_name: str, temperature: float = 0.6, max_tokens: int = 2500):
+        self.url = url
+        self.model_name = model_name
+        self.temperature = temperature
+        self.max_tokens = max_tokens
