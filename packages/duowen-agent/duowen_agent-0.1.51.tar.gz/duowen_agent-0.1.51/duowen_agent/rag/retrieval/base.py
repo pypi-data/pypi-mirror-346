dca1ba56de3ad2d594b6dd90cfbbaebccffa6600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:12 +0000

    Add package duowen-agent/0.1.51/duowen_agent-0.1.51.tar.gz

---
+++ packages/duowen-agent/duowen_agent-0.1.51.tar.gz/duowen_agent-0.1.51/duowen_agent/rag/retrieval/base.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from typing import Any, List
+
+import numpy as np
+
+from ..models import Document, SearchResult
+
+
+class BaseVector(ABC):
+
+    @abstractmethod
+    def get_backend_type(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def semantic_search(
+        self,
+        query: str,
+        query_embedding: np.ndarray | List[float] = None,
+        **kwargs: Any
+    ) -> list[SearchResult]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def full_text_search(self, query: str, **kwargs: Any) -> list[SearchResult]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_document(self, document: Document, **kwargs: Any):
+        raise NotImplementedError
+
+    @abstractmethod
+    def batch_add_document(
+        self, documents: list[Document], batch_num: int, **kwargs: Any
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_documents_by_ids(self, ids: list[str], **kwargs: Any) -> list[Document]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete_documents_by_ids(self, ids: list[str], **kwargs: Any):
+        raise NotImplementedError
