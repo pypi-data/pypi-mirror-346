ba6f0457016655a2d0965cde32ddeb6423cd1601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 07:07:21 +0000

    Add package SigProfilerAssignment/0.2.2/sigprofilerassignment-0.2.2.tar.gz

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-0.2.2.tar.gz/sigprofilerassignment-0.2.2/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.2.2'
+version = '0.2.2'
+Update = 'v0.2.2: Fix bug in CLI returning non-zero exit code'
+
+    
+    
