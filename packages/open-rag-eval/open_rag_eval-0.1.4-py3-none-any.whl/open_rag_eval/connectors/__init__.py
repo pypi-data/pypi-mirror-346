527c537afbb7c9ce546da85987e174972ba8d062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 08:04:50 +0000

    Add package open-rag-eval/0.1.4/open_rag_eval-0.1.4-py3-none-any.whl

---
+++ packages/open-rag-eval/open_rag_eval-0.1.4-py3-none-any.whl/open_rag_eval/connectors/__init__.py
@@ -0,0 +1,5 @@
+from .connector import Connector
+from .vectara_connector import VectaraConnector
+from .llama_index_connector import LlamaIndexConnector
+
+__all__ = ["Connector", "VectaraConnector", "LlamaIndexConnector"]
