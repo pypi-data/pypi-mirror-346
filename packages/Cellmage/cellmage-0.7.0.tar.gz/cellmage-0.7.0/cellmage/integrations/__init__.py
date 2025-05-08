1a4fa1160cfc743455ee675670233364b14edae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 17:05:52 +0000

    Add package Cellmage/0.7.0/cellmage-0.7.0.tar.gz

---
+++ packages/Cellmage/cellmage-0.7.0.tar.gz/cellmage-0.7.0/cellmage/integrations/__init__.py
@@ -0,0 +1,28 @@
+"""
+Integration modules for CellMage.
+
+This package contains modules that integrate CellMage with other systems.
+"""
+
+# Import the IPython magic modules for easy access
+from . import (
+    base_magic,
+    confluence_magic,
+    github_magic,
+    gitlab_magic,
+    ipython_magic,
+    jira_magic,
+    sqlite_magic,
+    webcontent_magic,
+)
+
+__all__ = [
+    "base_magic",
+    "ipython_magic",
+    "jira_magic",
+    "gitlab_magic",
+    "sqlite_magic",
+    "github_magic",
+    "confluence_magic",
+    "webcontent_magic",
+]
