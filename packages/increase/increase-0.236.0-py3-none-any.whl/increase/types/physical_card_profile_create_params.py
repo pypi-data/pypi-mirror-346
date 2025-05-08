59077845a785ad5f1415319e883c825fb1544acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 17:05:25 +0000

    Add package increase/0.236.0/increase-0.236.0-py3-none-any.whl

---
+++ packages/increase/increase-0.236.0-py3-none-any.whl/increase/types/physical_card_profile_create_params.py
@@ -0,0 +1,24 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, TypedDict
+
+__all__ = ["PhysicalCardProfileCreateParams"]
+
+
+class PhysicalCardProfileCreateParams(TypedDict, total=False):
+    carrier_image_file_id: Required[str]
+    """The identifier of the File containing the physical card's carrier image."""
+
+    contact_phone: Required[str]
+    """A phone number the user can contact to receive support for their card."""
+
+    description: Required[str]
+    """A description you can use to identify the Card Profile."""
+
+    front_image_file_id: Required[str]
+    """The identifier of the File containing the physical card's front image."""
+
+    program_id: Required[str]
+    """The identifier for the Program that this Physical Card Profile falls under."""
