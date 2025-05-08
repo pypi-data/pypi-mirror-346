ffdc1fd179040db99f4e6d6b0bbdb50288bc590f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:24 +0000

    Add package docutranslate/0.0.1/docutranslate-0.0.1.tar.gz

---
+++ packages/docutranslate/docutranslate-0.0.1.tar.gz/docutranslate-0.0.1/tests/test1.py
@@ -0,0 +1,11 @@
+from docutranslate.translater import FileTranslater
+
+# translater = FileTranslater(base_url="https://open.bigmodel.cn/api/paas/v4",
+#                             key="969ba51b61914cc2b710d1393dca1a3c.hSuATex5IoNVZNGu",
+#                             model_id="glm-4-flashx")
+translater = FileTranslater(base_url="http://127.0.0.1:1234/v1",
+                            key="969ba51b61914cc2b710d1393dca1a3c.hSuATex5IoNVZNGu",
+                            model_id="qwen3-30b-a3b-128k")
+translater.translate_markdown_file(r"C:\Users\jxgm\Desktop\FileTranslate\tests\resource\regex.md",
+                                   to_lang="中文",
+                                   output_format="html")
