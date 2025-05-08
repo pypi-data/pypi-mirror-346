ceccac7b62f5c336edcac75eb83c7844e71f3472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 04:06:03 +0000

    Add package photoshop-python-api/0.24.1/photoshop_python_api-0.24.1.tar.gz

---
+++ packages/photoshop-python-api/photoshop_python_api-0.24.1.tar.gz/photoshop_python_api-0.24.1/photoshop/api/constants.py
@@ -0,0 +1,12 @@
+# The Photoshop version to COM program ID mappings.
+PHOTOSHOP_VERSION_MAPPINGS = {
+    "2025": "190",
+    "2024": "180",
+    "2023": "170",
+    "2022": "160",
+    "2021": "150",
+    "2020": "140",
+    "2019": "130",
+    "2018": "120",
+    "2017": "110",
+}
