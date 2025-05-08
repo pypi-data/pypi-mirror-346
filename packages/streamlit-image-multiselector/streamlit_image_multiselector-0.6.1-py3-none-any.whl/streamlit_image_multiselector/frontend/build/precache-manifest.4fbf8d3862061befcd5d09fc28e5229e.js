557de3dfa4f795bbbc460a67cf7dfeb253bca2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:04 +0000

    Add package streamlit-image-multiselector/0.6.1/streamlit_image_multiselector-0.6.1-py3-none-any.whl

---
+++ packages/streamlit-image-multiselector/streamlit_image_multiselector-0.6.1-py3-none-any.whl/streamlit_image_multiselector/frontend/build/precache-manifest.4fbf8d3862061befcd5d09fc28e5229e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc9c5da2a44d63b51532917319b8ef1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24de3dcc5e92885a1761",
+    "url": "./static/js/2.94a27f97.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.94a27f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8972a333194e8ffe8afc",
+    "url": "./static/js/main.c396fd5a.chunk.js"
+  },
+  {
+    "revision": "18300b1ffba716d884c2",
+    "url": "./static/js/runtime-main.58369df8.js"
+  }
+]);
