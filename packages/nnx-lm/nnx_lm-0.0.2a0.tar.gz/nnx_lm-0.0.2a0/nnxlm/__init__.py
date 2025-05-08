0834b99921b935b24b717d1c13b4c8ad4ddbe68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:46 +0000

    Add package nnx-lm/0.0.2a0/nnx_lm-0.0.2a0.tar.gz

---
+++ packages/nnx-lm/nnx_lm-0.0.2a0.tar.gz/nnx_lm-0.0.2a0/nnxlm/__init__.py
@@ -0,0 +1,7 @@
+from .main import load
+from .utils import generate
+from .qwen3 import Qwen3ForCausalLM
+from .qwen2 import Qwen2ForCausalLM
+from .glm4 import Glm4ForCausalLM
+from .granite import GraniteForCausalLM
+from .llama import LlamaForCausalLM
