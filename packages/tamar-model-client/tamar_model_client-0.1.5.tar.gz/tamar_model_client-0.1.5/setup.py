044b625b90dce2cc7b85a7da6f76e18caeb68df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:24 +0000

    Add package tamar-model-client/0.1.5/tamar_model_client-0.1.5.tar.gz

---
+++ packages/tamar-model-client/tamar_model_client-0.1.5.tar.gz/tamar_model_client-0.1.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tamar-model-client",
+    version="0.1.5",
+    description="A Python SDK for interacting with the Model Manager gRPC service",
+    author="Oscar Ou",
+    author_email="oscar.ou@tamaredge.ai",
+    packages=find_packages(),
+    include_package_data=True,  # 包含非 .py 文件
+    package_data={
+        "tamar_model_client": ["generated/*.py"],  # 包含 gRPC 生成文件
+    },
+    install_requires=[
+        "grpcio",
+        "grpcio-tools",
+        "pydantic",
+        "PyJWT",
+        "nest_asyncio",
+        "openai",
+        "google-genai",
+    ],
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="http://gitlab.tamaredge.top/project-tap/AgentOS/model-manager-client",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
