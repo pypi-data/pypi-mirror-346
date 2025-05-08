53d035e7f37ea48db92f756b50b9e798be0d1644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 19:04:46 +0000

    Add package Cellmage/0.8.1/cellmage-0.8.1-py3-none-any.whl

---
+++ packages/Cellmage/cellmage-0.8.1-py3-none-any.whl/cellmage/integrations/__init__.py
@@ -0,0 +1,30 @@
+"""
+Integration modules for CellMage.
+
+This package contains modules that integrate CellMage with other systems.
+"""
+
+# Import the IPython magic modules for easy access
+from . import (
+    base_magic,
+    confluence_magic,
+    gdocs_magic,
+    github_magic,
+    gitlab_magic,
+    ipython_magic,
+    jira_magic,
+    sqlite_magic,
+    webcontent_magic,
+)
+
+__all__ = [
+    "base_magic",
+    "ipython_magic",
+    "jira_magic",
+    "gitlab_magic",
+    "sqlite_magic",
+    "github_magic",
+    "confluence_magic",
+    "webcontent_magic",
+    "gdocs_magic",
+]
