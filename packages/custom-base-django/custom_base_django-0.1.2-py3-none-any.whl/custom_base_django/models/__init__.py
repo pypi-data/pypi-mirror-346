b7df3bf356c29498eedd35dcf1eb3f5aa9419ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 05:04:40 +0000

    Add package custom-base-django/0.1.2/custom_base_django-0.1.2-py3-none-any.whl

---
+++ packages/custom-base-django/custom_base_django-0.1.2-py3-none-any.whl/custom_base_django/models/__init__.py
@@ -0,0 +1,3 @@
+from custom_base_django.models.base import BaseModelFiscalDelete, BaseModelWitDateNotFiscalDelete, CustomRelatedNameManager, \
+    BaseModelNotFiscalDelete, Select2FormChoices, CustomManageFiscalDelete, CustomGenericRelation
+from custom_base_django.models.choices import  Choice as Choice, ChoiceForeignKey
