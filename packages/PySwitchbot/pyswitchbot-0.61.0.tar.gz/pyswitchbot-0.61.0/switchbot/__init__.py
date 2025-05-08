128d6b12b2fca22aa06f7c54f797365b00d79b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 14:05:32 +0000

    Add package PySwitchbot/0.61.0/pyswitchbot-0.61.0.tar.gz

---
+++ packages/PySwitchbot/pyswitchbot-0.61.0.tar.gz/pyswitchbot-0.61.0/switchbot/__init__.py
@@ -0,0 +1,75 @@
+"""Library to handle connection with Switchbot."""
+
+from __future__ import annotations
+
+from bleak_retry_connector import (
+    close_stale_connections,
+    close_stale_connections_by_address,
+    get_device,
+)
+
+from .adv_parser import SwitchbotSupportedType, parse_advertisement_data
+from .const import (
+    FanMode,
+    LockStatus,
+    SwitchbotAccountConnectionError,
+    SwitchbotApiError,
+    SwitchbotAuthenticationError,
+    SwitchbotModel,
+)
+from .devices.base_light import SwitchbotBaseLight
+from .devices.blind_tilt import SwitchbotBlindTilt
+from .devices.bot import Switchbot
+from .devices.bulb import SwitchbotBulb
+from .devices.ceiling_light import SwitchbotCeilingLight
+from .devices.curtain import SwitchbotCurtain
+from .devices.device import ColorMode, SwitchbotDevice, SwitchbotEncryptedDevice
+from .devices.evaporative_humidifier import SwitchbotEvaporativeHumidifier
+from .devices.fan import SwitchbotFan
+from .devices.humidifier import SwitchbotHumidifier
+from .devices.light_strip import SwitchbotLightStrip
+from .devices.lock import SwitchbotLock
+from .devices.plug import SwitchbotPlugMini
+from .devices.relay_switch import SwitchbotRelaySwitch
+from .devices.roller_shade import SwitchbotRollerShade
+from .devices.vacuum import SwitchbotVacuum
+from .discovery import GetSwitchbotDevices
+from .models import SwitchBotAdvertisement
+
+__all__ = [
+    "ColorMode",
+    "FanMode",
+    "GetSwitchbotDevices",
+    "LockStatus",
+    "SwitchBotAdvertisement",
+    "Switchbot",
+    "Switchbot",
+    "SwitchbotAccountConnectionError",
+    "SwitchbotApiError",
+    "SwitchbotAuthenticationError",
+    "SwitchbotBaseLight",
+    "SwitchbotBlindTilt",
+    "SwitchbotBulb",
+    "SwitchbotCeilingLight",
+    "SwitchbotCurtain",
+    "SwitchbotDevice",
+    "SwitchbotEncryptedDevice",
+    "SwitchbotEvaporativeHumidifier",
+    "SwitchbotFan",
+    "SwitchbotHumidifier",
+    "SwitchbotLightStrip",
+    "SwitchbotLock",
+    "SwitchbotModel",
+    "SwitchbotModel",
+    "SwitchbotPlugMini",
+    "SwitchbotPlugMini",
+    "SwitchbotRelaySwitch",
+    "SwitchbotRollerShade",
+    "SwitchbotSupportedType",
+    "SwitchbotSupportedType",
+    "SwitchbotVacuum",
+    "close_stale_connections",
+    "close_stale_connections_by_address",
+    "get_device",
+    "parse_advertisement_data",
+]
