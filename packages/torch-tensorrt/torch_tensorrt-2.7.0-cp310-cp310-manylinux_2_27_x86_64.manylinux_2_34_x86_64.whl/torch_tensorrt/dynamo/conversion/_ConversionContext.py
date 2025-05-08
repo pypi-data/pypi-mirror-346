4cb003b3c151589b43c5513ded2b28d02aaa9fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 14:05:19 +0000

    Add package torch-tensorrt/2.7.0/torch_tensorrt-2.7.0-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_34_x86_64.whl

---
+++ packages/torch-tensorrt/torch_tensorrt-2.7.0-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_34_x86_64.whl/torch_tensorrt/dynamo/conversion/_ConversionContext.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+
+import numpy as np
+from torch_tensorrt.dynamo._settings import CompilationSettings
+from torch_tensorrt.fx.types import TRTNetwork
+
+
+@dataclass
+class ConversionContext:
+    """Class representing the context for conversion of a particular network
+
+    Args:
+        net: TensorRT Network being built
+        compilation_settings: Settings selected by the user for compilation
+        requires_output_allocator: Boolean flag indicating if the converter creates operators which require an Output Allocator to run (e.g. data dependent operators)
+    """
+
+    net: TRTNetwork
+    compilation_settings: CompilationSettings = field(
+        default_factory=CompilationSettings
+    )
+    requires_output_allocator: bool = False
+    mapping: dict[str, np.array] = field(default_factory=dict)
