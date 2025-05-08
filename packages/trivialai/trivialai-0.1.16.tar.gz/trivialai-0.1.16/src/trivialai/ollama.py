ea7b0d691fd3eb1fddd89d0e450e317be33ab2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 07:07:07 +0000

    Add package trivialai/0.1.16/trivialai-0.1.16.tar.gz

---
+++ packages/trivialai/trivialai-0.1.16.tar.gz/trivialai-0.1.16/src/trivialai/ollama.py
@@ -0,0 +1,34 @@
+import re
+
+import requests
+
+from .filesystem import FilesystemMixin
+from .llm import LLMMixin, LLMResult
+
+
+class Ollama(LLMMixin, FilesystemMixin):
+    def __init__(self, model, ollama_server):
+        self.model = model
+        self.server = ollama_server.rstrip("/")
+
+    def generate(self, system, prompt):
+        res = requests.post(
+            f"{self.server}/api/generate",
+            json={
+                "model": self.model,
+                "stream": False,
+                "prompt": f"SYSTEM PROMPT: {system} PROMPT: {prompt}",
+            },
+        )
+        if res.status_code == 200:
+            resp = res.json()["response"].strip()
+            pattern = r"<think>.*?</think>"
+            think_match = re.search(pattern, resp, re.DOTALL)
+            if not think_match:
+                return LLMResult(res, resp, None)
+
+            matched = think_match.group(0)
+            scratchpad = matched[7:-8].strip()
+            content = resp.content.replace(matched, "").strip()
+            return LLMResult(res, content, scratchpad)
+        return LLMResult(res, None, None)
