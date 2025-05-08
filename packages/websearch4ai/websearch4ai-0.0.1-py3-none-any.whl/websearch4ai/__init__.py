f1bf25030788a0199738f081082a7ab14833f09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 05:04:23 +0000

    Add package websearch4ai/0.0.1/websearch4ai-0.0.1-py3-none-any.whl

---
+++ packages/websearch4ai/websearch4ai-0.0.1-py3-none-any.whl/websearch4ai/__init__.py
@@ -0,0 +1,3 @@
+from .core import search
+
+__version__ = "0.1.0"
---
+++ packages/websearch4ai/websearch4ai-0.0.1-py3-none-any.whl/websearch4ai/core.py
@@ -0,0 +1,5 @@
+def search(query: str) -> str:
+    """
+    Placeholder search function
+    """
+    return f"Future implementation will search: {query}"
