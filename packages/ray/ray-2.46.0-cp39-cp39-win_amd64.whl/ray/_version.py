3fb758b1b4daa03705d56ac6431bb7478ca25ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 08:05:40 +0000

    Add package ray/2.46.0/ray-2.46.0-cp39-cp39-win_amd64.whl

---
+++ packages/ray/ray-2.46.0-cp39-cp39-win_amd64.whl/ray/_version.py
@@ -0,0 +1,6 @@
+# Replaced with the current commit when building the wheels.
+commit = "c3dd2ca0c2a24ddf327a213d2e936bd4eaa4ca0a"
+version = "2.46.0"
+
+if __name__ == "__main__":
+    print("%s %s" % (version, commit))
