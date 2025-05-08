42ccfd77cc5893eea65938e99673922571c4a377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 19:04:52 +0000

    Add package fogis-api-client-timmyBird/0.4.5/fogis_api_client_timmybird-0.4.5.tar.gz

---
+++ packages/fogis-api-client-timmyBird/fogis_api_client_timmybird-0.4.5.tar.gz/fogis_api_client_timmybird-0.4.5/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fogis-api-client-timmyBird",
+    version="0.4.5",
+    author="Bartek Svaberg",
+    author_email="bartek.svaberg@gmail.com",
+    description="A Python client for the FOGIS API (Svensk Fotboll)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timmyBird/fogis-api-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "flask",
+        "apispec>=6.0.0",
+        "flask-swagger-ui",
+        "marshmallow>=3.26.0",
+        "psutil",
+        "jsonschema>=4.17.3",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-mock",
+            "pytest-cov",
+            "flake8",
+            "flask-cors",
+        ],
+    },
+    include_package_data=True,
+)
