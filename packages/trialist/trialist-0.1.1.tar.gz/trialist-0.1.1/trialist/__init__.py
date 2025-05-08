9157e9b07f3d8343ac7ddad4b786112225b03398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 14:05:28 +0000

    Add package trialist/0.1.1/trialist-0.1.1.tar.gz

---
+++ packages/trialist/trialist-0.1.1.tar.gz/trialist-0.1.1/trialist/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2025-present Yiannis Charalambous <yiannis128@hotmail.com>
+#
+# SPDX-License-Identifier: GPLv3
+
+from trialist.experiment import Checkpoint, Experiment, Trials
+
+__all__ = [
+    "Checkpoint",
+    "Experiment",
+    "Trials",
+]
