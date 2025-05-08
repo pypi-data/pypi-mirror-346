008f19c0cb562e22747b013fba8d485dcb7c8404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:39 +0000

    Add package plutorcon/0.0.4/plutorcon-0.0.4.tar.gz

---
+++ packages/plutorcon/plutorcon-0.0.4.tar.gz/plutorcon-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plutorcon',
+    author='ReuS',
+    version='0.0.4',
+    packages=find_packages(),
+    description='Python RCON client for Plutonium',
+)
