3a5bfdd25be9d700e891f63354476e814c601c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:43 +0000

    Add package arc-int/0.0.1/arc-int-0.0.1.tar.gz

---
+++ packages/arc-int/arc-int-0.0.1.tar.gz/arc-int-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="arc-int",
+    version="0.0.1",
+    description="arc-int",
+    author="Anish Agrawal",
+    author_email="your.email@example.com",
+    packages=["arc-int"],
+)
