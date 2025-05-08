a93f31ce6c356a6610059899e2ee2cac1261c561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 08:05:15 +0000

    Add package crypticorn/2.8.0/crypticorn-2.8.0.tar.gz

---
+++ packages/crypticorn/crypticorn-2.8.0.tar.gz/crypticorn-2.8.0/crypticorn/common/router/status_router.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Literal
+from fastapi import APIRouter, Request
+
+router = APIRouter(tags=["Status"], prefix="")
+
+
+@router.get("/", operation_id="ping")
+async def ping(request: Request) -> dict:
+    """
+    Returns 'OK' if the API is running.
+    """
+    return {"origin": request.headers.get("x-forwarded-for")}
+
+
+@router.get("/time", operation_id="getTime")
+async def time(type: Literal["iso", "unix"] = "iso") -> str:
+    """
+    Returns the current time in either ISO or Unix timestamp (seconds) format.
+    """
+    if type == "iso":
+        return datetime.now().isoformat()
+    elif type == "unix":
+        return str(int(datetime.now().timestamp()))
