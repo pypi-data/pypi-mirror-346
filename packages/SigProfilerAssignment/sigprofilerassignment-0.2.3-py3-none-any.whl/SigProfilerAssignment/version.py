4be430662035b349f199a409b10588a4ca086fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 08:05:18 +0000

    Add package SigProfilerAssignment/0.2.3/sigprofilerassignment-0.2.3-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-0.2.3-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.2.3'
+version = '0.2.3'
+Update = 'v0.2.3:  Improved rounding strategy for handling low mutation counts'
+
+    
