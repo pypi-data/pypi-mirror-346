f8391425ddb8f912e24e3b2c0569e62dafc6b73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 19:04:48 +0000

    Add package xyz-tools-plus/0.1.1/xyz_tools_plus-0.1.1.tar.gz

---
+++ packages/xyz-tools-plus/xyz_tools_plus-0.1.1.tar.gz/xyz_tools_plus-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='xyz_tools_plus',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+
+    author='mly, wyy, sty',
+    description='一个用于多功能的Python库',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
