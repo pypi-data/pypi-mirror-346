2ce2ab0b4f52a9b2d716efb46e34988be8daf726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:16 +0000

    Add package flame-hub-client/0.2.0/flame_hub_client-0.2.0-py3-none-any.whl

---
+++ packages/flame-hub-client/flame_hub_client-0.2.0-py3-none-any.whl/flame_hub/types.py
@@ -0,0 +1,3 @@
+__all__ = ["PageParams", "SortParams", "FilterOperator", "FilterParams", "IncludeParams"]
+
+from ._base_client import PageParams, SortParams, FilterParams, FilterOperator, IncludeParams
