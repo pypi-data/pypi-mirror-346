5e665891d683f7a0beee245dc26312798a27bf2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 19:04:44 +0000

    Add package zoz-requests/2.32.3/zoz_requests-2.32.3-py3-none-any.whl

---
+++ packages/zoz-requests/zoz_requests-2.32.3-py3-none-any.whl/requests/__version__.py
@@ -0,0 +1,14 @@
+# .-. .-. .-. . . .-. .-. .-. .-.
+# |(  |-  |.| | | |-  `-.  |  `-.
+# ' ' `-' `-`.`-' `-' `-'  '  `-'
+
+__title__ = "zoz-requests"
+__description__ = "Python HTTP for Humans."
+__url__ = ""
+__version__ = "2.32.3"
+__build__ = 0x023203
+__author__ = ""
+__author_email__ = ""
+__license__ = "Apache-2.0"
+__copyright__ = ""
+__cake__ = "\u2728 \U0001f370 \u2728"
