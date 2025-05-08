1f692475a1e272e704da644594f57e01ed912943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 05:05:16 +0000

    Add package passagemath-fricas/10.5.31/passagemath_fricas-10.5.31-cp312-cp312-macosx_14_0_arm64.whl

---
+++ packages/passagemath-fricas/passagemath_fricas-10.5.31-cp312-cp312-macosx_14_0_arm64.whl/sage_wheels/lib/fricas/target/aarch64-apple-darwin23.6.0/algebra/STRTBL.c
@@ -0,0 +1,7 @@
+/*      Compiler: ECL 24.5.10                                         */
+/*      Date: 2025/5/7 11:49 (yyyy/mm/dd)                             */
+/*      Machine: Darwin 23.6.0 arm64                                  */
+/*      Source: /Users/runner/sage-local/var/tmp/sage/build/fricas-1.3.11/src/pre-generated/src/algebra/STRTBL.lsp */
+#include <ecl/ecl-cmp.h>
+#include "/Users/runner/sage-local/var/tmp/sage/build/fricas-1.3.11/src/target/aarch64-apple-darwin23.6.0/algebra/STRTBL.eclh"
+/*      function definition for StringTable                           */
