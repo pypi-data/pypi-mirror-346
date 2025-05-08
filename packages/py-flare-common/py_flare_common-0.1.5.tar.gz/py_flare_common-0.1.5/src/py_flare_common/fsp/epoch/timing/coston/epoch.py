38e854b5a02bdb93672af57dc1ef8d2ab578464d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 19:04:55 +0000

    Add package py-flare-common/0.1.5/py_flare_common-0.1.5.tar.gz

---
+++ packages/py-flare-common/py_flare_common-0.1.5.tar.gz/py_flare_common-0.1.5/src/py_flare_common/fsp/epoch/timing/coston/epoch.py
@@ -0,0 +1,34 @@
+from ...epoch import RewardEpoch, VotingEpoch
+from ...factory import RewardEpochFactory, VotingEpochFactory
+from ..config import coston_chain_config
+
+__all__ = [
+    "voting_epoch",
+    "reward_epoch",
+    "voting_epoch_factory",
+    "reward_epoch_factory",
+]
+
+voting_epoch_factory = VotingEpochFactory(
+    first_epoch_epoc=coston_chain_config.voting_first_epoch_epoc,
+    epoch_duration=coston_chain_config.voting_epoch_duration,
+    ftso_reveal_deadline=coston_chain_config.voting_ftso_reveal_deadline,
+    reward_first_epoch_epoc=coston_chain_config.reward_first_epoch_epoc,
+    reward_epoch_duration=coston_chain_config.reward_epoch_duration,
+)
+
+reward_epoch_factory = RewardEpochFactory(
+    first_epoch_epoc=coston_chain_config.reward_first_epoch_epoc,
+    epoch_duration=coston_chain_config.reward_epoch_duration,
+    voting_first_epoch_epoc=coston_chain_config.voting_first_epoch_epoc,
+    voting_epoch_duration=coston_chain_config.voting_epoch_duration,
+    voting_ftso_reveal_deadline=coston_chain_config.voting_ftso_reveal_deadline,
+)
+
+
+def voting_epoch(id: int) -> VotingEpoch:
+    return voting_epoch_factory.make_epoch(id)
+
+
+def reward_epoch(id: int) -> RewardEpoch:
+    return reward_epoch_factory.make_epoch(id)
