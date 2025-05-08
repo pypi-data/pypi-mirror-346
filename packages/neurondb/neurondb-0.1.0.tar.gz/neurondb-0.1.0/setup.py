a88f9183b96a3019d7be79f8d4a573103c19e67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:10 +0000

    Add package neurondb/0.1.0/neurondb-0.1.0.tar.gz

---
+++ packages/neurondb/neurondb-0.1.0.tar.gz/neurondb-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neurondb",
+    version="0.1.0",
+    description="A brain-inspired, property-graph database in Python.",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+) 
