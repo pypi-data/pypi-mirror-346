65c0278571c4f123dbfdf563e840fc88090249f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 14:05:53 +0000

    Add package ttable/0.7.0/ttable-0.7.0-py3-none-any.whl

---
+++ packages/ttable/ttable-0.7.0-py3-none-any.whl/tt/transformations/__init__.py
@@ -0,0 +1,24 @@
+"""Interfaces for transforming representations of expressions."""
+
+from .bexpr import (
+    apply_de_morgans,
+    apply_idempotent_law,
+    apply_identity_law,
+    apply_inverse_law,
+    distribute_ands,
+    distribute_ors,
+    coalesce_negations,
+    to_cnf,
+    to_primitives,
+)
+
+from .utils import (
+    AbstractTransformationModifier,
+    ComposedTransformation,
+    ensure_bexpr,
+    forever,
+    RepeatableAction,
+    repeat,
+    twice,
+    tt_compose,
+)
