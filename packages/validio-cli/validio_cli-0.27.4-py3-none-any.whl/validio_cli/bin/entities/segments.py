881e98097f5707911565af86d74a87ffc650d7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:27 +0000

    Add package validio-cli/0.27.4/validio_cli-0.27.4-py3-none-any.whl

---
+++ packages/validio-cli/validio_cli-0.27.4-py3-none-any.whl/validio_cli/bin/entities/segments.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass
+
+import typer
+
+from validio_cli import (
+    AsyncTyper,
+    ConfigDir,
+    Identifier,
+    Namespace,
+    OutputFormat,
+    OutputFormatOption,
+    get_client,
+    output_json,
+    output_text,
+)
+from validio_cli.namespace import get_namespace
+
+app = AsyncTyper(help="Segments for a segmentation")
+
+
+@app.async_command(help="Get segment")
+async def get(
+    config_dir: str = ConfigDir,
+    output_format: OutputFormat = OutputFormatOption,
+    namespace: str = Namespace(),
+    identifier: str = Identifier,
+    max_fields: int = typer.Option(3, help="Maximum number of fields to show"),
+) -> None:
+    vc, cfg = get_client(config_dir)
+
+    segments = await vc.get_segments(
+        segmentation_id=identifier,
+        namespace_id=get_namespace(namespace, cfg),
+    )
+
+    if output_format == OutputFormat.JSON:
+        return output_json(segments)
+
+    @dataclass
+    class SegmentOutput:
+        id: str
+        muted: bool
+        field_and_value: str
+
+    resources = []
+    for segment in segments:
+        field_and_value = []
+        for f in segment["fields"]:
+            field_and_value.append(f"{f['field']}={f['value']}")
+
+        additional = ""
+        if len(field_and_value) > max_fields:
+            additional = f" and {len(field_and_value) - max_fields} more"
+            field_and_value = field_and_value[:max_fields]
+
+        resources.append(
+            SegmentOutput(
+                id=segment["id"],
+                muted=segment["muted"],
+                field_and_value=", ".join(field_and_value) + additional,
+            )
+        )
+
+    return output_text(
+        resources,
+        fields={
+            "id": None,
+            "muted": None,
+            "field_and_value": None,
+        },
+    )
+
+
+if __name__ == "__main__":
+    typer.run(app())
