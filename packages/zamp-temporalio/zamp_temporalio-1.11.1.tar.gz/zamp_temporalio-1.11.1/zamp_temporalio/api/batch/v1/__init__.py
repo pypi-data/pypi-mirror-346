c5010a688cc4a51bcebc5edaccff65a718602de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:40 +0000

    Add package zamp-temporalio/1.11.1/zamp_temporalio-1.11.1.tar.gz

---
+++ packages/zamp-temporalio/zamp_temporalio-1.11.1.tar.gz/zamp_temporalio-1.11.1/zamp_temporalio/api/batch/v1/__init__.py
@@ -0,0 +1,21 @@
+from .message_pb2 import (
+    BatchOperationCancellation,
+    BatchOperationDeletion,
+    BatchOperationInfo,
+    BatchOperationReset,
+    BatchOperationSignal,
+    BatchOperationTermination,
+    BatchOperationUnpauseActivities,
+    BatchOperationUpdateWorkflowExecutionOptions,
+)
+
+__all__ = [
+    "BatchOperationCancellation",
+    "BatchOperationDeletion",
+    "BatchOperationInfo",
+    "BatchOperationReset",
+    "BatchOperationSignal",
+    "BatchOperationTermination",
+    "BatchOperationUnpauseActivities",
+    "BatchOperationUpdateWorkflowExecutionOptions",
+]
