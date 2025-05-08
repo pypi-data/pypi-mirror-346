8b01bf055591a1710209c900d8020f54a0b773d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 17:05:05 +0000

    Add package toga-web/0.5.1/toga_web-0.5.1-py3-none-any.whl

---
+++ packages/toga-web/toga_web-0.5.1-py3-none-any.whl/toga_web/widgets/button.py
@@ -0,0 +1,35 @@
+from toga_web.libs import create_proxy
+
+from .base import Widget
+
+
+class Button(Widget):
+    def create(self):
+        self.native = self._create_native_widget("sl-button")
+        self.native.addEventListener("onclick", create_proxy(self.dom_onclick))
+
+    def dom_onclick(self, event):
+        self.interface.on_press()
+
+    def get_text(self):
+        return self.native.innerHTML
+
+    def set_text(self, text):
+        self.native.innerHTML = text
+
+    def get_icon(self):
+        return self._icon
+
+    def set_icon(self, icon):
+        self._icon = icon
+        if icon:
+            self.interface.factory.not_implemented("Button.icon")
+
+    def set_enabled(self, value):
+        self.native.disabled = not value
+
+    def set_background_color(self, value):
+        pass
+
+    def rehint(self):
+        pass
