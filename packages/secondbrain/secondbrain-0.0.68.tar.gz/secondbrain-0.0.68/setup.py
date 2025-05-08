6dae691cb8d0e767d54cbe86bfbda2d9fbbcf289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 04:06:08 +0000

    Add package secondbrain/0.0.68/secondbrain-0.0.68.tar.gz

---
+++ packages/secondbrain/secondbrain-0.0.68.tar.gz/secondbrain-0.0.68/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="secondbrain",
+    version="0.0.68",
+    packages=find_packages(),
+    install_requires=[],
+    author="Jie Xiong",
+    author_email="363692146@qq.com",
+    description="Package for secondbrain only!",
+    long_description=open('README.md', encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_data={
+        'secondbrain.bot': ['prompt.jinja'],
+    },
+)
