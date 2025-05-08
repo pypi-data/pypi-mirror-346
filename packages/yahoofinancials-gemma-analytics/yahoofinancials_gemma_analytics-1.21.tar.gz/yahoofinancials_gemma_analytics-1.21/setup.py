9a84e8532031b41b040e0031e314e43a9a505130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 04:05:56 +0000

    Add package yahoofinancials-gemma-analytics/1.21/yahoofinancials_gemma_analytics-1.21.tar.gz

---
+++ packages/yahoofinancials-gemma-analytics/yahoofinancials_gemma_analytics-1.21.tar.gz/yahoofinancials_gemma_analytics-1.21/setup.py
@@ -0,0 +1,49 @@
+import codecs
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='yahoofinancials_gemma_analytics',
+    version='1.21',
+    description='A powerful financial data module used for pulling both fundamental and technical data from Yahoo Finance',
+    long_description=long_description,
+    url='https://github.com/JECSand/yahoofinancials',
+    download_url='https://github.com/JECSand/yahoofinancials/archive/1.20.tar.gz',
+    author='Connor Sanders',
+    author_email='connor@exceleri.com',
+    license='MIT',
+    keywords=['finance data', 'stocks', 'commodities', 'cryptocurrencies', 'currencies', 'forex', 'yahoo finance'],
+    packages=['yahoofinancials'],
+    install_requires=[
+        "pytz>=2022.5",
+        "requests>=2.31",
+        "appdirs>=1.4.4",
+        "frozendict>=2.3.4",
+        "peewee>=3.16.2",
+        "beautifulsoup4>=4.11.1",
+        "lxml>=4.9.1",
+        "curl_cffi",
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    zip_safe=False
+)
