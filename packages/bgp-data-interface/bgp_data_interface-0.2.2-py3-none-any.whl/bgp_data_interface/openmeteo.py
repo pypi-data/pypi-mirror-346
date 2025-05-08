3204756f7611fa786641476830321a7c8eccb75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:13 +0000

    Add package bgp-data-interface/0.2.2/bgp_data_interface-0.2.2-py3-none-any.whl

---
+++ packages/bgp-data-interface/bgp_data_interface-0.2.2-py3-none-any.whl/bgp_data_interface/openmeteo.py
@@ -0,0 +1,16 @@
+from openmeteo_api.openmeteo_api import OpenmeteoAPI
+import pandas as pd
+from typing import Dict, Any
+
+class Openmeteo:
+
+    _om: OpenmeteoAPI
+
+    def __init__(self) -> None:
+        self._om = OpenmeteoAPI()
+
+    def forecast(self, params: Dict[str, Any]) -> pd.DataFrame:
+        return self._om.forecast(params)
+
+    def historical(self, params: Dict[str, Any]) -> pd.DataFrame:
+        return self._om.historical(params)
