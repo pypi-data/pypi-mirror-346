358dd68f72d5e0a0f3354d74c6a3f59b99b5a402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:18 +0000

    Add package mlymath/0.4.2/mlymath-0.4.2.tar.gz

---
+++ packages/mlymath/mlymath-0.4.2.tar.gz/mlymath-0.4.2/hsah/__init__.py
@@ -0,0 +1,13 @@
+from .hash import (
+    sha256,
+    sha3_256,
+    sha3_512,
+    hash_help
+)
+
+__all__ = [
+    'sha256',
+    'sha3_256',
+    'sha3_512',
+    'hash_help'
+]
---
+++ packages/mlymath/mlymath-0.4.2.tar.gz/mlymath-0.4.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='mlymath',
+    version='0.4.2',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+
+    author='mly',
+    description='一个用于数学计算的Python库',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
