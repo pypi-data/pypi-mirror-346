90d759aa1aa2b35f8461e92f740cadef70df2e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 17:05:11 +0000

    Add package capymoa/0.9.1/capymoa-0.9.1-py3-none-any.whl

---
+++ packages/capymoa/capymoa-0.9.1-py3-none-any.whl/capymoa/anomaly/__init__.py
@@ -0,0 +1,9 @@
+from ._half_space_trees import HalfSpaceTrees
+from ._online_isolation_forest import OnlineIsolationForest
+from ._autoencoder import Autoencoder
+
+__all__ = [
+    "HalfSpaceTrees",
+    "OnlineIsolationForest",
+    "Autoencoder",
+]
---
+++ packages/capymoa/capymoa-0.9.1-py3-none-any.whl/capymoa/evaluation/__init__.py
@@ -0,0 +1,32 @@
+from .evaluation import (
+    prequential_evaluation,
+    prequential_evaluation_multiple_learners,
+    prequential_ssl_evaluation,
+    prequential_evaluation_anomaly,
+    ClassificationEvaluator,
+    ClassificationWindowedEvaluator,
+    RegressionWindowedEvaluator,
+    RegressionEvaluator,
+    PredictionIntervalEvaluator,
+    PredictionIntervalWindowedEvaluator,
+    AnomalyDetectionEvaluator,
+    ClusteringEvaluator,
+)
+from . import results, ocl
+
+__all__ = [
+    "prequential_evaluation",
+    "prequential_ssl_evaluation",
+    "prequential_evaluation_multiple_learners",
+    "prequential_evaluation_anomaly",
+    "ClassificationEvaluator",
+    "ClassificationWindowedEvaluator",
+    "RegressionWindowedEvaluator",
+    "RegressionEvaluator",
+    "PredictionIntervalEvaluator",
+    "PredictionIntervalWindowedEvaluator",
+    "AnomalyDetectionEvaluator",
+    "ClusteringEvaluator",
+    "results",
+    "ocl",
+]
