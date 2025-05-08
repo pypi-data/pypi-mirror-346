666e197a5e804e10a518e938d98478c186637bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:42 +0000

    Add package languagesda/2.1/languagesda-2.1.tar.gz

---
+++ packages/languagesda/languagesda-2.1.tar.gz/languagesda-2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='languagesda',
+    version='2.1',
+    packages = ['languagesda'],
+    install_requires=['os', 'tkinter'],
+    python_requires=">=3.11",
+    author="development",
+    author_email="agcs-development@rambler.ru"
+)
