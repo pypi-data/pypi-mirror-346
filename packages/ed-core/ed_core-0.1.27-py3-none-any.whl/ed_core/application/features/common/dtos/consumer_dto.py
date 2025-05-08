fb3cc7cc2d29053fa64ea81bfd5b41a690a6d365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:37 +0000

    Add package ed-core/0.1.27/ed_core-0.1.27-py3-none-any.whl

---
+++ packages/ed-core/ed_core-0.1.27-py3-none-any.whl/ed_core/application/features/common/dtos/consumer_dto.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from ed_domain.core.entities.consumer import Consumer
+from ed_domain.core.repositories.abc_unit_of_work import ABCUnitOfWork
+from pydantic import BaseModel
+
+from ed_core.application.features.common.dtos import LocationDto
+
+
+class ConsumerDto(BaseModel):
+    first_name: str
+    last_name: str
+    phone_number: str
+    email: Optional[str]
+    location: LocationDto
+
+    @classmethod
+    def from_consumer(cls, consumer: Consumer, uow: ABCUnitOfWork) -> "ConsumerDto":
+        location = uow.location_repository.get(id=consumer["location_id"])
+        assert location is not None, "Location not found"
+
+        return cls(
+            first_name=consumer["first_name"],
+            last_name=consumer["last_name"],
+            phone_number=consumer["phone_number"],
+            email=consumer["email"],
+            location=LocationDto.from_location(location),
+        )
---
+++ packages/ed-core/ed_core-0.1.27-py3-none-any.whl/ed_core/application/features/driver/dtos/__init__.py
@@ -0,0 +1,6 @@
+from ed_core.application.features.driver.dtos.create_driver_dto import \
+    CreateDriverDto
+from ed_core.application.features.driver.dtos.update_driver_dto import \
+    UpdateDriverDto
+
+__all__ = ["CreateDriverDto", "UpdateDriverDto"]
