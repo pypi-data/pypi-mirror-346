5479d11067374f85c191f25a8ece6219837ba583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 07:07:27 +0000

    Add package aioli-sdk/1.4.0/aioli_sdk-1.4.0.tar.gz

---
+++ packages/aioli-sdk/aioli_sdk-1.4.0.tar.gz/aioli_sdk-1.4.0/README.md
@@ -0,0 +1,3 @@
+Aioli (**AI** **O**n**L**ine **I**nference) is a platform for deploying models at scale.
+
+See [https://docs.determined.ai/](https://docs.determined.ai/) for more information.
---
+++ packages/aioli-sdk/aioli_sdk-1.4.0.tar.gz/aioli_sdk-1.4.0/aiolirest/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from aiolirest.api.authentication_api import AuthenticationApi
+from aiolirest.api.deployments_api import DeploymentsApi
+from aiolirest.api.information_api import InformationApi
+from aiolirest.api.packaged_models_api import PackagedModelsApi
+from aiolirest.api.projects_api import ProjectsApi
+from aiolirest.api.registries_api import RegistriesApi
+from aiolirest.api.roles_api import RolesApi
+from aiolirest.api.templates_api import TemplatesApi
+from aiolirest.api.tokens_api import TokensApi
+from aiolirest.api.users_api import UsersApi
+
