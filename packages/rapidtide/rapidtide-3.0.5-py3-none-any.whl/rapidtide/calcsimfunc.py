48356cd8fef8b66d7163dc7bd05f3534482f3791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 14:05:18 +0000

    Add package rapidtide/3.0.5/rapidtide-3.0.5-py3-none-any.whl

---
+++ packages/rapidtide/rapidtide-3.0.5-py3-none-any.whl/rapidtide/calcsimfunc.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#   Copyright 2016-2025 Blaise Frederick
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#
+import gc
+import logging
+import warnings
+
+import numpy as np
+from tqdm import tqdm
+
+import rapidtide.multiproc as tide_multiproc
+import rapidtide.resample as tide_resample
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+LGR = logging.getLogger("GENERAL")
+
+
+def _procOneVoxelCorrelation(
+    vox,
+    thetc,
+    theCorrelator,
+    fmri_x,
+    fmritc,
+    os_fmri_x,
+    oversampfactor=1,
+    interptype="univariate",
+    rt_floatset=np.float64,
+    rt_floattype="float64",
+):
+    if oversampfactor >= 1:
+        thetc[:] = tide_resample.doresample(fmri_x, fmritc, os_fmri_x, method=interptype)
+    else:
+        thetc[:] = fmritc
+    thexcorr_y, thexcorr_x, theglobalmax = theCorrelator.run(thetc)
+
+    return vox, np.mean(thetc), thexcorr_y, thexcorr_x, theglobalmax
+
+
+def correlationpass(
+    fmridata,
+    referencetc,
+    theCorrelator,
+    fmri_x,
+    os_fmri_x,
+    lagmininpts,
+    lagmaxinpts,
+    corrout,
+    meanval,
+    nprocs=1,
+    alwaysmultiproc=False,
+    oversampfactor=1,
+    interptype="univariate",
+    showprogressbar=True,
+    chunksize=1000,
+    rt_floatset=np.float64,
+    rt_floattype="float64",
+    debug=False,
+):
+    """
+
+    Parameters
+    ----------
+    fmridata
+    referencetc - the reference regressor, already oversampled
+    theCorrelator
+    fmri_x
+    os_fmri_x
+    tr
+    lagmininpts
+    lagmaxinpts
+    corrout
+    meanval
+    nprocs
+    oversampfactor
+    interptype
+    showprogressbar
+    chunksize
+    rt_floatset
+    rt_floattype
+
+    Returns
+    -------
+
+    """
+    if debug:
+        print(f"calling setreftc in calcsimfunc with length {len(referencetc)}")
+    theCorrelator.setreftc(referencetc)
+    theCorrelator.setlimits(lagmininpts, lagmaxinpts)
+
+    inputshape = np.shape(fmridata)
+    volumetotal = 0
+    thetc = np.zeros(np.shape(os_fmri_x), dtype=rt_floattype)
+    theglobalmaxlist = []
+    if nprocs > 1 or alwaysmultiproc:
+        # define the consumer function here so it inherits most of the arguments
+        def correlation_consumer(inQ, outQ):
+            while True:
+                try:
+                    # get a new message
+                    val = inQ.get()
+
+                    # this is the 'TERM' signal
+                    if val is None:
+                        break
+
+                    # process and send the data
+                    outQ.put(
+                        _procOneVoxelCorrelation(
+                            val,
+                            thetc,
+                            theCorrelator,
+                            fmri_x,
+                            fmridata[val, :],
+                            os_fmri_x,
+                            oversampfactor=oversampfactor,
+                            interptype=interptype,
+                            rt_floatset=rt_floatset,
+                            rt_floattype=rt_floattype,
+                        )
+                    )
+
+                except Exception as e:
+                    print("error!", e)
+                    break
+
+        data_out = tide_multiproc.run_multiproc(
+            correlation_consumer,
+            inputshape,
+            None,
+            nprocs=nprocs,
+            showprogressbar=showprogressbar,
+            chunksize=chunksize,
+        )
+
+        # unpack the data
+        volumetotal = 0
+        for voxel in data_out:
+            meanval[voxel[0]] = voxel[1]
+            corrout[voxel[0], :] = voxel[2]
+            thecorrscale = voxel[3]
+            theglobalmaxlist.append(voxel[4] + 0)
+            volumetotal += 1
+        del data_out
+    else:
+        for vox in tqdm(
+            range(0, inputshape[0]),
+            desc="Voxel",
+            disable=(not showprogressbar),
+        ):
+            (
+                dummy,
+                meanval[vox],
+                corrout[vox, :],
+                thecorrscale,
+                theglobalmax,
+            ) = _procOneVoxelCorrelation(
+                vox,
+                thetc,
+                theCorrelator,
+                fmri_x,
+                fmridata[vox, :],
+                os_fmri_x,
+                oversampfactor=oversampfactor,
+                interptype=interptype,
+                rt_floatset=rt_floatset,
+                rt_floattype=rt_floattype,
+            )
+            theglobalmaxlist.append(theglobalmax + 0)
+            volumetotal += 1
+    LGR.info(f"\nSimilarity function calculated on {volumetotal} voxels")
+
+    # garbage collect
+    uncollected = gc.collect()
+    if uncollected != 0:
+        LGR.info(f"garbage collected - unable to collect {uncollected} objects")
+    else:
+        LGR.info("garbage collected")
+
+    return volumetotal, theglobalmaxlist, thecorrscale
---
+++ packages/rapidtide/rapidtide-3.0.5-py3-none-any.whl/rapidtide/makelaggedtcs.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#   Copyright 2016-2025 Blaise Frederick
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#
+import gc
+import logging
+
+import numpy as np
+from tqdm import tqdm
+
+import rapidtide.multiproc as tide_multiproc
+
+
+def _procOneVoxelMakelagtc(
+    vox,
+    lagtcgenerator,
+    maxlag,
+    timeaxis,
+    rt_floatset=np.float64,
+    rt_floattype="float64",
+):
+    # question - should maxlag be added or subtracted?  As of 10/18, it is subtracted
+    #  potential answer - tried adding, results are terrible.
+    thelagtc = rt_floatset(lagtcgenerator.yfromx(timeaxis - maxlag))
+
+    return (
+        vox,
+        thelagtc,
+    )
+
+
+def makelaggedtcs(
+    lagtcgenerator,
+    timeaxis,
+    lagmask,
+    lagtimes,
+    lagtc,
+    LGR=None,
+    nprocs=1,
+    alwaysmultiproc=False,
+    showprogressbar=True,
+    chunksize=1000,
+    rt_floatset=np.float64,
+    rt_floattype="float64",
+):
+    inputshape = lagtc.shape
+    if nprocs > 1 or alwaysmultiproc:
+        # define the consumer function here so it inherits most of the arguments
+        def makelagtc_consumer(inQ, outQ):
+            while True:
+                try:
+                    # get a new message
+                    val = inQ.get()
+
+                    # this is the 'TERM' signal
+                    if val is None:
+                        break
+
+                    # process and send the data
+                    outQ.put(
+                        _procOneVoxelMakelagtc(
+                            val,
+                            lagtcgenerator,
+                            lagtimes[val],
+                            timeaxis,
+                            rt_floatset=rt_floatset,
+                            rt_floattype=rt_floattype,
+                        )
+                    )
+                except Exception as e:
+                    print("error!", e)
+                    break
+
+        data_out = tide_multiproc.run_multiproc(
+            makelagtc_consumer,
+            inputshape,
+            lagmask,
+            verbose=(LGR is not None),
+            nprocs=nprocs,
+            showprogressbar=showprogressbar,
+            chunksize=chunksize,
+        )
+
+        # unpack the data
+        volumetotal = 0
+        for voxel in data_out:
+            volumetotal += 1
+            lagtc[voxel[0], :] = voxel[1]
+        del data_out
+    else:
+        volumetotal = 0
+        for vox in tqdm(
+            range(0, inputshape[0]),
+            desc="Voxel",
+            unit="voxels",
+            disable=(not showprogressbar),
+        ):
+            if lagmask[vox] > 0:
+                dothisone = True
+            else:
+                dothisone = False
+            if dothisone:
+                (
+                    dummy,
+                    lagtc[vox, :],
+                ) = _procOneVoxelMakelagtc(
+                    vox,
+                    lagtcgenerator,
+                    lagtimes[vox],
+                    timeaxis,
+                    rt_floatset=rt_floatset,
+                    rt_floattype=rt_floattype,
+                )
+                volumetotal += 1
+
+    if LGR is not None:
+        LGR.info(f"\nLagged timecourses created for {volumetotal} voxels")
+
+    # garbage collect
+    uncollected = gc.collect()
+    if uncollected != 0:
+        if LGR is not None:
+            LGR.info(f"garbage collected - unable to collect {uncollected} objects")
+    else:
+        if LGR is not None:
+            LGR.info("garbage collected")
+
+    return volumetotal
