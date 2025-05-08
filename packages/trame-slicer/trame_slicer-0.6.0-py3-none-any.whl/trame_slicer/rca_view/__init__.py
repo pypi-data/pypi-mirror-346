0e9c5c9097cbee47293c9a116acd3f9c55f626cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:14 +0000

    Add package trame-slicer/0.6.0/trame_slicer-0.6.0-py3-none-any.whl

---
+++ packages/trame-slicer/trame_slicer-0.6.0-py3-none-any.whl/trame_slicer/rca_view/__init__.py
@@ -0,0 +1,11 @@
+from .rca_view_factory import (
+    RemoteSliceViewFactory,
+    RemoteThreeDViewFactory,
+    register_rca_factories,
+)
+
+__all__ = [
+    "RemoteSliceViewFactory",
+    "RemoteThreeDViewFactory",
+    "register_rca_factories",
+]
