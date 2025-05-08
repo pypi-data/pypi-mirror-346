27edf3a373c43da89962f3ff52d67b34e49c02bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 17:04:49 +0000

    Add package streambotcore/0.4/streambotcore-0.4.tar.gz

---
+++ packages/streambotcore/streambotcore-0.4.tar.gz/streambotcore-0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='streambotcore',
+    version='0.4',
+    description='Unified modular Discord and Twitch bot framework',
+    author='AmongTheCouch23',
+    author_email='AmongTheCouch_Studios@hotmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'discord.py',
+        'twitchio',
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
