d7e037ac7c36302fa5abc439ef306b5626520aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 17:04:53 +0000

    Add package hcube/0.27.2/hcube-0.27.2.tar.gz

---
+++ packages/hcube/hcube-0.27.2.tar.gz/hcube-0.27.2/hcube/backends/clickhouse/indexes.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class IndexDefinition:
+    name: str
+    type: str
+    expression: str  # what will be indexed - typically a name of column, but may be more
+    granularity: int = 1
+
+    def definition(self) -> str:
+        return (
+            f"INDEX {self.name} ({self.expression}) TYPE {self.type} GRANULARITY "
+            f"{self.granularity}"
+        )
