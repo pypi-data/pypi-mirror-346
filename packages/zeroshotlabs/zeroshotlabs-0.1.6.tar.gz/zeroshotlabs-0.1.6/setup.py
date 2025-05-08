fd6ff61609b5926ad521d642668f321a16bd8519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 18:05:07 +0000

    Add package zeroshotlabs/0.1.6/zeroshotlabs-0.1.6.tar.gz

---
+++ packages/zeroshotlabs/zeroshotlabs-0.1.6.tar.gz/zeroshotlabs-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="zeroshotlabs",
+    version="0.1.6",
+    description="Stream zeroshot dataset",
+    author="Zeroshot",
+    author_email="code@zeroshotdata.com",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.18.0",
+        "pandas>=2.0.0",
+        "requests>=2.22.0",
+        "mosaicml-streaming>=0.11.0",
+        "opencv-python>=4.8.0.0",
+        "pyarrow>=15.0.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
