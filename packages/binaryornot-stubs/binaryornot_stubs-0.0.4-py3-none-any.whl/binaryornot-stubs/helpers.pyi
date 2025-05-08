2facfc0e16c78100671563e08540cc63d9cc6d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 03:22:47 +0000

    Add package binaryornot-stubs/0.0.4/binaryornot_stubs-0.0.4-py3-none-any.whl

---
+++ packages/binaryornot-stubs/binaryornot_stubs-0.0.4-py3-none-any.whl/binaryornot-stubs/helpers.pyi
@@ -0,0 +1,10 @@
+def print_as_hex(s: str) -> None:
+    ...
+
+
+def get_starting_chunk(filename: str, length: int = ...) -> bytes:
+    ...
+
+
+def is_binary_string(bytes_to_check: bytes) -> bool:
+    ...
