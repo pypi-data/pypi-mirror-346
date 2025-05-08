e03b4f43caf351c909c965e6b42ef55a1d7075fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 07:06:40 +0000

    Add package django-neurobank/0.10.10/django_neurobank-0.10.10.tar.gz

---
+++ packages/django-neurobank/django_neurobank-0.10.10.tar.gz/django_neurobank-0.10.10/nbank_registry/admin.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from nbank_registry.models import Archive, DataType, Location, Resource
+
+
+class LocationInline(admin.TabularInline):
+    model = Location
+
+
+class DataTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "content_type")
+
+
+class ArchiveAdmin(admin.ModelAdmin):
+    list_display = ("name", "scheme", "root")
+    list_filter = ("name", "scheme")
+    search_fields = ("name__istartswith", "scheme__istartswith", "root__icontains")
+
+
+class ResourceAdmin(admin.ModelAdmin):
+    fields = ("name", "sha1", "dtype", "metadata", "created_by")
+    list_display = (
+        "name",
+        "dtype",
+    )
+    list_filter = ("name", "dtype")
+    search_fields = (
+        "name__istartswith",
+        "sha1__istartswith",
+        "dtype",
+        "metadata__icontains",
+    )
+    inlines = (LocationInline,)
+
+
+admin.site.register(Resource, ResourceAdmin)
+admin.site.register(DataType, DataTypeAdmin)
+admin.site.register(Archive, ArchiveAdmin)
