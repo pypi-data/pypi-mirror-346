65e4559df2b0def92191a66a8181bd93d9be0b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-08 19:04:49 +0000

    Add package cmake/4.0.2/cmake-4.0.2.tar.gz

---
+++ packages/cmake/cmake-4.0.2.tar.gz/cmake-4.0.2/CMakeUrls.cmake
@@ -0,0 +1,29 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2.tar.gz")
+set(unix_source_sha256       "1c3a82c8ca7cf12e0b17178f9d0c32f7ac773bd5651a98fcfd80fbf4977f8d48")
+
+set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2.zip")
+set(windows_source_sha256    "983f2b0cc24edd9d6c2754d2f52f0fce36f0368c95a9b6f45df3faf1dfd0728a")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url       "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256    "NA")
+
+set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2-linux-x86_64.tar.gz")
+set(linux64_binary_sha256    "80940e81de61584fe4eedd3c40adc597d7c5b76ad8709668007b467a3c2a36c7")
+
+set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2-macos10.10-universal.tar.gz")
+set(macos10_10_binary_sha256 "edb0713b557040a2e90718909e11cac82810fe2bf8943828b11cfb84b75815a0")
+
+set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2-windows-i386.zip")
+set(win32_binary_sha256      "026009a57d06cb8342e42840fcbd83bc5e58ee3ea90d3ddc8e46f82a0d93d9ff")
+
+set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2-windows-x86_64.zip")
+set(win64_binary_sha256      "109ec7de10416d6d78991bab9714d2cb1ccb71d1b436dff42ec978dd283c29fc")
+
+set(winarm64_binary_url      "https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2-windows-arm64.zip")
+set(winarm64_binary_sha256   "634d6bab01a639e314f4fb2fee0967c84c3a37e39b9b2c9455c2dff2deb6a6dc")
