515e114d5350759da612369ade7f302da10bafbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 05:05:31 +0000

    Add package fintoolkit/0.0.17/fintoolkit-0.0.17.tar.gz

---
+++ packages/fintoolkit/fintoolkit-0.0.17.tar.gz/fintoolkit-0.0.17/tests/test_data.py
@@ -0,0 +1,17 @@
+import unittest
+import toolkit as ftk
+
+
+class TestData(unittest.TestCase):
+
+    def test_yahoo(self):
+        ftk.get_yahoo('TSLA')
+        ftk.get_yahoo_bulk(['AAPL', 'MSFT'])
+
+    def test_famafrench(self):
+        ds = ftk.get_famafrench_datasets()
+        ftk.get_famafrench_factors(ds[0])
+
+    def test_msci(self):
+        acwi = ftk.get_msci([892400], ror=False)
+        self.assertAlmostEqual(acwi.loc['2023-12-29'].iloc[0], 726.996)
