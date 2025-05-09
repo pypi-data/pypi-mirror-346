957ace03c918f952086a16502e0f5dabc1e511d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:06:18 +0000

    Add package web-crawler-for-map-services-shared/0.1.3/web_crawler_for_map_services_shared-0.1.3.tar.gz

---
+++ packages/web-crawler-for-map-services-shared/web_crawler_for_map_services_shared-0.1.3.tar.gz/web_crawler_for_map_services_shared-0.1.3/src/web_crawler_for_map_services_shared/models/outlet_model.py
@@ -0,0 +1,11 @@
+from typing import List
+from pydantic import BaseModel
+from .legal_information_model import LegalInformationModel
+
+class OutletModel(BaseModel):
+    signboard: str
+    address: str
+    activity_types: List[str]
+    contact_phones: List[str]
+    review_keywords: List[str]
+    legal_information: LegalInformationModel
