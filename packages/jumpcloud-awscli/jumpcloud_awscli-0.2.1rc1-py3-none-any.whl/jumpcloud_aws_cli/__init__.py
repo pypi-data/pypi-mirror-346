9f4b255017fc8fb5bb02a6914fffa421d034cd9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:28 +0000

    Add package jumpcloud-awscli/0.2.1rc1/jumpcloud_awscli-0.2.1rc1-py3-none-any.whl

---
+++ packages/jumpcloud-awscli/jumpcloud_awscli-0.2.1rc1-py3-none-any.whl/jumpcloud_aws_cli/__init__.py
@@ -0,0 +1,8 @@
+"""
+JumpCloud AWS CLI Authentication Utility
+
+This package provides utilities for authenticating with AWS CLI using JumpCloud SAML
+and Device Trust certificates.
+"""
+
+__version__ = "0.2.0"
