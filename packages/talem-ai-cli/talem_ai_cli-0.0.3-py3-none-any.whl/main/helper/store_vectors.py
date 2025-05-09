2f3a46de87bf0da47264ccfa5a76103778ea1b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:06:03 +0000

    Add package talem-ai-cli/0.0.3/talem_ai_cli-0.0.3-py3-none-any.whl

---
+++ packages/talem-ai-cli/talem_ai_cli-0.0.3-py3-none-any.whl/main/helper/store_vectors.py
@@ -0,0 +1,51 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_astradb import AstraDBVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
+from main.helper.pdf import save_online_pdf
+from main.helper.web_crawl import crawler
+from main.helper.creditionals import read_db_config
+from main.helper.spinner import spinner
+import os
+import click
+
+async def store_vectors(pdf_or_web, url, collection_name, namespace):
+
+    db_config = read_db_config()
+
+    pdf_path = ""
+
+    if (pdf_or_web == "pdf"):
+        pdf_path = save_online_pdf(url)
+    elif (pdf_or_web == "web");
+        pdf_path = crawler(url)
+
+    # Error handling if file is not found
+    if not os.path.exists(pdf_path):
+        raise FileNotFoundError(f"PDF file not found at: {pdf_path}")
+
+    pdf_loader = PyPDFLoader(pdf_path)  
+    documents = pdf_loader.load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    docs = text_splitter.split_documents(documents)
+
+    embedding_model = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(model_name=embedding_model)
+
+
+    spinner()
+
+    vectorstore = AstraDBVectorStore(
+        collection_name=collection_name,
+        embedding=embeddings,
+        api_endpoint=db_config.api_endpoint,
+        token=db_config.token,
+        namespace=namespace,
+    )
+  
+    vectorstore.add_documents(documents=docs)
+
+    os.remove(pdf_path)
+
+    click.echo(click.style("Stored vector embeddings ✅", fg="green"))
