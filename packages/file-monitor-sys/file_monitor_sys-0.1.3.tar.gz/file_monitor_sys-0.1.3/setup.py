3df9ded506deb4718f3f8d39ea01cd6103f35e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 15:06:39 +0000

    Add package file-monitor-sys/0.1.3/file_monitor_sys-0.1.3.tar.gz

---
+++ packages/file-monitor-sys/file_monitor_sys-0.1.3.tar.gz/file_monitor_sys-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="file_monitor_sys",
+    version="0.1.3",
+    description="A file monitoring system with email alerts and logging",
+    author="Your Name",
+    author_email="your.email@example.com",
+    python_requires=">=3.6",
+    packages=find_packages(where="file_monitor"),
+    package_dir={"": "file_monitor"},
+    install_requires=[
+        "watchdog",
+        "yagmail",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
