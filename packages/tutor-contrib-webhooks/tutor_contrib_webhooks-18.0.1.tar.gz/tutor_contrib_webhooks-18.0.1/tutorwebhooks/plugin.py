a39697d3d63a4d960c61a0ea946bf97dd576d62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:06:14 +0000

    Add package tutor-contrib-webhooks/18.0.1/tutor_contrib_webhooks-18.0.1.tar.gz

---
+++ packages/tutor-contrib-webhooks/tutor_contrib_webhooks-18.0.1.tar.gz/tutor_contrib_webhooks-18.0.1/tutorwebhooks/plugin.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+
+import importlib_resources
+from tutor import hooks
+
+from .__about__ import __version__
+
+########################################
+# CONFIGURATION
+########################################
+
+hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [
+        ("WEBHOOKS_VERSION", __version__),
+        ("WEBHOOKS_PLUGIN_VERSION", "==18.0.1"),
+    ]
+)
+
+########################################
+# PATCH LOADING
+# (It is safe & recommended to leave
+#  this section as-is :)
+########################################
+
+# For each file in tutorwebhooks/patches,
+# apply a patch based on the file's name and contents.
+for path in glob(str(importlib_resources.files("tutorwebhooks") / "patches" / "*")):
+    with open(path, encoding="utf-8") as patch_file:
+        hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
