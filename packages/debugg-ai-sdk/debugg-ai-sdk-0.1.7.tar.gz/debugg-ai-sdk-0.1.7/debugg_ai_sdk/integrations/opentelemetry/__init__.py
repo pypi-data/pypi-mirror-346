1b8bd77e4e4dabeaf1a6bcb25e8627c4addf021b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 07:06:19 +0000

    Add package debugg-ai-sdk/0.1.7/debugg-ai-sdk-0.1.7.tar.gz

---
+++ packages/debugg-ai-sdk/debugg-ai-sdk-0.1.7.tar.gz/debugg-ai-sdk-0.1.7/debugg_ai_sdk/integrations/opentelemetry/__init__.py
@@ -0,0 +1,7 @@
+from debugg_ai_sdk.integrations.opentelemetry.span_processor import SentrySpanProcessor
+from debugg_ai_sdk.integrations.opentelemetry.propagator import SentryPropagator
+
+__all__ = [
+    "SentryPropagator",
+    "SentrySpanProcessor",
+]
