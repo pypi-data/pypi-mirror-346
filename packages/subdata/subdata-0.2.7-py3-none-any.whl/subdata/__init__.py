4f71a3486f6e986e6d229126a88fcfa70355aea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 16:09:01 +0000

    Add package subdata/0.2.7/subdata-0.2.7-py3-none-any.whl

---
+++ packages/subdata/subdata-0.2.7-py3-none-any.whl/subdata/__init__.py
@@ -0,0 +1,13 @@
+import subdata.download
+import subdata.process
+
+from subdata.main import get_target_info
+from subdata.main import get_category_info
+from subdata.main import create_target_dataset
+from subdata.main import create_category_dataset
+
+from subdata.update import update_mapping_specific
+from subdata.update import update_mapping_all
+from subdata.update import update_taxonomy
+from subdata.update import update_overview
+from subdata.update import add_target
