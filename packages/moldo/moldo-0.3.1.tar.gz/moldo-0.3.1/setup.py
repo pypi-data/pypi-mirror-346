969a5148df8aa5405dd27ec27a81be7c810c9939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 15:06:52 +0000

    Add package moldo/0.3.1/moldo-0.3.1.tar.gz

---
+++ packages/moldo/moldo-0.3.1.tar.gz/moldo-0.3.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="moldo",
+    version="0.3.1",
+    packages=find_packages(),
+    install_requires=[
+        "antlr4-python3-runtime>=4.13.1",
+        "typing-extensions>=4.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "moldo=moldo.cli:main",
+        ],
+    },
+    author="Mutiibwa Grace Peter",
+    author_email="gracepetermutiibwa@gmail.com",
+    description="A visual programming language that compiles to Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GracePeterMutiibwa/moldo",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Compilers",
+        "Topic :: Education",
+    ],
+    python_requires=">=3.8",
+)
