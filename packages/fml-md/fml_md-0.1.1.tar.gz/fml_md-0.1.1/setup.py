839aa77a48989a3e41a1ded5ad28a00dbc22dabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:05:47 +0000

    Add package fml-md/0.1.1/fml_md-0.1.1.tar.gz

---
+++ packages/fml-md/fml_md-0.1.1.tar.gz/fml_md-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fml-md",
+    version="0.1.1",
+    author="OwPor",
+    author_email="concepcionrayvincent@gmail.com",
+    description="Fibonacci Markup Language - where indentation follows the Fibonacci sequence",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OwPor/fml-md",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "click>=8.0.0",
+        "markdown>=3.3.0",
+        "pygments>=2.7.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "fml=fml.cli:cli",
+        ],
+    },
+    include_package_data=True,
+    keywords="fibonacci, markup, language, indentation",
+)
