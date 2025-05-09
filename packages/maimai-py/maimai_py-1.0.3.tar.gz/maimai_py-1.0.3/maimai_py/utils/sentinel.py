7cd4b5271089292ebb7a3352a4a2b37a0f4c0981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:29 +0000

    Add package maimai-py/1.0.3/maimai_py-1.0.3.tar.gz

---
+++ packages/maimai-py/maimai_py-1.0.3.tar.gz/maimai_py-1.0.3/maimai_py/utils/sentinel.py
@@ -0,0 +1,23 @@
+from typing import Any, TypeVar
+
+_T = TypeVar("_T")
+
+
+class _UnsetSentinel:
+    def __repr__(self) -> str:
+        return "Unset"
+
+    def __copy__(self: _T) -> _T:
+        return self
+
+    def __reduce__(self) -> str:
+        return "Unset"
+
+    def __deepcopy__(self: _T, _: Any) -> _T:
+        return self
+
+    def __hash__(self) -> int:
+        return hash("unset_sentinel")
+
+
+UNSET = _UnsetSentinel()
