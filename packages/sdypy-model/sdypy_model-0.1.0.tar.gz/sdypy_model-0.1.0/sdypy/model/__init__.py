0c40ec6f3e9aee708680dec2e954baf59fd54c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 16:09:05 +0000

    Add package sdypy-model/0.1.0/sdypy_model-0.1.0.tar.gz

---
+++ packages/sdypy-model/sdypy_model-0.1.0.tar.gz/sdypy_model-0.1.0/sdypy/model/__init__.py
@@ -0,0 +1,7 @@
+import pyLump as lumped
+from .shell.shell import Shell
+from .tetrahedron.tet10 import Tetrahedron
+from .beam.beam import Beam
+from .eigenvalue_solution import solve_eigenvalue
+
+from . import mesh
