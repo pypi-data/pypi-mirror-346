e21c969d6e7d20ee82d986df26f93527dc0df9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:01 +0000

    Add package upkie/8.0.0/upkie-8.0.0-py3-none-any.whl

---
+++ packages/upkie/upkie-8.0.0-py3-none-any.whl/upkie/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 Inria
+
+"""Python module to control Upkie wheeled bipeds."""
+
+__version__ = "8.0.0"
