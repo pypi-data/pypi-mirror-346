3a18a938ed4a1e52982b7330aba10d7ab6afd56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 07:06:27 +0000

    Add package biobit/0.0.8/biobit-0.0.8-cp313-cp313t-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/biobit/biobit-0.0.8-cp313-cp313t-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/biobit/toolkit/annotome/transcriptome/__init__.py
@@ -0,0 +1,6 @@
+from .core import Location
+from .gene import Gene, GeneBundle
+from .cds import CDS, CDSBundle
+from .rna import RNA, RNABundle
+
+__all__ = ["Location", "Gene", "GeneBundle", "RNA", "RNABundle", "CDS", "CDSBundle"]
