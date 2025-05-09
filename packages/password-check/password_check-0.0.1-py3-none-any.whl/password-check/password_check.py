205bf33b3113f204e48fdcaa42c20d535dc9c7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 07:06:21 +0000

    Add package password-check/0.0.1/password_check-0.0.1-py3-none-any.whl

---
+++ packages/password-check/password_check-0.0.1-py3-none-any.whl/password-check/password_check.py
@@ -0,0 +1,14 @@
+import re
+
+def is_strong(password: str) -> bool:
+    if len(password) < 8:
+        return False
+    if not re.search(r"[A-Z]", password):
+        return False
+    if not re.search(r"[a-z]", password):
+        return False
+    if not re.search(r"\d", password):
+        return False
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        return False
+    return True
