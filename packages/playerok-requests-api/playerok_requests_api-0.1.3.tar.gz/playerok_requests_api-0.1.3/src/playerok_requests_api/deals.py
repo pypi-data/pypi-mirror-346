54ac14ae62a54820266ee6e19c062b0f8fa45b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:32 +0000

    Add package playerok-requests-api/0.1.3/playerok_requests_api-0.1.3.tar.gz

---
+++ packages/playerok-requests-api/playerok_requests_api-0.1.3.tar.gz/playerok_requests_api-0.1.3/src/playerok_requests_api/deals.py
@@ -0,0 +1,88 @@
+import json
+import tls_requests
+from PlayerokRequestsApi.utils import globalheaders, load_cookies
+
+class PlayerokDealsApi:
+    def __init__(self, cookies_file="cookies.json", logger=False):
+        self.cookies = load_cookies(cookies_file)
+        self.Logging = logger
+        self.api_url = "https://playerok.com/graphql"
+        self.username, self.id = self.get_username()
+
+    def get_username(self):
+        """получить username и id пользователя (используется для получения в начале self.id, self.username)"""
+        try:
+            json_data = {
+                'operationName': 'viewer',
+                'variables': {},
+                'query': 'query viewer {\n  viewer {\n    ...Viewer\n    __typename\n  }\n}\n\nfragment Viewer on User {\n  id\n  username\n  email\n  role\n  hasFrozenBalance\n  supportChatId\n  systemChatId\n  unreadChatsCounter\n  isBlocked\n  isBlockedFor\n  createdAt\n  lastItemCreatedAt\n  hasConfirmedPhoneNumber\n  canPublishItems\n  profile {\n    id\n    avatarURL\n    testimonialCounter\n    __typename\n  }\n  __typename\n}',
+            }
+            response = tls_requests.post(self.api_url, cookies=self.cookies, headers=globalheaders, json=json_data)
+            try:
+                data = response.json()
+                viewer = data.get('data', {}).get('viewer', {})
+                username = viewer.get('username', '')
+                id = viewer.get('id', '')
+                if not username:
+                    raise ValueError("Username not found")
+                return username, id
+            except Exception as e:
+                print(f'Unsolved problem(Please pass this error to the API owner.) - ERROR: {e}')
+        except ValueError as e:
+            print(f"Ошибка данных: {e}")
+            return '', ''
+        except Exception as e:
+            print(f"Неизвестная ошибка: {e}")
+            return '', ''
+
+    def deal_confirm(self, id):
+        """подтвердить сделку по id"""
+        json_data = {
+            "operationName": "updateDeal",
+            "variables": {
+                "input": {
+                    "id": f"{id}",
+                    "status": "SENT"
+                }
+            },
+            "query": "mutation updateDeal($input: UpdateItemDealInput!) {\n  updateDeal(input: $input) {\n    ...RegularItemDeal\n    __typename\n  }\n}\n\nfragment RegularItemDeal on ItemDeal {\n  id\n  status\n  direction\n  statusExpirationDate\n  statusDescription\n  obtaining\n  hasProblem\n  reportProblemEnabled\n  completedBy {\n    ...MinimalUserFragment\n    __typename\n  }\n  props {\n    ...ItemDealProps\n    __typename\n  }\n  prevStatus\n  completedAt\n  createdAt\n  logs {\n    ...ItemLog\n    __typename\n  }\n  transaction {\n    ...ItemDealTransaction\n    __typename\n  }\n  user {\n    ...UserEdgeNode\n    __typename\n  }\n  chat {\n    ...RegularChatId\n    __typename\n  }\n  item {\n    ...PartialDealItem\n    __typename\n  }\n  testimonial {\n    ...RegularItemDealTestimonial\n    __typename\n  }\n  obtainingFields {\n    ...GameCategoryDataFieldWithValue\n    __typename\n  }\n  commentFromBuyer\n  __typename\n}\n\nfragment MinimalUserFragment on UserFragment {\n  id\n  username\n  role\n  __typename\n}\n\nfragment ItemDealProps on ItemDealProps {\n  autoConfirmPeriod\n  __typename\n}\n\nfragment ItemLog on ItemLog {\n  id\n  event\n  createdAt\n  user {\n    ...UserEdgeNode\n    __typename\n  }\n  __typename\n}\n\nfragment UserEdgeNode on UserFragment {\n  ...RegularUserFragment\n  __typename\n}\n\nfragment RegularUserFragment on UserFragment {\n  id\n  username\n  role\n  avatarURL\n  isOnline\n  isBlocked\n  rating\n  testimonialCounter\n  createdAt\n  supportChatId\n  systemChatId\n  __typename\n}\n\nfragment ItemDealTransaction on Transaction {\n  id\n  operation\n  direction\n  providerId\n  status\n  value\n  createdAt\n  paymentMethodId\n  statusExpirationDate\n  __typename\n}\n\nfragment RegularChatId on Chat {\n  id\n  __typename\n}\n\nfragment PartialDealItem on Item {\n  ...PartialDealMyItem\n  ...PartialDealForeignItem\n  __typename\n}\n\nfragment PartialDealMyItem on MyItem {\n  id\n  slug\n  priority\n  status\n  name\n  price\n  priorityPrice\n  rawPrice\n  statusExpirationDate\n  sellerType\n  approvalDate\n  createdAt\n  priorityPosition\n  viewsCounter\n  feeMultiplier\n  comment\n  attachments {\n    ...RegularFile\n    __typename\n  }\n  user {\n    ...UserEdgeNode\n    __typename\n  }\n  game {\n    ...RegularGameProfile\n    __typename\n  }\n  category {\n    ...MinimalGameCategory\n    __typename\n  }\n  dataFields {\n    ...GameCategoryDataFieldWithValue\n    __typename\n  }\n  obtainingType {\n    ...MinimalGameCategoryObtainingType\n    __typename\n  }\n  __typename\n}\n\nfragment RegularFile on File {\n  id\n  url\n  filename\n  mime\n  __typename\n}\n\nfragment RegularGameProfile on GameProfile {\n  id\n  name\n  type\n  slug\n  logo {\n    ...PartialFile\n    __typename\n  }\n  __typename\n}\n\nfragment PartialFile on File {\n  id\n  url\n  __typename\n}\n\nfragment MinimalGameCategory on GameCategory {\n  id\n  slug\n  name\n  __typename\n}\n\nfragment GameCategoryDataFieldWithValue on GameCategoryDataFieldWithValue {\n  id\n  label\n  type\n  inputType\n  copyable\n  hidden\n  required\n  value\n  __typename\n}\n\nfragment MinimalGameCategoryObtainingType on GameCategoryObtainingType {\n  id\n  name\n  description\n  gameCategoryId\n  noCommentFromBuyer\n  instructionForBuyer\n  instructionForSeller\n  sequence\n  feeMultiplier\n  props {\n    minTestimonialsForSeller\n    __typename\n  }\n  __typename\n}\n\nfragment PartialDealForeignItem on ForeignItem {\n  id\n  slug\n  priority\n  status\n  name\n  price\n  rawPrice\n  sellerType\n  approvalDate\n  priorityPosition\n  createdAt\n  viewsCounter\n  feeMultiplier\n  comment\n  attachments {\n    ...RegularFile\n    __typename\n  }\n  user {\n    ...UserEdgeNode\n    __typename\n  }\n  game {\n    ...RegularGameProfile\n    __typename\n  }\n  category {\n    ...MinimalGameCategory\n    __typename\n  }\n  dataFields {\n    ...GameCategoryDataFieldWithValue\n    __typename\n  }\n  obtainingType {\n    ...MinimalGameCategoryObtainingType\n    __typename\n  }\n  __typename\n}\n\nfragment RegularItemDealTestimonial on Testimonial {\n  id\n  status\n  text\n  rating\n  createdAt\n  updatedAt\n  creator {\n    ...RegularUserFragment\n    __typename\n  }\n  moderator {\n    ...RegularUserFragment\n    __typename\n  }\n  user {\n    ...RegularUserFragment\n    __typename\n  }\n  __typename\n}"
+        }
+        try:
+            response = tls_requests.post(self.api_url, headers=globalheaders, cookies=self.cookies, json=json_data)
+            return response.json()
+        except Exception as e:
+            print(f"Ошибка при подтверждении сделки: {e}")
+            return None
+
+    def get_actual_deals(self):
+        """получить список актуальных сделок который оплачены"""
+        params = {
+            "operationName": "countDeals",
+            "variables": json.dumps({
+                "filter": {
+                    "userId": self.id,
+                    "direction": "OUT",
+                    "status": ["PAID"]
+                }
+            }),
+            "extensions": json.dumps({
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "1e2605594b38041bb7a5466a9cab994a169725d7b69aae3b7ee185d0b7fc33c2"
+                }
+            })
+        }
+
+        try:
+            response = tls_requests.get(
+                self.api_url,
+                params=params,
+                headers=globalheaders,
+                cookies=self.cookies
+            )
+            response.raise_for_status()
+            data = response.json()
+            return data
+        except Exception as e:
+            print(f"Ошибка при получении актуальных сделок: {e}")
+            return None
