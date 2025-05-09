fc5b6a0f7e4e74e5a7c2a596806227c9d55a0002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 15:06:31 +0000

    Add package Arko/0.0.1/Arko-0.0.1.tar.gz

---
+++ packages/Arko/Arko-0.0.1.tar.gz/Arko-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Arko",
+    version="0.0.1",
+    description="Arko",
+    author="Anish Agrawal",
+    author_email="your.email@example.com",
+    packages=["Arko"],
+)
