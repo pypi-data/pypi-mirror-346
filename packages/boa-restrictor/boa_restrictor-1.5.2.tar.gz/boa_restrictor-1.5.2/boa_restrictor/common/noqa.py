daf343cb94ce00f484af74045eeb302af8806fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 15:06:47 +0000

    Add package boa-restrictor/1.5.2/boa_restrictor-1.5.2.tar.gz

---
+++ packages/boa-restrictor/boa_restrictor-1.5.2.tar.gz/boa_restrictor-1.5.2/boa_restrictor/common/noqa.py
@@ -0,0 +1,23 @@
+import re
+import tokenize
+from io import StringIO
+
+from boa_restrictor.common.rule import PYTHON_LINTING_RULE_PREFIX
+
+
+def get_noqa_comments(*, source_code: str) -> list[tuple[int, str]]:
+    """
+    Walk the target code and detect all lines having a "# noqa" comment with "our" prefix.
+    """
+    noqa_statements = []
+
+    tokens = tokenize.generate_tokens(StringIO(source_code).readline)
+    pattern = re.compile(r"^#\snoqa:\s*.*?" + PYTHON_LINTING_RULE_PREFIX + r"\d{3}")
+
+    for token in tokens:
+        token_type, token_string, start, _, _ = token
+
+        if token_type == tokenize.COMMENT and pattern.search(token_string):
+            noqa_statements.append((start[0], token_string.strip()))
+
+    return noqa_statements
