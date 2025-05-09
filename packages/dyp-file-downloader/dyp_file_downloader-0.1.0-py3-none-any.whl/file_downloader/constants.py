824603949c7d084fd62abc162f688dbd6fca1d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 05:05:19 +0000

    Add package dyp-file-downloader/0.1.0/dyp_file_downloader-0.1.0-py3-none-any.whl

---
+++ packages/dyp-file-downloader/dyp_file_downloader-0.1.0-py3-none-any.whl/file_downloader/constants.py
@@ -0,0 +1,7 @@
+"""Constants for the file downloader package."""
+
+# Base URLs for different file types
+FILE_URLS = {
+    "readme.txt": "https://raw.githubusercontent.com/github/gitignore/main/README.md",
+    "python.gitignore": "https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore",
+}
