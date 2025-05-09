a5908210b435258611c5195f07fa1044e733846e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:29 +0000

    Add package fastsparsegams/0.2.0/fastsparsegams-0.2.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/fastsparsegams/fastsparsegams-0.2.0-cp310-cp310-macosx_11_0_arm64.whl/fastsparsegams/share/Armadillo/CMake/ArmadilloConfig.cmake
@@ -0,0 +1,16 @@
+# - Config file for the Armadillo package
+# It defines the following variables
+#  ARMADILLO_INCLUDE_DIRS - include directories for Armadillo
+#  ARMADILLO_LIBRARY_DIRS - library directories for Armadillo (normally not used!)
+#  ARMADILLO_LIBRARIES    - libraries to link against
+
+# Tell the user project where to find our headers and libraries
+set(ARMADILLO_INCLUDE_DIRS "/Users/runner/work/L0Learn/L0Learn/python/_skbuild/macosx-11.0-arm64-3.10/cmake-install/fastsparsegams/include")
+set(ARMADILLO_LIBRARY_DIRS "/Users/runner/work/L0Learn/L0Learn/python/_skbuild/macosx-11.0-arm64-3.10/cmake-install/fastsparsegams/lib")
+
+# Our library dependencies (contains definitions for IMPORTED targets)
+include("/Users/runner/work/L0Learn/L0Learn/python/_skbuild/macosx-11.0-arm64-3.10/cmake-install/fastsparsegams/share/Armadillo/CMake/ArmadilloLibraryDepends.cmake")
+
+# These are IMPORTED targets created by ArmadilloLibraryDepends.cmake
+set(ARMADILLO_LIBRARIES armadillo)
+
