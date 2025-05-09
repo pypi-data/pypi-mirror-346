1855373120919335cdd11487afcb1bb52b59565e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 06:06:30 +0000

    Add package alli-googledrive/0.1.0a0/alli_googledrive-0.1.0a0-py3-none-any.whl

---
+++ packages/alli-googledrive/alli_googledrive-0.1.0a0-py3-none-any.whl/shipyard_googledrive/cli/authtest.py
@@ -0,0 +1,26 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "alli-googledrive",
+# ]
+# ///
+import os
+import sys
+from shipyard_googledrive import GoogleDriveClient
+
+
+def main():
+    drive = GoogleDriveClient(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+    try:
+        conn = drive.connect()
+        drive.logger.info("Successfully connected to Google Drive")
+        sys.exit(0)
+    except Exception as e:
+        drive.logger.error(
+            "Could not connect to Google Drive with the service account provided"
+        )
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
