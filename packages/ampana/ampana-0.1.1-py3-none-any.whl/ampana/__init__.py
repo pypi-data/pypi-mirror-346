badc27c03687e56d686722c3e2a72f3e2c120cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 05:05:27 +0000

    Add package ampana/0.1.1/ampana-0.1.1-py3-none-any.whl

---
+++ packages/ampana/ampana-0.1.1-py3-none-any.whl/ampana/__init__.py
@@ -0,0 +1,8 @@
+from .weather import get_weather
+from .vibe import get_vibe
+from .advice import get_advice
+from .compliment import get_compliment
+from .plan import get_plan
+
+
+__all__=['get_weather','get_vibe','get_advice','get_compliment','get_plan']
