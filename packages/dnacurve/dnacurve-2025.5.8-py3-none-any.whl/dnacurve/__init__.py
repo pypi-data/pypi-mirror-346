9d75d578ac520886940d6ff7b0561a7edda615fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 15:06:31 +0000

    Add package dnacurve/2025.5.8/dnacurve-2025.5.8-py3-none-any.whl

---
+++ packages/dnacurve/dnacurve-2025.5.8-py3-none-any.whl/dnacurve/__init__.py
@@ -0,0 +1,17 @@
+# dnacurve/__init__.py
+
+from .dnacurve import *
+from .dnacurve import __all__, __doc__, __version__
+
+
+def _set_module() -> None:
+    """Set __module__ attribute for all public objects."""
+    globs = globals()
+    module = globs['__name__']
+    for item in __all__:
+        obj = globs[item]
+        if hasattr(obj, '__module__'):
+            obj.__module__ = module
+
+
+_set_module()
