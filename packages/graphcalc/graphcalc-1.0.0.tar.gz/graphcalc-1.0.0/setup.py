7bc5102d69d7a90f526936ed8ed04667753a4ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:05:47 +0000

    Add package graphcalc/1.0.0/graphcalc-1.0.0.tar.gz

---
+++ packages/graphcalc/graphcalc-1.0.0.tar.gz/graphcalc-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="graphcalc",
+    version="1.0.0",
+    author="Randy Davila",
+    author_email="rrd6@rice.edu",
+    description="A Python package for graph computation functions",
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/randydavila/graphcalc",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",            # Numerical operations
+        "networkx",          # Graph-theoretic computations
+        "pillow",            # Image handling (if used for visualization)
+        "PuLP",              # Linear programming
+        "matplotlib",        # Plotting (if visualization is part of the package)
+        "python-dateutil",   # Date handling (if required by your package)
+        "pandas",           # Data manipulation
+        "matplotlib",       # Plotting
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    keywords="graph theory, networkx, graph computation",
+    project_urls={
+        "Documentation": "https://graphcalc.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/randydavila/graphcalc",
+        "PyPI": "https://pypi.org/project/graphcalc/"
+    },
+)
