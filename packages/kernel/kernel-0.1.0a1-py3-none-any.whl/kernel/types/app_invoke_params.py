08dd43e8c47cb77fc5ade6679d7644a90fbc3677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 05:05:41 +0000

    Add package kernel/0.1.0a1/kernel-0.1.0a1-py3-none-any.whl

---
+++ packages/kernel/kernel-0.1.0a1-py3-none-any.whl/kernel/types/app_invoke_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["AppInvokeParams"]
+
+
+class AppInvokeParams(TypedDict, total=False):
+    app_name: Required[Annotated[str, PropertyInfo(alias="appName")]]
+    """Name of the application"""
+
+    payload: Required[object]
+    """Input data for the application"""
+
+    version: Required[str]
+    """Version of the application"""
