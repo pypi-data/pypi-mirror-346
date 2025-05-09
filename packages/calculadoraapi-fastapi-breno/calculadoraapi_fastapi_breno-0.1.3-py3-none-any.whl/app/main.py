42b0dca65cda7eba256ee8d844f0a36ca303479f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:20 +0000

    Add package calculadoraapi-fastapi-breno/0.1.3/calculadoraapi_fastapi_breno-0.1.3-py3-none-any.whl

---
+++ packages/calculadoraapi-fastapi-breno/calculadoraapi_fastapi_breno-0.1.3-py3-none-any.whl/app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from Models import Operandos
+from Operations import soma, subtracao, multiplicacao, divisao, potencia, raiz
+
+app = FastAPI()
+
+@app.post("/soma")
+def somaRoute(operando: Operandos):
+    return soma(operando)
+
+@app.post("/subtracao")
+def subtracaoRoute(operando: Operandos):
+    return subtracao(operando)
+
+@app.post("/multiplicacao")
+def multiplicacaoRoute(operando: Operandos):
+    return multiplicacao(operando)
+
+@app.post("/divisao")
+def divisaoRoute(operando: Operandos):
+    return divisao(operando)
+
+@app.post("/potencia")
+def potenciaRoute(operando: Operandos):
+    return potencia(operando)
+
+@app.post("/raiz")
+def raizRoute(operando: Operandos):
+    return raiz(operando)
