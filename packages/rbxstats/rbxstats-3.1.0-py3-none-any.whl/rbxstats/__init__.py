fe6aab6b9a73c6c264166b04f51ed58675e5878a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:26 +0000

    Add package rbxstats/3.1.0/rbxstats-3.1.0-py3-none-any.whl

---
+++ packages/rbxstats/rbxstats-3.1.0-py3-none-any.whl/rbxstats/__init__.py
@@ -0,0 +1,4 @@
+from .client import RbxStatsClient, ClientConfig, LogLevel
+from .exceptions import RbxStatsError, AuthenticationError, RateLimitError, NotFoundError, ServerError
+
+__version__ = "3.0.0"
