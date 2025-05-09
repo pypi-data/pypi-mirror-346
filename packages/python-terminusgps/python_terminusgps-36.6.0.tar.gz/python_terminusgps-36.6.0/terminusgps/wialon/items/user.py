d8bc471f5cb5407820cec2672a89798aed296274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 05:05:22 +0000

    Add package python-terminusgps/36.6.0/python_terminusgps-36.6.0.tar.gz

---
+++ packages/python-terminusgps/python_terminusgps-36.6.0.tar.gz/python_terminusgps-36.6.0/terminusgps/wialon/items/user.py
@@ -0,0 +1,229 @@
+from urllib.parse import quote_plus
+
+from terminusgps.wialon import constants, flags
+from terminusgps.wialon.items.base import WialonBase
+
+
+class WialonUser(WialonBase):
+    def create(self, creator_id: str | int, name: str, password: str) -> int | None:
+        """
+        Creates a new Wialon user.
+
+        :param creator_id: A Wialon user id.
+        :type creator_id: :py:obj:`str` | :py:obj:`int`
+        :param name: A name for the user.
+        :type name: :py:obj:`str`
+        :param password: A password for the user.
+        :type password: :py:obj:`str`
+        :raises ValueError: If ``creator_id`` is not a digit.
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: The Wialon id for the new user, if it was created.
+        :rtype: :py:obj:`int` | :py:obj:`None`
+
+        """
+        if isinstance(creator_id, str) and not creator_id.isdigit():
+            raise ValueError(f"'creator_id' must be a digit, got '{creator_id}'.")
+
+        response = self.session.wialon_api.core_create_user(
+            **{
+                "creatorId": str(creator_id),
+                "name": name,
+                "password": password,
+                "dataFlags": str(flags.DataFlag.UNIT_BASE.value),
+            }
+        )
+        return (
+            int(response.get("item", {}).get("id"))
+            if response and response.get("item")
+            else None
+        )
+
+    def _get_access_response(self, hw_type: str) -> dict:
+        """
+        Returns a dict of the Wialon objects the user has access to.
+
+        :param hw_type: A hardware type of Wialon objects to generate a list for.
+        :type hw_type: :py:obj:`str`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: The Wialon API response.
+        :rtype: :py:obj:`dict`
+
+        """
+        return self.session.wialon_api.user_get_items_access(
+            **{
+                "userId": self.id,
+                "directAccess": True,
+                "itemSuperclass": hw_type,
+                "flags": 0x2,
+            }
+        )
+
+    @property
+    def units(self) -> list[str]:
+        """
+        The user's units.
+
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: A list of unit ids the user has access to.
+        :rtype: :py:obj:`list`
+
+        """
+        response = self._get_access_response(hw_type="avl_unit")
+        return [key for key in response.keys()]
+
+    @property
+    def groups(self) -> list[str]:
+        """
+        The user's unit groups.
+
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: A list of group ids the user has access to.
+        :rtype: :py:obj:`list`
+
+        """
+        response = self._get_access_response(hw_type="avl_unit_group")
+        return [key for key in response.keys()]
+
+    def has_access(self, other: WialonBase) -> bool:
+        """
+        Checks if the user has access to ``other``.
+
+        :param other: A Wialon object.
+        :type phone: :py:obj:`~terminusgps.wialon.items.base.WialonBase`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: :py:obj:`True` if the user can access ``other``, else :py:obj:`False`.
+        :rtype: :py:obj:`bool`
+
+        """
+        response: dict = self._get_access_response(hw_type=other.hw_type)
+        items: list[str] = [key for key in response.keys()]
+        return True if str(other.id) in items else False
+
+    def assign_phone(self, phone: str) -> None:
+        """
+        Assigns a phone number to the user.
+
+        :param phone: A phone number, including country code.
+        :type phone: :py:obj:`str`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        self.add_cproperty("phone", quote_plus(phone))
+
+    def assign_email(self, email: str) -> None:
+        """
+        Assigns an email address to the user.
+
+        :param phone: An email address.
+        :type phone: :py:obj:`str`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        self.add_cproperty("email", email)
+
+    def grant_access(
+        self, item: WialonBase, access_mask: int = constants.ACCESSMASK_UNIT_BASIC
+    ) -> None:
+        """
+        Grants the user access to ``item``.
+
+        :param item: A Wialon object.
+        :type item: :py:obj:`~terminusgps.wialon.items.base.WialonBase`
+        :param access_mask: A Wialon access mask integer.
+        :type access_mask: :py:obj:`int`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        self.session.wialon_api.user_update_item_access(
+            **{"userId": self.id, "itemId": item.id, "accessMask": access_mask}
+        )
+
+    def set_settings_flags(self, flags: int, flags_mask: int) -> None:
+        """
+        Sets the user's settings flags.
+
+        :param flags: The new user settings flags.
+        :type flags: :py:obj:`int`
+        :param flags_mask: A user settings flag mask.
+        :type flags_mask: :py:obj:`int`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        self.session.wialon_api.user_update_user_flags(
+            **{"userId": self.id, "flags": flags, "flagsMask": flags_mask}
+        )
+
+    def reset_password(self, username: str, email: str, url: str) -> None:
+        """
+        Sends a password reset email for a user to ``email``.
+
+        :param username: A Wialon user name.
+        :type username: :py:obj:`str`
+        :param email: A Wialon user email.
+        :type email: :py:obj:`str`
+        :param url: A password reset URL for the user.
+        :type url: :py:obj:`str`
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        self.session.wialon_api.core_reset_password_request(
+            **{"user": username, "url": url, "email": email}
+        )
+
+    def update_password(self, old_password: str, new_password: str) -> None:
+        """
+        Updates the password of the user.
+
+        :param old_password: The user's original password.
+        :type old_password: :py:obj:`str`
+        :param new_password: A new password.
+        :type new_password: :py:obj:`str`
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        self.session.wialon_api.user_update_password(
+            **{
+                "userId": self.id,
+                "oldPassword": old_password,
+                "newPassword": new_password,
+            }
+        )
+
+    def verify_auth(self, destination: str, method: str = "email") -> str:
+        """
+        Sends an authentication code to ``destination`` via ``method``.
+
+        ``Method`` can be ``"email"`` or ``"sms"``.
+
+        :param destination: The email or phone number to send an auth code to.
+        :type destination: :py:obj:`str`
+        :param method: Email or sms. Default is ``"email"``.
+        :type method: :py:obj:`str`
+        :raises ValueError: If the method isn't ``"email"`` or ``"sms"``.
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: An auth code.
+        :rtype: :py:obj:`str`
+
+        """
+        if method != "email" or "sms":
+            raise ValueError(f"Invalid method '{method}'.")
+
+        response = self.session.wialon_api.user_verify_auth(
+            **{
+                "userId": self.id,
+                "type": 1 if method == "email" else 0,
+                "destination": destination,
+            }
+        )
+        return response.get("code")
---
+++ packages/python-terminusgps/python_terminusgps-36.6.0.tar.gz/python_terminusgps-36.6.0/terminusgps/wialon/session.py
@@ -0,0 +1,375 @@
+import dataclasses
+import datetime
+import os
+import typing
+
+import wialon.api
+from django.conf import settings
+from loguru import logger
+
+
+@dataclasses.dataclass
+class WialonAPICall:
+    action: str
+    timestamp: datetime.datetime
+    args: tuple
+    kwargs: dict
+    result: typing.Any = None
+    error: Exception | None = None
+
+
+class Wialon(wialon.api.Wialon):
+    def __init__(
+        self,
+        debug: bool = False,
+        log_level: int = 10,
+        log_days: int = 10,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.call_history: list[WialonAPICall] = []
+        logger.add(
+            f"logs/{self.__class__.__name__}.log",
+            level=log_level,
+            retention=f"{log_days} days",
+            diagnose=debug,
+        )
+
+    @property
+    def total_calls(self) -> int:
+        return len(self.call_history)
+
+    def token_login(self, *args, **kwargs) -> typing.Any:
+        kwargs["appName"] = "python-terminusgps"
+        return self.call("token_login", *args, **kwargs)
+
+    def call(self, action_name: str, *argc, **kwargs) -> typing.Any:
+        logger.debug(f"Executing '{action_name}'...")
+        call_record = WialonAPICall(
+            action=action_name,
+            timestamp=datetime.datetime.now(),
+            args=argc,
+            kwargs=kwargs,
+        )
+
+        try:
+            result = super().call(action_name, *argc, **kwargs)
+            call_record.result = result
+            logger.debug(f"Executed '{action_name}' successfully.")
+            return result
+        except wialon.api.WialonError as e:
+            call_record.error = e
+            logger.warning(f"Failed to execute '{action_name}': '{e}'")
+            return
+        finally:
+            self.call_history.append(call_record)
+
+
+class WialonSession:
+    def __init__(
+        self,
+        token: str | None = None,
+        sid: str | None = None,
+        scheme: str = "https",
+        host: str = "hst-api.wialon.com",
+        port: int = 443,
+        log_level: int = 10,
+        log_days: int = 10,
+        debug: bool = False,
+    ) -> None:
+        """
+        Starts or continues a Wialon API session.
+
+        :param token: An optional Wialon API token. Default is :confval:`WIALON_TOKEN`.
+        :type token: :py:obj:`str` | :py:obj:`None`
+        :param sid: An optional Wialon API session id. If provided, the session is continued.
+        :type sid: :py:obj:`str` | :py:obj:`None`
+        :param scheme: HTTP request scheme to use. Default is ``"https"``.
+        :type scheme: :py:obj:`str`
+        :param host: Wialon API host url. Default is ``"hst-api.wialon.com"``.
+        :type host: :py:obj:`str`
+        :param port: Wialon API host port. Default is ``443``.
+        :type port: :py:obj:`int`
+        :param log_level: Level of emitted logs. Default is ``10``.
+        :type log_level: :py:obj:`int`
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+
+        logger.add(
+            f"logs/{self.__class__.__name__}.log",
+            level=log_level,
+            retention=f"{log_days} days",
+            diagnose=debug or settings.DEBUG
+        )
+        self._token = token or settings.WIALON_TOKEN
+        self._username = None
+        self._gis_sid = None
+        self._hw_gp_ip = None
+        self._wsdk_version = None
+        self._uid = None
+        self.wialon_api = Wialon(
+            scheme=scheme,
+            host=host,
+            port=port,
+            sid=sid,
+            log_level=log_level,
+            log_days=log_days,
+            debug=debug,
+        )
+
+    def __str__(self) -> str:
+        return str(self.id)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__}(sid={self.id})"
+
+    def __enter__(self) -> "WialonSession":
+        """
+        Logs into the Wialon API using :py:meth:`login`.
+
+        :raises AssertionError: If the Wialon API token was not set.
+        :returns: A valid Wialon API session.
+        :rtype: :py:obj:`~terminusgps.wialon.session.WialonSession`
+
+        """
+        assert self.token, "Wialon API token was not set"
+        self.login(self.token)
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback) -> None:
+        """
+        Logs out of the session by calling :py:meth:`logout`.
+
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        self.logout()
+
+    @property
+    def active(self) -> bool:
+        """
+        Whether or not the session is logged in.
+
+        :type: :py:obj:`bool`
+        :value: :py:obj:`False`
+
+        """
+        return bool(self.id)
+
+    @property
+    def gis_geocode(self) -> str | None:
+        """
+        Gis geocode URL.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._gis_geocode
+
+    @property
+    def gis_render(self) -> str | None:
+        """
+        Gis rendering URL.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._gis_render
+
+    @property
+    def gis_routing(self) -> str | None:
+        """
+        Gis routing URL.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._gis_routing
+
+    @property
+    def gis_search(self) -> str | None:
+        """
+        Gis search URL.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._gis_search
+
+    @property
+    def gis_sid(self) -> str | None:
+        """
+        Gis session id.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._gis_sid
+
+    @property
+    def host(self) -> str | None:
+        """
+        IP of the client hosting the Wialon session.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._host
+
+    @property
+    def hw_gw_ip(self) -> str | None:
+        """
+        Hardware gateway IP.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._hw_gw_ip
+
+    @property
+    def hw_gw_dns(self) -> str | None:
+        """
+        Hardware gateway domain name, should evaluate to :py:attr:`hw_gw_ip` if present.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._hw_gw_dns
+
+    @property
+    def wsdk_version(self) -> str | None:
+        """
+        The Wialon Source Developer Kit (WSDK) version number of the session.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._wsdk_version
+
+    @property
+    def uid(self) -> str | None:
+        """
+        A Wialon user ID this session is operating as.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+        """
+        return self._uid
+
+    @property
+    def username(self) -> str | None:
+        """
+        A Wialon username the session is operating as.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self._username
+
+    @property
+    def id(self) -> str | None:
+        """
+        Shortcut property for :py:attr:`WialonSession.wialon_api.sid`.
+
+        Returns :py:obj:`None` if the session wasn't logged in.
+
+        :type: :py:obj:`str` | :py:obj:`None`
+        :value: :py:obj:`None`
+
+        """
+        return self.wialon_api.sid
+
+    @property
+    def token(self) -> str:
+        """
+        A Wialon API token set during :py:meth:`WialonSession.__init__`.
+
+        Default token value is :confval:`WIALON_TOKEN`.
+
+        :type: :py:obj:`str`
+        :value: :confval:`WIALON_TOKEN`
+
+        """
+        return str(self._token)
+
+    def login(self, token: str, flags: int = sum([0x1, 0x2, 0x20])) -> str:
+        """
+        Logs into the Wialon API and starts a new session.
+
+        :param token: An active Wialon API token.
+        :type token: :py:obj:`str`
+        :param flags: A login response flag integer.
+        :type flags: :py:obj:`int`
+        :raises WialonError: If the login fails.
+        :raises AssertionError: If the login token was not set.
+        :returns: The new session id.
+        :rtype: :py:obj:`str`
+
+        """
+        try:
+            logger.debug("Logging into Wialon API session...")
+            response = self.wialon_api.token_login(**{"token": token, "fl": flags})
+            self._set_login_response(response)
+            logger.debug(f"Logged into Wialon API session '{response.get('eid')}'")
+            return response.get("eid")
+        except (wialon.api.WialonError, AssertionError) as e:
+            logger.critical(f"Failed to login to Wialon API: '{e}'")
+            raise
+
+    def logout(self) -> None:
+        """
+        Logs out of the Wialon API session.
+
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        logger.debug("Logging out of Wialon API session...")
+        response: dict = self.wialon_api.core_logout({})
+        self.wialon_api.sid = None
+
+        if response.get("error") != 0:
+            logger.warning(f"Failed to logout of session: '{response.get('message')}'")
+        else:
+            logger.debug(
+                f"Logged out after {self.wialon_api.total_calls} Wialon API calls."
+            )
+
+    def _set_login_response(self, login_response: dict) -> None:
+        """
+        Sets the Wialon API session's attributes based on a login response.
+
+        :param login_response: A response returned from :py:meth:`login` or :py:meth:`duplicate`.
+        :type login_response: :py:obj:`dict`
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
+        self.wialon_api.sid = login_response.get("eid")
+        self._gis_geocode = login_response.get("gis_geocode")
+        self._gis_render = login_response.get("gis_render")
+        self._gis_routing = login_response.get("gis_routing")
+        self._gis_search = login_response.get("gis_search")
+        self._gis_sid = login_response.get("gis_sid")
+        self._host = login_response.get("host")
+        self._hw_gw_dns = login_response.get("hw_gw_dns")
+        self._hw_gw_ip = login_response.get("hw_gw_ip")
+        self._uid = login_response.get("user", {}).get("id")
+        self._nm = login_response.get("user", {}).get("nm")
+        self._username = login_response.get("au")
+        self._video_service_url = login_response.get("video_service_url")
+        self._wsdk_version = login_response.get("wsdk_version")
