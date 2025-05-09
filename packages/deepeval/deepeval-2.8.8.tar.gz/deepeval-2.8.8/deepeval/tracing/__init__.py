1b66b1fb8910b8a6f232e1288ceffdf934371bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 06:06:21 +0000

    Add package deepeval/2.8.8/deepeval-2.8.8.tar.gz

---
+++ packages/deepeval/deepeval-2.8.8.tar.gz/deepeval-2.8.8/deepeval/tracing/__init__.py
@@ -0,0 +1,11 @@
+from .tracing import (
+    observe,
+    update_current_span_attributes,
+    update_current_span_test_case,
+    LlmAttributes,
+    RetrieverAttributes,
+    ToolAttributes,
+    AgentAttributes,
+    get_current_trace,
+    trace_manager,
+)
