56341f2dc0f592b2324b8fa1aad60a22721f2803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:27 +0000

    Add package i_principal_central_link/0.1.2/i_principal_central_link-0.1.2.tar.gz

---
+++ packages/i_principal_central_link/i_principal_central_link-0.1.2.tar.gz/i_principal_central_link-0.1.2/src/i_principal_central_link/core.py
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def i_principal_central_link():
+
+
+
+    global i
+
+
+    i = {}
+
+
+    i["link"] = r"https://github.com/IPrincipalCentral/i_principal_central"
+
+
+
+    print(i["link"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
