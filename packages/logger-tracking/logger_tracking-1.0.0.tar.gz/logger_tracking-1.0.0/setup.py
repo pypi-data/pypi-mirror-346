5d5209b165cfef5d5b0a5718dc4d400c4211f845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 16:08:52 +0000

    Add package logger-tracking/1.0.0/logger_tracking-1.0.0.tar.gz

---
+++ packages/logger-tracking/logger_tracking-1.0.0.tar.gz/logger_tracking-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='logger_tracking',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Log parameters, metrics, and artifacts using Logger Tracking.',
+    author='Saurav Kumar',
+    author_email='Saurav.Kumar@cognizant.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
