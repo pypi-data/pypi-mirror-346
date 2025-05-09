18f4149f31ca9befea8ffb076b451a7099f9a77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:27 +0000

    Add package flowtask/5.7.21/flowtask-5.7.21-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/flowtask/flowtask-5.7.21-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/flowtask/components/ImageFeatures/plugins/__init__.py
@@ -0,0 +1,15 @@
+from .yolo import YOLOPlugin
+from .vision import VisionTransformerPlugin
+from .hash import ImageHashPlugin
+from .abstract import ImagePlugin
+from .exif import EXIFPlugin
+from .zerodetect import ZeroShotDetectionPlugin
+
+
+PLUGINS = {
+    "exif": EXIFPlugin,
+    "hash": ImageHashPlugin,
+    "yolo": YOLOPlugin,
+    "vectorization": VisionTransformerPlugin,
+    "zeroshot": ZeroShotDetectionPlugin,
+}
