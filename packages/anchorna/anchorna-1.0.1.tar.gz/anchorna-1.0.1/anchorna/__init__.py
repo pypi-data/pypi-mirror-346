cbebd83de72eb8ae70a70cf1ad6880638e0ce590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:24 +0000

    Add package anchorna/1.0.1/anchorna-1.0.1.tar.gz

---
+++ packages/anchorna/anchorna-1.0.1.tar.gz/anchorna-1.0.1/anchorna/__init__.py
@@ -0,0 +1,10 @@
+"""
+AnchoRNA
+
+Find anchors in RNA/DNA sequences
+"""
+
+from anchorna.core import combine, cutout, find_my_anchors
+from anchorna.io import read_anchors, jalview_features
+
+__version__ = '1.0.1'
