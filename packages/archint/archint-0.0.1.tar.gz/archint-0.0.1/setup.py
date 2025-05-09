0327add3cf8ca5db733449a7fad36f76e882d32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:11 +0000

    Add package archint/0.0.1/archint-0.0.1.tar.gz

---
+++ packages/archint/archint-0.0.1.tar.gz/archint-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="archint",
+    version="0.0.1",
+    description="archint",
+    author="Anish Agrawal",
+    author_email="your.email@example.com",
+    packages=["archint"],
+)
