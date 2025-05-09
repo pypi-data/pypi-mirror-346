40541c9df12156025e67321a8f1aaeea613230e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 16:09:44 +0000

    Add package pyinitpro/0.1.6/pyinitpro-0.1.6.tar.gz

---
+++ packages/pyinitpro/pyinitpro-0.1.6.tar.gz/pyinitpro-0.1.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import pathlib
+
+this_directory = pathlib.Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="pyinitpro",
+    version="0.1.6",
+    py_modules=["pyinit"],
+    include_package_data=True,
+    packages=["pyinit"],
+    package_data={"pyinit": ["templates/*.tpl"]},
+    install_requires=[
+        "colorama>=0.4.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pyinitpro=pyinit.__main__:main",
+        ],
+    },
+    author="Martins O Jojolola",
+    description="A CLI tool to scaffold Python projects with venv and folder structure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Martins-O/pyinit.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.1",
+    license="MIT",
+    keywords="python cli scaffold virtualenv project setup",
+)
