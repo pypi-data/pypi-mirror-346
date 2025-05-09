0835b1ad3dc84b3f94467049a859f90e384ce6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 15:06:49 +0000

    Add package speedtable/1.0.5/speedtable-1.0.5-cp310-cp310-musllinux_1_2_i686.whl

---
+++ packages/speedtable/speedtable-1.0.5-cp310-cp310-musllinux_1_2_i686.whl/speedtable/speedtable.pyi
@@ -0,0 +1,10 @@
+def render_table(
+    table_data: dict,
+    header_color: str = ...,
+    border_color: str = ...,
+    body_color: str = ...,
+    type_color: str = ...,
+    title_text: str = ...,
+    title_color: str = ...
+) -> str:
+    ...
