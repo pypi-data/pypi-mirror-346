969e76443d6766da6c8609b39d2f78f4efbe75f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 16:09:17 +0000

    Add package grizzlo/0.1.1/grizzlo-0.1.1.tar.gz

---
+++ packages/grizzlo/grizzlo-0.1.1.tar.gz/grizzlo-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grizzlo",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=1.3.0,<2.0.0",
+        "matplotlib>=3.0.0",
+        "numpy>=1.18.0"
+    ],
+    author="Your Name",
+    author_email="datawithshah@gmail.com",
+    description="High-performance, scalable Pandas-compatible analytics library",
+    url="https://github.com/datwithshah/grizzlo",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
