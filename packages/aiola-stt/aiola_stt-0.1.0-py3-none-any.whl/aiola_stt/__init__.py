f978cc3cdf705031b1e63400fb4a26c4387347cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:05:46 +0000

    Add package aiola-stt/0.1.0/aiola_stt-0.1.0-py3-none-any.whl

---
+++ packages/aiola-stt/aiola_stt-0.1.0-py3-none-any.whl/aiola_stt/__init__.py
@@ -0,0 +1,5 @@
+from .client import AiolaSttClient
+from .errors import AiolaSocketError, AiolaSocketErrorCode
+
+__version__ = "0.0,1"
+__all__ = ['AiolaSttClient', 'AiolaSocketError', 'AiolaSocketErrorCode']
