4f358dca79016bb8f3795e896a724eb176763f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 16:08:41 +0000

    Add package skpro/2.9.1/skpro-2.9.1-py3-none-any.whl

---
+++ packages/skpro/skpro-2.9.1-py3-none-any.whl/skpro/tests/_config.py
@@ -0,0 +1,11 @@
+"""Test configs."""
+
+# list of str, names of estimators to exclude from testing
+# WARNING: tests for these estimators will be skipped
+EXCLUDE_ESTIMATORS = [
+    "DummySkipped",
+    "ClassName",  # exclude classes from extension templates
+]
+
+
+EXCLUDED_TESTS = {"GLMRegressor": ["test_online_update"]}
