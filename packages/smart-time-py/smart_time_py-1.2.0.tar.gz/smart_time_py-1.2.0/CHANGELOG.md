08394525433b9f341dbec8f3ab8bf292d25f414e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 07:06:17 +0000

    Add package smart-time-py/1.2.0/smart_time_py-1.2.0.tar.gz

---
+++ packages/smart-time-py/smart_time_py-1.2.0.tar.gz/smart_time_py-1.2.0/CHANGELOG.md
@@ -0,0 +1,32 @@
+# 📜 Changelog
+
+
+## [1.2.0] - 2025-05-08
+### Adicionado
+- 📊 Módulo de análise temporal com funcionalidades avançadas:
+  - Agrupamento temporal (diário, semanal, mensal, trimestral, anual)
+  - Cálculo de estatísticas temporais
+  - Detecção de padrões em séries temporais
+  - Análise de sazonalidade
+- 🧪 Testes unitários abrangentes para o módulo de análise
+- 📈 Melhorias na documentação e exemplos de uso
+
+## [1.1.3] - 2025-04-17
+### Adicionado
+- 🚀 Novas funções e validações
+
+## [1.1.2] - 2025-04-17
+### Adicionado
+- 🚀 Novas funções e validações
+
+## [1.1.1] - 2025-02-24
+### Adicionado
+- 🚀 Novas funções e validações
+
+## [0.1.1] - 2025-02-24
+### Adicionado
+- 🚀 Ajuste na rota do repositório.
+
+## [0.1.0] - 2025-02-24
+### Adicionado
+- 🚀 Primeira versão com conversão de data com smart_time_py.
---
+++ packages/smart-time-py/smart_time_py-1.2.0.tar.gz/smart_time_py-1.2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="smart_time_py",
+    version="1.2.0",
+    author="Roberto Lima",
+    author_email="robertolima.izphera@gmail.com",
+    description="Conversão inteligente de datas e horas em Python.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/robertolima-dev/smart-time-py",
+    packages=find_packages(),
+    install_requires=[
+        "pytest>=6.0.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+    license="MIT",
+    project_urls={
+        "Homepage": "https://github.com/robertolima-dev/smart-time-py", # noqa501
+        "Repository": "https://github.com/robertolima-dev/smart-time-py", # noqa501
+        "Issues": "https://github.com/robertolima-dev/smart-time-py/issues", # noqa501
+    },
+)
