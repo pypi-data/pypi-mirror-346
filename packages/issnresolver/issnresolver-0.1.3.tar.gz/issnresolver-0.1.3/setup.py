0d75b93772aaccd42f2be7a6c750570e1e2d7d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 06:05:38 +0000

    Add package issnresolver/0.1.3/issnresolver-0.1.3.tar.gz

---
+++ packages/issnresolver/issnresolver-0.1.3.tar.gz/issnresolver-0.1.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='issnresolver',
+    version='0.1.3',
+
+    # Editable fields:
+    description='Fast async ISSN ↔ ISSN-L resolver using the ISSN Portal API',
+    long_description = open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+
+    author='Akmal Setiawan',
+    author_email='your@email.com',
+    url='https://github.com/acepocalypse/issnresolver',
+    project_urls={
+        'Source': 'https://github.com/acepocalypse/issnresolver',
+        'Tracker': 'https://github.com/acepocalypse/issnresolver/issues',
+        'Documentation': 'https://github.com/acepocalypse/issnresolver#readme',
+    },
+
+    license='MIT',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=[
+        'aiohttp>=3.8',
+        'pandas>=1.3',
+        'tqdm>=4.0',
+    ],
+    extras_require={
+        "notebook": ["nest_asyncio"]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
