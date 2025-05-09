7fa50308196137fb8aed14f2cc9e5e377ebb03bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:05 +0000

    Add package yousif-raiyan-pip-package/1.4.0/yousif_raiyan_pip_package-1.4.0.tar.gz

---
+++ packages/yousif-raiyan-pip-package/yousif_raiyan_pip_package-1.4.0.tar.gz/yousif_raiyan_pip_package-1.4.0/src/yousif_raiyan_pip_package/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.4.0"
+
+from .eeg_graph_representation import EEGGraphProcessor
+from .edf_loader import EDFLoader
+from .trigger_detector import TriggerDetector
+
+__all__ = ["EEGGraphProcessor",
+           "EDFLoader",
+           "TriggerDetector"]
