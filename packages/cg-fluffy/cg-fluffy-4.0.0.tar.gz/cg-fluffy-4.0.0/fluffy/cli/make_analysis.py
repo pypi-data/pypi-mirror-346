b8255f76117839db494d0280e945a993c0c27b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:05:43 +0000

    Add package cg-fluffy/4.0.0/cg-fluffy-4.0.0.tar.gz

---
+++ packages/cg-fluffy/cg-fluffy-4.0.0.tar.gz/cg-fluffy-4.0.0/fluffy/cli/make_analysis.py
@@ -0,0 +1,70 @@
+"""CLI to run the analysis"""
+import logging
+
+import shutil
+
+from fluffy.config import check_configs
+from fluffy.deliverables import print_deliverables
+from fluffy.status import print_status
+from fluffy.workflows.analyse_samples import analyse_workflow
+
+
+LOG = logging.getLogger(__name__)
+
+def analyse(ctx, skip_preface, dry_run ,batch_ref,bwa_mem):
+    """Run the pipeline to call NIPT"""
+    LOG.info("Running fluffy analyse")
+
+    samples = ctx["samples"]
+    configs = ctx["configs"]
+    configs["sample_sheet"] = ctx["sample_sheet"]	
+    project_dir=ctx["project"]
+    slurm_api = ctx["slurm_api"]
+
+    config_path=configs["out"] / configs["name"]
+    if config_path.exists():
+        LOG.warning("Config already exists, create new dir or remove config")
+        quit()
+
+
+    LOG.info("Copy config to %s", config_path)
+    shutil.copy(configs["config_path"], str(config_path))
+    try:
+        check_configs(configs, skip_preface=skip_preface)
+    except FileNotFoundError as err:
+        print("config not found")
+        quit()
+
+    summarise_prefix="summary"
+    project_ids=set([])
+    for sample in samples:
+         if "project" in sample:
+             project_ids.add(sample["project"])
+         else:
+             continue
+
+    if len(project_ids) == 1:
+         summarise_prefix=list(project_ids)[0]
+
+    print_deliverables(
+        output_dir=configs["out"],
+        batch_ref=batch_ref,
+        project_dir=project_dir,
+        project_id=summarise_prefix,
+        samples=samples,
+    )
+
+    print_status(
+        output_dir=configs["out"],
+    )
+
+    configs["project_id"]=summarise_prefix
+    analyse_workflow(
+        samples=list(samples),
+        configs=configs,
+        skip_preface=skip_preface,
+        slurm_api=slurm_api,
+        dry_run=dry_run,
+        batch_ref=batch_ref,
+        bwa_mem=bwa_mem,
+    )
---
+++ packages/cg-fluffy/cg-fluffy-4.0.0.tar.gz/cg-fluffy-4.0.0/fluffy/commands/bwa.py
@@ -0,0 +1,102 @@
+"""Commands that use bwa"""
+
+
+def get_align_command(
+    fastq: str,
+    out_dir: str,
+    out_prefix: str,
+    reference: str,
+    singularity: str,
+    sample_id: str,
+    threads: str,
+    tmp_dir: str,
+    read: str = None,
+) -> str:
+    """create a command to run bwa align. Read can be r1, r2 or none"""
+    read_prefix = ".sai"
+    if read == "r1":
+        read_prefix = "_R1.sai"
+    if read == "r2":
+        read_prefix = "_R2.sai"
+
+    fastqc_prefix = read_prefix.replace(".sai", "")
+    cmd = (
+        f"{singularity} bwa aln -n 0 -k 0 -t {threads} {reference} {fastq} > "
+        f"{out_prefix}{read_prefix}\n"
+        f"{singularity} cat {fastq} | "
+        f"{singularity} fastqc stdin:{sample_id}{fastqc_prefix} -d {tmp_dir} -o {out_dir}/{sample_id}"
+    )
+    return cmd
+
+
+def get_samse_command(
+    fastq: str, out_prefix: str, reference: str, singularity: str, threads: str
+) -> str:
+    """create a command for running bwa samsw"""
+    cmd = (
+        f"{singularity} bwa samse -n -1 {reference} {out_prefix}.sai {fastq} | {singularity} samtools sort - "
+        f"-@ {threads} -T {out_prefix}.tmp > {out_prefix}.tmp.bam"
+    )
+    return cmd
+
+
+def get_sampe_command(
+    fastq1: str,
+    fastq2: str,
+    out_prefix: str,
+    reference: str,
+    singularity: str,
+    threads: str,
+) -> str:
+    """create a command for running bwa sampe"""
+    cmd = (
+        f"{singularity} bwa sampe -n -1 {reference} "
+        f"{out_prefix}_R1.sai {out_prefix}_R2.sai {fastq1} {fastq2} | {singularity} samtools sort - -@ {threads} "
+        f"-T {out_prefix}.tmp > {out_prefix}.tmp.bam"
+    )
+    return cmd
+
+def get_bwa_mem_command(
+    singularity: str,
+    reference: str,
+    threads: str,
+    fastq: list,
+    out_prefix: str,
+    single_end: bool,
+    sample_id: str,
+    tmp_dir: str,
+    out_dir: str,
+) -> str:
+     """Create a command for running bwa mem"""
+
+     if single_end:
+        cmd= (
+        f"{singularity} cat {fastq[0]} | "
+        f"{singularity} fastqc stdin:{sample_id} -d {tmp_dir} -o {out_dir}/{sample_id}\n"
+        f"{singularity} bwa mem -t {threads} {reference} {fastq[0]} | {singularity} samtools sort - -@ {threads} "
+        f"-T {out_prefix}.tmp > {out_prefix}.tmp.bam"
+        )
+
+     else:
+        cmd= (
+        f"{singularity} cat {fastq[0]} | "
+        f"{singularity} fastqc stdin:{sample_id}.R1 -d {tmp_dir} -o {out_dir}/{sample_id}\n"
+        f"{singularity} cat {fastq[1]} | "
+        f"{singularity} fastqc stdin:{sample_id}.R2 -d {tmp_dir} -o {out_dir}/{sample_id}\n"
+        f"{singularity} bwa mem -t {threads} {reference} {fastq[0]} {fastq[1]} | {singularity} samtools sort - -@ {threads} "
+        f"-T {out_prefix}.tmp > {out_prefix}.tmp.bam"
+        )
+     return cmd
+
+
+def get_bamsormadup_command(
+    out_prefix: str, singularity: str, tmp_dir: str
+) -> str:
+    """Create a command for running bamorsamdup"""
+    cmd = (
+        f"{singularity} picard MarkDuplicates TMP_DIR={tmp_dir} I={out_prefix}.tmp.bam O={out_prefix}.bam "
+        f"M={out_prefix}.md.txt CREATE_INDEX=true VALIDATION_STRINGENCY=LENIENT\n"
+	f"{singularity} samtools stats {out_prefix}.bam > {out_prefix}.stats.txt\n"
+        f"rm {out_prefix}.tmp.bam"
+    )
+    return cmd
