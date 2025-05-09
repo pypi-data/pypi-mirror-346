ca7566bd8f423691402616693eb71c1d137ca7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 05:05:43 +0000

    Add package serato-tools/2.0.0/serato_tools-2.0.0-py3-none-any.whl

---
+++ packages/serato-tools/serato_tools-2.0.0-py3-none-any.whl/serato_tools/utils/__init__.py
@@ -0,0 +1,20 @@
+from typing import Iterable, TypeVar, Union, cast, Any
+
+T = TypeVar("T")
+
+
+def to_array(x: Union[T, Iterable[T]]) -> Iterable[T]:
+    if isinstance(x, (str, bytes)):
+        return cast(list[T], [x])
+    if isinstance(x, Iterable):
+        return x
+    return [x]
+
+
+class DataTypeError(Exception):
+    def __init__(
+        self, value: Any, expected_type: type | Iterable[type], field: str | None
+    ):
+        super().__init__(
+            f"value must be {' or '.join(e.__name__ for e in to_array(expected_type))} when field is {field} (type: {type(value).__name__}) (value: {str(value)})"
+        )
