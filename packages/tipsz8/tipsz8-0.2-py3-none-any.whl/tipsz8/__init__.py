c27fec4a16939b99ff751949f7077c76f8fc8843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:01 +0000

    Add package tipsz8/0.2/tipsz8-0.2-py3-none-any.whl

---
+++ packages/tipsz8/tipsz8-0.2-py3-none-any.whl/tipsz8/__init__.py
@@ -0,0 +1,4 @@
+from .useFileHook import greet, saveLocalData, loadLocalData, clear_cache, getFileUTime
+# from .module2 import calculate
+
+__all__ = ["greet","saveLocalData","loadLocalData","clear_cache","getFileUTime"]  # 定义导出的内容
