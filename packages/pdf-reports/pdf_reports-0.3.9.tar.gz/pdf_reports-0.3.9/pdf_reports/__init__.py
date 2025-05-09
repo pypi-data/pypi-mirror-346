deb629920ed13d7c05d18ed9880c74f0a694d01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 06:06:36 +0000

    Add package pdf-reports/0.3.9/pdf_reports-0.3.9.tar.gz

---
+++ packages/pdf-reports/pdf_reports-0.3.9.tar.gz/pdf_reports-0.3.9/pdf_reports/__init__.py
@@ -0,0 +1,16 @@
+from .pdf_reports import (
+    pug_to_html,
+    write_report,
+    ReportWriter,
+    GLOBALS,
+    preload_stylesheet,
+)
+from .tools import (
+    dataframe_to_html,
+    style_table_rows,
+    add_css_class,
+    figure_data,
+    JupyterPDF,
+)
+
+from .version import __version__
