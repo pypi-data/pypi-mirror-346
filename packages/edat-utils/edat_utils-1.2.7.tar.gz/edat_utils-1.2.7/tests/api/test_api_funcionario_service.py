697bb289f3e0f7a28052274ae79e5bae26d5c952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 06:05:46 +0000

    Add package edat-utils/1.2.7/edat_utils-1.2.7.tar.gz

---
+++ packages/edat-utils/edat_utils-1.2.7.tar.gz/edat_utils-1.2.7/tests/api/test_api_funcionario_service.py
@@ -0,0 +1,18 @@
+from typing import List, Union
+from edat_utils.api import ApiFuncionarioService
+from edat_utils.api.models import TipoUsuario, Usuario
+
+
+def test_get_funcionarios(get_api_funcionario_service: ApiFuncionarioService):
+    query = f'startWith: {{nome: "Mar"}}'
+    funcionarios: Union[List[Usuario], None] = get_api_funcionario_service.get(query=query)
+
+    if not funcionarios:
+        assert False
+
+    assert len(funcionarios) > 0
+
+    for funcionario in funcionarios:
+        assert funcionario.tipo_usuario in [TipoUsuario.FUNCIONARIO, TipoUsuario.FUNCAMP, TipoUsuario.DOCENTE]
+        assert funcionario.email
+        assert funcionario.telefone
