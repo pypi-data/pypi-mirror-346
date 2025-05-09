bbe48c36fc05f454acbc0cb647fac174c3a22381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:25 +0000

    Add package calculadoraapi-fastapi-breno/0.1.5/calculadoraapi_fastapi_breno-0.1.5.tar.gz

---
+++ packages/calculadoraapi-fastapi-breno/calculadoraapi_fastapi_breno-0.1.5.tar.gz/calculadoraapi_fastapi_breno-0.1.5/app/models.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+class Operandos(BaseModel):
+    num1: float
+    num2: float
---
+++ packages/calculadoraapi-fastapi-breno/calculadoraapi_fastapi_breno-0.1.5.tar.gz/calculadoraapi_fastapi_breno-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calculadoraapi-fastapi-breno",
+    version="0.1.5",
+    description="Calculadora API utilizando FastAPI",
+    author="Breno Amâncio",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi>=0.95.2,<0.100.0",  # Compatível com Pydantic v1
+        "pydantic==1.10.7",
+        "uvicorn==0.22.0"
+    ],
+    include_package_data=True,
+    python_requires=">=3.9, <=3.12.5"
+)
