48775f2f2d219ea577dbd2af81ba1886b09d4459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 16:08:41 +0000

    Add package piqtree/0.5.0/piqtree-0.5.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/piqtree/piqtree-0.5.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/piqtree/model/__init__.py
@@ -0,0 +1,37 @@
+"""Models available in IQ-TREE."""
+
+from ._freq_type import FreqType, get_freq_type
+from ._model import Model, make_model
+from ._options import available_freq_type, available_models, available_rate_type
+from ._rate_type import (
+    DiscreteGammaModel,
+    FreeRateModel,
+    RateModel,
+    RateType,
+    get_rate_type,
+)
+from ._substitution_model import (
+    AaModel,
+    DnaModel,
+    SubstitutionModel,
+    get_substitution_model,
+)
+
+__all__ = [
+    "AaModel",
+    "DiscreteGammaModel",
+    "DnaModel",
+    "FreeRateModel",
+    "FreqType",
+    "Model",
+    "RateModel",
+    "RateType",
+    "SubstitutionModel",
+    "available_freq_type",
+    "available_models",
+    "available_rate_type",
+    "get_freq_type",
+    "get_rate_type",
+    "get_substitution_model",
+    "make_model",
+]
