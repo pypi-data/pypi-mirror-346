a63bbe66c4d933fd70aa66c0321663571df99b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:25 +0000

    Add package calculadoraapi-fastapi-breno/0.1.5/calculadoraapi_fastapi_breno-0.1.5-py3-none-any.whl

---
+++ packages/calculadoraapi-fastapi-breno/calculadoraapi_fastapi_breno-0.1.5-py3-none-any.whl/app/operations.py
@@ -0,0 +1,21 @@
+def soma(operando: Operandos):
+    return {"resultado": operando.num1 + operando.num2}
+
+def subtracao(operando: Operandos):
+    return {"resultado": operando.num1 - operando.num2}
+
+def multiplicacao(operando: Operandos):
+    return {"resultado": operando.num1 * operando.num2}
+
+def divisao(operando: Operandos):
+    if operando.num2 == 0:
+        return {"erro": "Divisão por zero não é permitida."}
+    return {"resultado": operando.num1 / operando.num2}
+
+def potencia(operando: Operandos):
+    return {"resultado": operando.num1 ** operando.num2}
+
+def raiz(operando: Operandos):
+    if operando.num1 < 0:
+        return {"erro": "Raiz quadrada de número negativo não é permitida."}
+    return {"resultado": operando.num1 ** (1 / operando.num2)}
