64a98e5080cf631f9b8b71e2323f05be72e35775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:02 +0000

    Add package auth0-ai-langchain/1.0.0b1/auth0_ai_langchain-1.0.0b1.tar.gz

---
+++ packages/auth0-ai-langchain/auth0_ai_langchain-1.0.0b1.tar.gz/auth0_ai_langchain-1.0.0b1/auth0_ai_langchain/federated_connections/__init__.py
@@ -0,0 +1,7 @@
+from auth0_ai.interrupts.federated_connection_interrupt import (
+    FederatedConnectionError as FederatedConnectionError,
+    FederatedConnectionInterrupt as FederatedConnectionInterrupt
+)
+
+from auth0_ai.authorizers.federated_connection_authorizer import get_credentials_for_connection as get_credentials_for_connection
+from .federated_connection_authorizer import FederatedConnectionAuthorizer as FederatedConnectionAuthorizer
