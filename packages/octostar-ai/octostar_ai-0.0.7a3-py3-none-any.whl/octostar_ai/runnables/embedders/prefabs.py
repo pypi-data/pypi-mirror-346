c4b122f857d8aefe37eed74e2d99d62f2da01c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:05:45 +0000

    Add package octostar-ai/0.0.7a3/octostar_ai-0.0.7a3-py3-none-any.whl

---
+++ packages/octostar-ai/octostar_ai-0.0.7a3-py3-none-any.whl/octostar_ai/runnables/embedders/prefabs.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from octostar_ai.utils.gpu import get_device
+
+
+def default_rag_embedder(openai_api_key: Optional[str] = None, model: Optional[str] = None, device: Optional[str] = None):
+    """Get the default embedder for Retrieval-Augmented Generation (RAG)."""
+    if openai_api_key:
+        from octostar_ai.runnables.embedders.commons import OpenAIEmbeddings
+
+        return OpenAIEmbeddings(
+            model=model or "text-embedding-3-small", model_version="1", api_key=openai_api_key
+        )
+
+    from octostar_ai.runnables.embedders.commons import HuggingFaceEmbeddings
+
+    model_name = model or "sentence-transformers/all-mpnet-base-v2"
+    model_kwargs = {"device": device or get_device()}
+    encode_kwargs = {"normalize_embeddings": False}
+    return HuggingFaceEmbeddings(
+        model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
+    )
