5a1bdc9f59aab58d1518c805fea40bb07875acca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 05:05:46 +0000

    Add package helix-py/0.2.1/helix_py-0.2.1.tar.gz

---
+++ packages/helix-py/helix_py-0.2.1.tar.gz/helix_py-0.2.1/helix/__init__.py
@@ -0,0 +1,8 @@
+# env vars?
+# debug output on specific env var like DEBUG=1
+
+from helix.client import Client, Query
+from helix.loader import Loader
+from helix.types import Payload
+
+__version__ = "0.1.0"
