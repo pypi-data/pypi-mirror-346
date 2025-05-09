574ac04924e5968e9541d9c84580d63195db2c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:05:48 +0000

    Add package exomiser-ml/0.1.0/exomiser_ml-0.1.0-py3-none-any.whl

---
+++ packages/exomiser-ml/exomiser_ml-0.1.0-py3-none-any.whl/exomiser_ml/main.py
@@ -0,0 +1,32 @@
+import click
+
+from exomiser_ml.data.create_features.add_features import add_features_command
+from exomiser_ml.data.split_data.split_train_and_test import split_data
+from exomiser_ml.models.logistic_regression import run_logistic_regression, run_lr_pipeline
+from exomiser_ml.models.random_forest import run_random_forest, run_rf_pipeline
+from exomiser_ml.models.xgboost import run_xgboost, run_xgboost_pipeline
+from exomiser_ml.post_process.post_process import post_process_test_dir_command
+
+
+@click.group()
+def main():
+    """main command group"""
+
+
+@main.group()
+def ml():
+    """machine learning utilities."""
+
+
+ml.add_command(run_lr_pipeline)
+ml.add_command(add_features_command)
+ml.add_command(split_data)
+ml.add_command(run_logistic_regression)
+ml.add_command(post_process_test_dir_command)
+ml.add_command(run_random_forest)
+ml.add_command(run_rf_pipeline)
+ml.add_command(run_xgboost)
+ml.add_command(run_xgboost_pipeline)
+
+if __name__ == "__main__":
+    main()
