c1fe64e8589ab66e8632c215aa927fcf2e6a3bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:06 +0000

    Add package dodopayments/1.22.0/dodopayments-1.22.0.tar.gz

---
+++ packages/dodopayments/dodopayments-1.22.0.tar.gz/dodopayments-1.22.0/src/dodopayments/types/subscription_charge_params.py
@@ -0,0 +1,16 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, TypedDict
+
+__all__ = ["SubscriptionChargeParams"]
+
+
+class SubscriptionChargeParams(TypedDict, total=False):
+    product_price: Required[int]
+    """The product price.
+
+    Represented in the lowest denomination of the currency (e.g., cents for USD).
+    For example, to charge $1.00, pass `100`.
+    """
