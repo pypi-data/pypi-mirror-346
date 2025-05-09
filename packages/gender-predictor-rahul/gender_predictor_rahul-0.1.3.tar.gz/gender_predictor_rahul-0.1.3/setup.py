1deeec79285dce762eb48adfd78b7e5b0d79e326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 16:08:37 +0000

    Add package gender-predictor-rahul/0.1.3/gender_predictor_rahul-0.1.3.tar.gz

---
+++ packages/gender-predictor-rahul/gender_predictor_rahul-0.1.3.tar.gz/gender_predictor_rahul-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gender_predictor_rahul',
+    version='0.1.3',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'gender_predictor_module': [
+            'gender_predictor_module/tokenizer.pkl',
+            'gender_predictor_module/gender_predictor.pkl',
+        ],
+    },
+    install_requires=[
+        'keras',
+        'tensorflow',
+    ],
+    author='Rahul Patel',
+    description='Gender prediction from names using a trained ML model',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',  # Optional: specify the required Python version
+)
