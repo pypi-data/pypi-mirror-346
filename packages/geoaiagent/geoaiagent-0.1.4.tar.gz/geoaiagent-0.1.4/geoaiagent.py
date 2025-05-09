e27f523b4fddb908d8c52a83e70039686fea001d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 05:05:50 +0000

    Add package geoaiagent/0.1.4/geoaiagent-0.1.4.tar.gz

---
+++ packages/geoaiagent/geoaiagent-0.1.4.tar.gz/geoaiagent-0.1.4/geoaiagent.py
@@ -0,0 +1,29 @@
+from mcp.server import FastMCP
+
+
+server = FastMCP("geoaiagent")
+    
+@server.tool()
+async def add_geo(lat: float, lon: float) -> float:
+    """
+    Adds a new geolocation to the database.
+    """
+    return lat + lon
+@server.tool()
+async def sub_geo(lat: float, lon: float) -> float:
+    """
+    Subtracts a geolocation from the database.
+    """
+    return lat - lon
+
+# @server.list_tools()
+# async def list_tools() -> list:
+#     """
+#     Lists all available tools.
+#     """
+#     return ["add_geo", "sub_geo"]
+
+def main():
+    server.run()
+    print("Hello from aiagent!")
+
