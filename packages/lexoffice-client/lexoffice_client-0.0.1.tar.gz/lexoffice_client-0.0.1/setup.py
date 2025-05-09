b83d88ba36902d63322e15a07885e64354a5ec04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:32 +0000

    Add package lexoffice-client/0.0.1/lexoffice_client-0.0.1.tar.gz

---
+++ packages/lexoffice-client/lexoffice_client-0.0.1.tar.gz/lexoffice_client-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lexoffice-client",
+    version="0.0.1",
+    author="James Maguire",
+    author_email="jvm986@gmail.com",
+    description="A Python client for the Lexoffice API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jvm986/lexoffice-client",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],  # Add dependencies here
+)
