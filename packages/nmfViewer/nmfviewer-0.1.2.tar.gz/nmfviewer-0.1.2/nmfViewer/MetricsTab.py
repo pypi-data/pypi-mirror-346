6e3f996a9e0a74557c202af21e6193e14db41fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 16:09:13 +0000

    Add package nmfViewer/0.1.2/nmfviewer-0.1.2.tar.gz

---
+++ packages/nmfViewer/nmfviewer-0.1.2.tar.gz/nmfviewer-0.1.2/nmfViewer/MetricsTab.py
@@ -0,0 +1,8 @@
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QVBoxLayout, QWidget, QPushButton, QFileDialog
+import pandas as pd
+
+
+class MetricsTab(QWidget):
+    pass
