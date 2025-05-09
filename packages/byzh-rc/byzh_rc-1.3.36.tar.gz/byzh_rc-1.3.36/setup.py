f61183f9134617c07d84ac532caa32dbb353dfea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:28 +0000

    Add package byzh-rc/1.3.36/byzh_rc-1.3.36.tar.gz

---
+++ packages/byzh-rc/byzh_rc-1.3.36.tar.gz/byzh_rc-1.3.36/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import byzh_rc
+setup(
+    name='byzh_rc',
+    version=byzh_rc.__version__,
+    author="byzh_rc",
+    description="更方便的深度学习",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        # 依赖列表
+    ],
+)
