cbe572ac9b9b3f3e677e5e9b70bc95d1280e8740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:01 +0000

    Add package model-framework/0.1.0/model_framework-0.1.0-py3-none-any.whl

---
+++ packages/model-framework/model_framework-0.1.0-py3-none-any.whl/mframework/__init__.py
@@ -0,0 +1,6 @@
+MODULE_NAME = 'mframework'
+
+from ._mframework import FeatureSpec, ModelSpec, SchemadictValidators
+from ._utils import UIDDict
+from ._docgen import doc2rst
+from ._log import _plogger as log
