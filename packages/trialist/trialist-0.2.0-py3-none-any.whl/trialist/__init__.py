656c44b888f4c8f4fb4c7ce75a8bf2d7afa0d302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 05:05:10 +0000

    Add package trialist/0.2.0/trialist-0.2.0-py3-none-any.whl

---
+++ packages/trialist/trialist-0.2.0-py3-none-any.whl/trialist/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2025-present Yiannis Charalambous <yiannis128@hotmail.com>
+#
+# SPDX-License-Identifier: GPLv3
+
+from trialist.experiment import Checkpoint, Experiment, Trial
+from trialist.trial_viewer import TrialViewer
+
+__all__ = [
+    "Checkpoint",
+    "Experiment",
+    "Trial",
+    "TrialViewer",
+]
