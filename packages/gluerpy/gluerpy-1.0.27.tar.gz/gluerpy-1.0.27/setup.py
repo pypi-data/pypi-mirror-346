2b19987e94d31d2957472e207333794291b9760d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 04:06:03 +0000

    Add package gluerpy/1.0.27/gluerpy-1.0.27.tar.gz

---
+++ packages/gluerpy/gluerpy-1.0.27.tar.gz/gluerpy-1.0.27/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+# autossh -M 0 -R gluer.serveo.net:443:localhost:9001 serveo.net
+setup(
+    name="gluerpy",
+    version="1.0.27",
+    packages=find_packages(),
+    install_requires=['redis'],  # List your dependencies here
+    description="Python library to connect to gluer services",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://www.gluer.io",  # GitHub repo or project page
+    author="Thiago Magro",
+    author_email="thiago.magro@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
