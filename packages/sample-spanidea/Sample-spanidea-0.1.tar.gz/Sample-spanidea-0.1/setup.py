2d329922e3802cda9210236c4cd4bbd16333aacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:06 +0000

    Add package sample-spanidea/0.1/Sample-spanidea-0.1.tar.gz

---
+++ packages/sample-spanidea/Sample-spanidea-0.1.tar.gz/Sample-spanidea-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Sample-spanidea",
+    version="0.1",
+    packages=find_packages(),
+    description="A sample module with hello function",
+    author="Nikhil",
+    author_email="vidhani40@gmail.com",
+)
