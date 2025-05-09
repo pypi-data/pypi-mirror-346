849276a43bbff5b7bc3d2741403dfffa8d68d0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 15:06:30 +0000

    Add package ff-python-package/0.1.0/ff_python_package-0.1.0.tar.gz

---
+++ packages/ff-python-package/ff_python_package-0.1.0.tar.gz/ff_python_package-0.1.0/ff_python_package/__init__.py
@@ -0,0 +1,5 @@
+from .file1 import function1
+from .file2 import function2
+from .file3 import function3
+
+__all__ = ["function1", "function2", "function3"]
---
+++ packages/ff-python-package/ff_python_package-0.1.0.tar.gz/ff_python_package-0.1.0/ff_python_package/file2.py
@@ -0,0 +1,2 @@
+def function2():
+    print("This is function 2 from file2.py")
