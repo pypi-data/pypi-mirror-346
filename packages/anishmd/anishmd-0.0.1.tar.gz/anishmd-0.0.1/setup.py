522cc9c69dc3c05b4b8d02a72346bc210ed0ab46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 14:06:12 +0000

    Add package anishmd/0.0.1/anishmd-0.0.1.tar.gz

---
+++ packages/anishmd/anishmd-0.0.1.tar.gz/anishmd-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="anishmd",
+    version="0.0.1",
+    description="anishmd",
+    author="Anish Agrawal",
+    author_email="your.email@example.com",
+    packages=["anishmd"],
+)
