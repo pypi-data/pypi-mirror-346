c7ae7c212348a411bf9ec81713d43267958c46ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-09 03:22:18 +0000

    Add package pumaz/1.7.7/pumaz-1.7.7.tar.gz

---
+++ packages/pumaz/pumaz-1.7.7.tar.gz/pumaz-1.7.7/README.md
@@ -0,0 +1,211 @@
+![Puma-logo](Images/Puma-logo.png)
+
+## PUMA 1.0 🐾 -  One Image multiple perspectives 🎭
+[![PyPI version](https://img.shields.io/pypi/v/pumaz?color=FF1493&style=flat-square&logo=pypi)](https://pypi.org/project/pumaz/) [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-red.svg?style=flat-square&logo=gnu&color=FF0000)](https://www.gnu.org/licenses/gpl-3.0) [![Monthly Downloads](https://img.shields.io/pypi/dm/pumaz?label=Downloads%20(Monthly)&color=9400D3&style=flat-square&logo=python)](https://pypi.org/project/pumaz/) [![Daily Downloads](https://img.shields.io/pypi/dd/pumaz?label=Downloads%20(Daily)&color=9400D3&style=flat-square&logo=python)](https://pypi.org/project/pumaz/) <!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+[![All Contributors](https://img.shields.io/badge/all_contributors-3-orange.svg?style=flat-square)](#contributors-)
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
+
+**🌈 PET Reimagined: From Monochrome to a Palette of Possibilities with PUMA 1.0**
+
+Step into the vibrant new world of PET imaging with PUMA 1.0 🌟, where traditional monochrome scans are transformed into a dynamic spectrum of diagnostic data. 🎨
+
+With PUMA 1.0, experience the power of multiplexing: a process that fuses multiple tracer images into a single, multicolored composite that reveals not just the presence of disease but its multifaceted physiological context. 🔄 The multiplexed approach provides a more comprehensive view, helping clinicians uncover nuanced insights about tumors and their microenvironments. 🔍
+
+Whether you're in a high-tech lab or a remote clinic, PUMA 1.0 delivers high-quality, multiplexed PET scans that are as rich in detail as they are in color. 🏥 It’s not just an upgrade—it’s a whole new way of seeing PET data, turning every image into a detailed map of insight and opportunity. 🗺️
+
+## 🎉 Key Features
+
+### 🚀 Versatile and Powerful
+Run PUMA 1.0 on any device, any OS, from x86 to ARM64 (hello, Apple Silicon fans!). Whether you’re on a high-end GPU or a humble CPU, PUMA adapts to your needs without breaking a sweat.
+
+### 🔍 Precision Meets Speed
+Harness the power of AI with MOOSE-driven segmentations and ‘greedy’ library’s wizardry for diffeomorphic image registration. PUMA 1.0 nails the perfect balance of sharp accuracy and zippy performance.
+
+### 🎨 Art of Imaging
+Why settle for ordinary when you can visualize in vibrant RGB? Each color shines a spotlight on a different tracer, turning complex data into a vivid, easy-to-interpret display. With processing times ranging from just 5 to 12 minutes, you're all set for a speedy yet thorough diagnostic journey.
+
+## 🚀 Why PUMA?
+
+
+https://github.com/ENHANCE-PET/PUMA/assets/48599863/03368642-a288-44cb-8eaf-4833380a26c8
+
+
+## Requirements ✅
+
+Before stepping into the future with PUMA 1.0, here's what you need for an optimal experience:
+
+- **Operating System**: Windows, Mac, or Linux - PUMA 1.0 is versatile and works across all these platforms.
+
+- **Memory**: Make sure your system has enough memory (8-16 GB) to run the tasks smoothly.
+
+- **GPU**: You would benefit from a cuda-enabled GPU (NVIDIA), 8 GB or more, or MPS (Apple silicon) - if you like quick results ⚡️. But if you don't fret not, PUMA runs a cpu-tuned version if there is no GPU.
+
+- **Python**: PUMA 1.0 operates the best with Python 3.10.
+
+Once these specifications are met, you're all set to experience PUMA 1.0's capabilities.
+
+## Installation Guide 🛠️
+
+Installation is a breeze on Windows, Linux, and MacOS. Follow the steps below to start your journey with PUMA 1.0.
+
+### For Linux and MacOS 🐧🍏
+
+1. Create a Python environment named 'puma-env' or as per your preference.
+   ```bash
+   python3.10 -m venv puma-env
+   ```
+
+
+2. Activate the environment.
+   ```bash
+   source puma-env/bin/activate  # for Linux
+   source puma-env/bin/activate  # for MacOS
+   ```
+
+3. Install PUMA 1.0.
+   ```bash
+   pip install pumaz # for Linux and MacOS
+   ```
+
+4. Apple M1 Specific Installation: If you are installing PUMA on an Apple Silicon device (e.g., Apple M1), follow this step. Do not do this if you are installing PUMA on Linux!
+   ```bash
+   pip install git+https://github.com/LalithShiyam/pytorch-mps.git
+   ```
+
+
+Congratulations! You're all set to start using PUMA 1.0.
+
+### For Windows 🪟
+
+1. Create a Python environment, e.g., 'puma-env'.
+   ```bash
+   python3.10 -m venv puma-env
+   ```
+
+2. Activate the environment.
+   ```bash
+   .\puma-env\Scripts\activate
+   ```
+
+3. Install PUMA 1.0.
+   ```bash
+   pip install pumaz
+   ```
+
+You're now ready to experience the precision and speed of PUMA 1.0.
+
+## Usage Guide 📚
+
+Start your journey with PUMA 1.0 by using our straightforward command-line tool. It requires the directory path containing different tracer images, and each image should be stored in separate folders. Here's how you can get started:
+
+```bash
+
+   pumaz \
+       -d   <path_to_image_dir>              # Directory path containing the images to be analyzed
+       -ir  <regions_to_ignore>              # Regions to ignore: arms, legs, head, none
+       -m                                    # Optional: Enable multiplexed RGB image output
+       -cs  <color_selection>                # Optional: Custom color selection for RGB output (requires -m)
+       -c2d <convert_back_to_dicom>          # Optional: Once set, the generated nifti images will be converted back to DICOM
+```
+
+- `<path_to_image_dir>` refers to the parent directory containing different tracer images in their respective sub-directories.
+
+- `-ir` specifies the regions to be ignored during registration. If you don't want to ignore any regions, use `none`. If you want to ignore the arms, legs, or head during registration, pass the corresponding regions delimited by a `,`. For example: `-ir head,arms` to ignore the head and arms.
+
+- `-m` will activate the output of a multiplexed RGB image of the combined tracer images.
+
+- `-cs`, when passed along with `-m`, PUMA will ask you to provide a custom order of color channels for the corresponding tracer images. That way, you can freely decide which tracer image is associated with which channel.
+- `-c2d`, when set the generated aligned nifti images will be converted back to DICOM.
+  
+For assistance or additional information, you can always type:
+
+```bash
+pumaz -h
+```
+
+
+### Example usage:
+Apply PUMA to images in a directory, ignoring arms and legs, with multiplexed RGB output and custom colors:
+
+```bash
+pumaz -d /path/to/images -ir arms,legs -m -cs -c2d 
+```
+
+
+## Directory Structure and Naming Conventions for PUMA 📂🏷️
+
+PUMA 1.0 requires your data to be structured in a certain way. It supports DICOM directories and NIFTI files. For NIFTI files, users need to ensure that the files are named with the correct modality tag at the start.
+
+### Required Directory Structure 🌳
+
+Here is the directory structure that PUMA 1.0 expects:
+
+```
+📁 Parent_Directory
+│
+└───📂 Tracer1 # can be named anything
+│   │
+│   └───📁 PET_DICOM_Directory or 🗃️ PT_xxxx.nii.gz # If it's DICOM, the folder name can be anything, but if nifti use a prefix 'PT' for PET
+│   │
+│   └───📁 CT_DICOM_Directory or 🗃️ CT_xxxx.nii.gz # If it's DICOM, the folder name can be anything, but if nifti use a prefix 'CT' for CT
+│
+└───📂 Tracer2
+│   │
+│   └───📁 PET_DICOM_Directory or 🗃️ PT_xxxx.nii.gz
+│   │
+│   └───📁 CT_DICOM_Directory or 🗃️ CT_xxxx.nii.gz
+...
+
+└───📂 Tracer3
+    │
+    └───📁 PET_DICOM_Directory or 🗃️ PT_xxxx.nii.gz
+    │
+    └───📁 CT_DICOM_Directory or 🗃️ CT_xxxx.nii.gz
+
+```
+
+### Naming Conventions 🏷️
+
+- For DICOM directories, no specific naming is required.
+- For NIFTI files, the file should start with the DICOM modality tag (e.g., 'PT_' or 'CT_') followed by the desired name. For example, 'PT_MySample.nii.gz'.
+
+Note: All the PET and CT images related to a tracer should be placed in the same directory named after the tracer.
+
+## 🚀 Benchmarks
+
+To be updated.
+
+## A Note on QIMP Python Packages: The 'Z' Factor 📚🚀
+
+All of our Python packages here at QIMP carry a special signature – a distinctive 'Z' at the end of their names. The 'Z' is more than just a letter to us; it's a symbol of our forward-thinking approach and commitment to continuous innovation.
+
+Our PUMA package, for example, is named as 'pumaz', pronounced "puma-see". So, why 'Z'?
+
+Well, in the world of mathematics and science, 'Z' often represents the unknown, the variable that's yet to be discovered, or the final destination in a series. We at QIMP believe in always pushing boundaries, venturing into uncharted territories, and staying on the cutting edge of technology. The 'Z' embodies this philosophy. It represents our constant quest to uncover what lies beyond the known, to explore the undiscovered, and to bring you the future of medical imaging.
+
+Each time you see a 'Z' in one of our package names, be reminded of the spirit of exploration and discovery that drives our work. With QIMP, you're not just installing a package; you're joining us on a journey to the frontiers of medical image processing. Here's to exploring the 'Z' dimension together! 🚀
+ 
+
+## Contributors ✨
+
+Thanks goes to these wonderful people  ✨:
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tbody>
+    <tr>
+      <td align="center" valign="top" width="14.28%"><a href="https://github.com/W7ebere"><img src="https://avatars.githubusercontent.com/u/166598214?v=4?s=100" width="100px;" alt="W7ebere"/><br /><sub><b>W7ebere</b></sub></a><br /><a href="https://github.com/LalithShiyam/PUMA/commits?author=W7ebere" title="Documentation">📖</a></td>
+      <td align="center" valign="top" width="14.28%"><a href="https://github.com/mprires"><img src="https://avatars.githubusercontent.com/u/48754309?v=4?s=100" width="100px;" alt="Manuel Pires"/><br /><sub><b>Manuel Pires</b></sub></a><br /><a href="https://github.com/LalithShiyam/PUMA/commits?author=mprires" title="Code">💻</a> <a href="https://github.com/LalithShiyam/PUMA/commits?author=mprires" title="Documentation">📖</a></td>
+      <td align="center" valign="top" width="14.28%"><a href="https://github.com/Keyn34"><img src="https://avatars.githubusercontent.com/u/87951050?v=4?s=100" width="100px;" alt="Sebastian Gutschmayer"/><br /><sub><b>Sebastian Gutschmayer</b></sub></a><br /><a href="https://github.com/LalithShiyam/PUMA/commits?author=Keyn34" title="Code">💻</a> <a href="https://github.com/LalithShiyam/PUMA/commits?author=Keyn34" title="Documentation">📖</a></td>
+    </tr>
+  </tbody>
+</table>
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!
---
+++ packages/pumaz/pumaz-1.7.7.tar.gz/pumaz-1.7.7/pumaz/pumaz.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Author: Lalith Kumar Shiyam Sundar | Sebastian Gutschmayer
+# Institution: Medical University of Vienna
+# Research Group: Quantitative Imaging and Medical Physics (QIMP) Team
+# Date: 04.07.2023
+# Version: 0.1.0
+#
+# Description:
+# The main module of the pumaz. It contains the main function that is executed when the pumaz is run.
+#
+# Usage:
+# The variables in this module can be imported and used in other modules within the pumaz.
+#
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+import argparse
+import logging
+import os
+import time
+from datetime import datetime
+
+import colorama
+import emoji
+from pumaz import constants
+from pumaz import display
+from pumaz import download
+from pumaz import file_utilities
+from pumaz import image_conversion
+from pumaz import image_processing
+from pumaz import input_validation
+from pumaz import resources
+from rich.console import Console
+
+console = Console()
+
+
+def main():
+    """
+    Run the PUMA-Z preprocessing and registration pipeline.
+
+    This function standardizes input data to NIFTI format, checks for PUMA-compliant subjects, and runs the preprocessing
+    and registration pipeline. It also downloads the necessary binaries and sets the appropriate permissions.
+
+    :return: None
+    :rtype: None
+    :Example:
+        >>> main()
+    """
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', level=logging.INFO,
+                        filename=datetime.now().strftime('pumaz-v.1.0.0.%H-%M-%d-%m-%Y.log'), filemode='w')
+    colorama.init()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--subject_directory", type=str,
+                        help="Subject directory containing the different PET/CT images of the same subject",
+                        required=True)
+
+    def str2list(value):
+        choices_list = list(constants.MOOSE_LABEL_INDEX.values()) + ['none']
+        value_list = value.lower().split(',')
+        if all(item in choices_list for item in value_list):
+            return value_list
+        else:
+            raise argparse.ArgumentTypeError(f"invalid choice: {value_list} (choose from {choices_list})")
+
+    parser.add_argument("-ir", "--ignore_regions", type=str2list,
+                        help="Comma-separated list of regions to ignore during registration e.g. arms,legs,"
+                             "none. 'none' indicates no regions to ignore.", required=True)
+
+    parser.add_argument("-m", "--multiplex", action='store_true', default=False,
+                        help="Multiplex the aligned PT images.", required=False)
+
+    parser.add_argument("-st", "--segment_tumors", action='store_true', default=False,
+                        help="Segment tumors in the multiplexed images (not implemented: postponed for puma v.2.0)", required=False)
+
+    parser.add_argument("-cs", "--custom_colors", action='store_true', default=False,
+                        help="Manually assign colors to tracer images.", required=False)
+
+    parser.add_argument("-c2d", "--convert_to_dicom", action='store_true', default=False,
+                        help="Convert DICOM images to NIFTI format. Set this to true only if your input is DICOM",
+                        required=False)
+    parser.add_argument("-ra", "--risk_analysis", action='store_true', default=False,
+                        help="Highlight areas of misalignment in the images.",
+                        required=False)
+
+    args = parser.parse_args()
+
+    subject_folder = os.path.abspath(args.subject_directory)
+    regions_to_ignore = args.ignore_regions
+    multiplex = args.multiplex
+    custom_colors = args.custom_colors
+    segment_tumors = args.segment_tumors
+    convert_to_dicom = args.convert_to_dicom
+    perform_risk_analysis = args.risk_analysis
+
+    display.logo()
+    display.citation()
+
+    logging.info('----------------------------------------------------------------------------------------------------')
+    logging.info('                                     STARTING PUMA-Z V.1.0.0                                       ')
+    logging.info('----------------------------------------------------------------------------------------------------')
+
+    # ----------------------------------
+    # INPUT VALIDATION AND PREPARATION
+    # ----------------------------------
+
+    logging.info(' ')
+    logging.info('- Subject directory: ' + subject_folder)
+    logging.info(' ')
+    print(' ')
+    print(f'{constants.ANSI_VIOLET} {emoji.emojize(":memo:")} NOTE:{constants.ANSI_RESET}')
+    print(' ')
+    display.expectations()
+
+    # ----------------------------------
+    # DISPLAYING INPUT CHOICES
+    # ----------------------------------
+    console.print(f' Multiplexing: {multiplex} | '
+                  f'Custom Colors: {custom_colors} | '
+                  f'Segment Tumors: {segment_tumors} | ',
+                  f'Convert to DICOM: {convert_to_dicom} | ',
+                  f'Regions to Ignore: {regions_to_ignore} | ',
+                  f'Risk Analysis: {perform_risk_analysis}',
+                  style='bold yellow')
+
+    # ----------------------------------
+    # DOWNLOADING THE BINARIES
+    # ----------------------------------
+
+    print('')
+    print(f'{constants.ANSI_VIOLET} {emoji.emojize(":globe_with_meridians:")} BINARIES DOWNLOAD:{constants.ANSI_RESET}')
+
+    print('')
+    binary_path = constants.BINARY_PATH
+    file_utilities.create_directory(binary_path)
+    system_os, system_arch = file_utilities.get_system()
+    console.print(f' Detected system: {system_os} | Detected architecture: {system_arch}', style='bold yellow')
+    download.download(item_name=f'puma-{system_os}-{system_arch}', item_path=binary_path,
+                      item_dict=resources.PUMA_BINARIES)
+    file_utilities.set_permissions(constants.GREEDY_PATH, system_os)
+    file_utilities.set_permissions(constants.C3D_PATH, system_os)
+
+    # ----------------------------------
+    # INPUT STANDARDIZATION
+    # ----------------------------------
+
+    print('')
+    print(f'{constants.ANSI_VIOLET} {emoji.emojize(":magnifying_glass_tilted_left:")} STANDARDIZING INPUT DATA TO '
+          f'NIFTI:{constants.ANSI_RESET}')
+    print('')
+    logging.info(' ')
+    logging.info(' STANDARDIZING INPUT DATA TO NIFTI:')
+    logging.info(' ')
+    image_conversion.standardize_to_nifti(subject_folder)
+    logging.info(" Standardization complete.")
+
+    # --------------------------------------
+    # CHECKING FOR PUMA COMPLIANT SUBJECTS
+    # --------------------------------------
+
+    tracer_dirs = [os.path.join(subject_folder, d) for d in os.listdir(subject_folder)
+                   if os.path.isdir(os.path.join(subject_folder, d)) and not d.startswith('PUMAZ-v1')]
+    puma_compliant_subject_folders = input_validation.select_puma_compliant_subject_folders(tracer_dirs)
+
+    num_subject_folders = len(puma_compliant_subject_folders)
+    if num_subject_folders < 1:
+        print(
+            f'{constants.ANSI_RED} {emoji.emojize(":cross_mark:")} No puma compliant tracer directories found to continue!{constants.ANSI_RESET} {emoji.emojize(":light_bulb:")} See: https://github.com/Keyn34/PUMA#directory-structure-and-naming-conventions-for-puma-%EF%B8%8F')
+        return
+
+    # -------------------------------------------------
+    # RUNNING PREPROCESSING AND REGISTRATION PIPELINE
+    # -------------------------------------------------
+    # calculate elapsed time for the entire procedure below
+    start_time = time.time()
+    print('')
+    print(
+        f'{constants.ANSI_VIOLET} {emoji.emojize(":rocket:")} RUNNING PREPROCESSING AND REGISTRATION PIPELINE:{constants.ANSI_RESET}')
+    print('')
+    logging.info(' ')
+    logging.info(' RUNNING PREPROCESSING AND REGISTRATION PIPELINE:')
+    logging.info(' ')
+    puma_dir, ct_dir, pt_dir, mask_dir = image_processing.preprocess(
+        puma_compliant_subjects=puma_compliant_subject_folders,
+        regions_to_ignore=regions_to_ignore)
+    reference_mask, reference_ct, reference_pt = image_processing.align(puma_dir, ct_dir, pt_dir, mask_dir)
+
+    # store the reference images as a dictionary
+    reference_dict = {
+        'reference_mask': reference_mask,
+        'reference_ct': reference_ct,
+        'reference_pt': reference_pt
+    }
+
+    if perform_risk_analysis:
+        # ---------------------------------------
+        # DISPLAY POSSIBLE AREAS OF MISALIGNMENT
+        # ---------------------------------------
+        print('')
+        print(f'{constants.ANSI_VIOLET} {emoji.emojize(":face_screaming_in_fear:")} POSSIBLE AREAS OF MISALIGNMENT:{constants.ANSI_RESET}')
+        print('')
+        logging.info(' ')
+        logging.info(' DISPLAYING POSSIBLE AREAS OF MISALIGNMENT:')
+        logging.info(' ')
+        misaligned_regions = image_processing.display_misalignment(puma_dir, reference_dict)
+        reference_filename = os.path.basename(reference_dict["reference_mask"])
+        image_processing.display_misalignment_table(misaligned_regions, reference_filename)
+
+
+    # ----------------------------------
+    # MULTIPLEXING
+    # ----------------------------------
+
+    if multiplex:
+        print('')
+        print(f'{constants.ANSI_VIOLET} {emoji.emojize(":artist_palette:")} MULTIPLEXING:{constants.ANSI_RESET}')
+        print('')
+        logging.info(' ')
+        logging.info(' MULTIPLEXING:')
+        logging.info(' ')
+        aligned_pt_dir = os.path.join(puma_dir, constants.ALIGNED_PET_FOLDER)
+        rgb_image = os.path.join(aligned_pt_dir, constants.MULTIPLEXED_COMPOSITE_IMAGE)
+        image_processing.multiplex(aligned_pt_dir, '*nii*', 'PET',
+                                   rgb_image, custom_colors)
+        grayscale_image = os.path.join(aligned_pt_dir, constants.GRAYSCALE_COMPOSITE_IMAGE)
+        image_processing.rgb2gray(rgb_image, grayscale_image)
+        if segment_tumors:
+            print('')
+            print(
+                f'{constants.ANSI_VIOLET} {emoji.emojize(":face_with_medical_mask:")} SEGMENTING TUMORS:{constants.ANSI_RESET}')
+            print('')
+            print(f' {constants.ANSI_ORANGE}Segmentation may take a few minutes...{constants.ANSI_RESET}')
+            seg_dir = os.path.join(puma_dir, constants.SEGMENTATION_FOLDER)
+            file_utilities.create_directory(seg_dir)
+            file_utilities.copy_reference_image(grayscale_image, seg_dir, constants.LIONZ_PREFIX)
+            output_dir = os.path.join(seg_dir, constants.LIONZ_OUTPUT_DIR)
+            file_utilities.create_directory(output_dir)
+            image_processing.segment_tumors(seg_dir, output_dir)
+            console.print(f' Segmentation complete.', style='bold green')
+
+    if convert_to_dicom:
+        print('')
+        print(f'{constants.ANSI_VIOLET} {emoji.emojize(":file_folder:")} CONVERTING TO DICOM:{constants.ANSI_RESET}')
+        print('')
+        logging.info(' ')
+        logging.info(' CONVERTING TO DICOM:')
+        logging.info(' ')
+        n2dConverter = image_conversion.NiftiToDicomConverter(
+            subject_folder=subject_folder,
+            puma_dir=puma_dir,
+        )
+        n2dConverter.set_reference_image(reference_mask)
+        n2dConverter.convert_to_dicom(puma_compliant_subject_folders=puma_compliant_subject_folders)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    # show elapsed time in minutes and round it to 2 decimal places
+    elapsed_time = round(elapsed_time / 60, 2)
+    console.print(f" 🐾 PUMA has successfully completed the hunt in {elapsed_time} minutes."
+                  f" Track down your results in the directory: {puma_dir} 🐾", style='white')
