00566751661d0abd96052db1d4af659490740d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 16:06:53 +0000

    Add package FahrenCels/0.1.1/fahrencels-0.1.1.tar.gz

---
+++ packages/FahrenCels/fahrencels-0.1.1.tar.gz/fahrencels-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FahrenCels",
+    version="0.1.1",
+    author="annzelly",
+    description="A simple Python module for converting temperatures between Celsius and Fahrenheit.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'requests',
+        'numpy'
+    ],
+)
