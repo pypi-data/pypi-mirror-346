0ff0348745dfc7dcb841c8f555004ac8dd5e62e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 04:06:27 +0000

    Add package llmwhisperer-client/2.3.1/llmwhisperer_client-2.3.1-py3-none-any.whl

---
+++ packages/llmwhisperer-client/llmwhisperer_client-2.3.1-py3-none-any.whl/unstract/llmwhisperer/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "2.3.1"
+
+from .client import LLMWhispererClient  # noqa: F401
+from .client_v2 import LLMWhispererClientV2  # noqa: F401
+
+
+def get_llmw_py_client_version():
+    """Returns the SDK version."""
+    return __version__
