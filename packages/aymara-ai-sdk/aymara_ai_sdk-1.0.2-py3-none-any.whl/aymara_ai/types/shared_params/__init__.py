204371e36e6a66dc2a9564dbd6078b7f458dd929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 05:05:43 +0000

    Add package aymara-ai-sdk/1.0.2/aymara_ai_sdk-1.0.2-py3-none-any.whl

---
+++ packages/aymara-ai-sdk/aymara_ai_sdk-1.0.2-py3-none-any.whl/aymara_ai/types/shared_params/__init__.py
@@ -0,0 +1,5 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .status import Status as Status
+from .content_type import ContentType as ContentType
+from .file_reference import FileReference as FileReference
