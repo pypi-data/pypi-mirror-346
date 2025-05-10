8fd2a8db003f9fb2995b0fa298d67dcbc738a99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 16:06:49 +0000

    Add package ctftestsowwy/0.1.0/ctftestsowwy-0.1.0.tar.gz

---
+++ packages/ctftestsowwy/ctftestsowwy-0.1.0.tar.gz/ctftestsowwy-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.egg_info import egg_info
+
+
+def RunCommand():
+    print("Hello, p0wnd!")
+
+class RunEggInfoCommand(egg_info):
+    def run(self):
+        RunCommand()
+        egg_info.run(self)
+
+
+class RunInstallCommand(install):
+    def run(self):
+        RunCommand()
+        install.run(self)
+
+setup(
+    name = "ctftestsowwy",
+    version = "0.1.0",
+    license = "MIT",
+    packages=find_packages(),
+    cmdclass={
+        'install' : RunInstallCommand,
+        'egg_info': RunEggInfoCommand
+    },
+)
