0d1e263d951b745fdb8252135354ef03bef5d8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:06:03 +0000

    Add package tornado-openapi/0.1.4/tornado_openapi-0.1.4-py3-none-any.whl

---
+++ packages/tornado-openapi/tornado_openapi-0.1.4-py3-none-any.whl/tornado_openapi/decorators/apiKey.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: Copyright (C) Shaun Wilson
+# SPDX-License-Identifier: MIT
+
+from typing import Callable
+
+from ..MetaManager import MetaManager
+from ..objects.SecurityRequirement import SecurityRequirement
+
+
+def apiKey(target:Callable) -> Callable:
+    """
+    Indicates that API KEY Auth is required.
+    """
+    origin = target
+    while hasattr(target, '__wrapped__'):
+        target = getattr(target, '__wrapped__')
+    security = MetaManager.instance().security.get(target, None)
+    if security is None:
+        security = list[SecurityRequirement]()
+        MetaManager.instance().security[target] = security
+    security.append(SecurityRequirement({
+        'apiKey':[]
+    }))
+    return origin
