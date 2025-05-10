7b7a95949a83005a46c0c97158a08ea97469e5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 16:06:51 +0000

    Add package uznltk/0.0.6/uznltk-0.0.6-py3-none-any.whl

---
+++ packages/uznltk/uznltk-0.0.6-py3-none-any.whl/uznltk/__init__.py
@@ -0,0 +1,8 @@
+#from .tokenizer import split_sentences, split_words
+from .stop_words import stop_word, clean_stop_words
+from .download import Book, News
+from .lemmatizer import lemmatize
+from .stem import stem_word
+from .clean import clean_text
+from .ssign import solid_sign
+from .syllable import syllables
