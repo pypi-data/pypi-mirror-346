a388695c3a5c6f212cd82c4324f875175eb8f525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:52 +0000

    Add package Bomba-nikita-yuminov/0.1/bomba_nikita_yuminov-0.1.tar.gz

---
+++ packages/Bomba-nikita-yuminov/bomba_nikita_yuminov-0.1.tar.gz/bomba_nikita_yuminov-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Bomba_nikita_yuminov',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Юминов Никита',
+    author_email='nikitayuminov0852@gmail.com',
+    description='Описание пакета',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nij777/Bomba.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
