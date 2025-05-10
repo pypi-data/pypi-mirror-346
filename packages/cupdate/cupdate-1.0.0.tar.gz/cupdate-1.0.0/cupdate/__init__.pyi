80caaf52149685802503f34448e558581289a3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 05:05:32 +0000

    Add package cupdate/1.0.0/cupdate-1.0.0.tar.gz

---
+++ packages/cupdate/cupdate-1.0.0.tar.gz/cupdate-1.0.0/cupdate/__init__.pyi
@@ -0,0 +1,21 @@
+def read_requirements(file_path: str) -> dict[str, str]:
+    """
+    Reads the requirements.txt and returns a dictionary with package and version.
+    """
+def read_excluded_packages(config_path: str) -> set[str]:
+    """
+    Reads the updates.config.txt and returns a set of packages that should not be updated.
+    """
+def get_latest_versions(packages: list[str]) -> dict[str, dict[str, str]]:
+    """
+    Determines the latest versions for a list of packages with additional information.
+    """
+def update_requirements(requirements_path: str, config_path: str | None = None) -> None:
+    """
+    Updates the requirements.txt with the latest versions, except for excluded packages.
+    """
+def main() -> None:
+    """
+    Main function that is executed when the command is called.
+    """
+def cli_main() -> None: ...
