edc648f686f8bdffac4ce070b7dff45da9ef6388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 14:06:17 +0000

    Add package millennium-core-utils/5.2.6/millennium-core-utils-5.2.6.tar.gz

---
+++ packages/millennium-core-utils/millennium-core-utils-5.2.6.tar.gz/millennium-core-utils-5.2.6/setup.py
@@ -0,0 +1,22 @@
+import json
+from setuptools import setup, find_packages
+
+with open('../../package.json', 'r') as f:
+    package_info = json.load(f)
+
+setup(
+    name='millennium-core-utils',
+    version=package_info.get('version'),
+    author='Steam Client Homebrew',
+    description='A support library for creating plugins with Millennium.',
+    long_description=open('../../README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/SteamClientHomebrew/PluginComponents',
+    packages=find_packages(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.11.8'
+)
