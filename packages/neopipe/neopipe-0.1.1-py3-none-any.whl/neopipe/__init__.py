ad5a5a320f0626e3dc52b527465397e568f21587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 14:06:19 +0000

    Add package neopipe/0.1.1/neopipe-0.1.1-py3-none-any.whl

---
+++ packages/neopipe/neopipe-0.1.1-py3-none-any.whl/neopipe/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+# Initialize logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+# # Import necessary modules
+from .result import Err, Ok, Result
+
+# from .task import AsyncTask, SyncTask, AbstractAsyncTask, AbstractSyncTask
+# from .pipeline import Pipeline
+
+
+# Specify what is available for import from this package
+__all__ = ["Result", "Ok", "Err"]
+
+from .__about__ import __version__
