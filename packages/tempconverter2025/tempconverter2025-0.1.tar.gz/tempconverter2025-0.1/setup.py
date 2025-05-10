667d69cb2159be733b4fef918aa33de13b0848ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 16:06:49 +0000

    Add package tempconverter2025/0.1/tempconverter2025-0.1.tar.gz

---
+++ packages/tempconverter2025/tempconverter2025-0.1.tar.gz/tempconverter2025-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tempconverter2025',
+    version='0.1',
+    py_modules=['temp_converter'],
+    author='Boss',
+    author_email='Vladimirtlv@gmail.com',
+    description='Simple temperature conversion library',
+    url='https://github.com/Cryptonations/temp_converter',
+)
