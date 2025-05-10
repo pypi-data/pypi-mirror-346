6b3ab282f0a367e3771afe92239a347a2b458512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 16:06:47 +0000

    Add package telegram-bale-bot/4.27.0/telegram_bale_bot-4.27.0-py3-none-any.whl

---
+++ packages/telegram-bale-bot/telegram_bale_bot-4.27.0-py3-none-any.whl/telebot/ext/aio/__init__.py
@@ -0,0 +1,7 @@
+"""
+A folder with all the async extensions.
+"""
+
+from .webhooks import AsyncWebhookListener
+
+__all__ = ["AsyncWebhookListener"]
---
+++ packages/telegram-bale-bot/telegram_bale_bot-4.27.0-py3-none-any.whl/telebot/ext/sync/__init__.py
@@ -0,0 +1,7 @@
+"""
+A folder with all the sync extensions.
+"""
+
+from .webhooks import SyncWebhookListener
+
+__all__ = ["SyncWebhookListener"]
