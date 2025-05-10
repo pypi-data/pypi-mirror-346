59613620d142d681760c773c37e91c2954f10498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 04:06:14 +0000

    Add package agentize/0.0.15/agentize-0.0.15-py3-none-any.whl

---
+++ packages/agentize/agentize-0.0.15-py3-none-any.whl/agentize/agents/summary.py
@@ -0,0 +1,29 @@
+from agents import Agent
+from agents import Model
+from agents import ModelSettings
+
+from ..model import get_openai_model
+from ..model import get_openai_model_settings
+from ..prompts.summary import INSTRUCTIONS
+from ..prompts.summary import Summary
+
+
+def get_summary_agent(
+    lang: str,
+    length: int = 200,
+    model: Model | None = None,
+    model_settings: ModelSettings | None = None,
+) -> Agent:
+    """Get the summary agent."""
+    if model is None:
+        model = get_openai_model()
+
+    if model_settings is None:
+        model_settings = get_openai_model_settings()
+    return Agent(
+        name="summary_agent",
+        instructions=INSTRUCTIONS.format(lang=lang, length=length),
+        model=model,
+        model_settings=model_settings,
+        output_type=Summary,
+    )
