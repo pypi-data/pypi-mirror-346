5eea364481810a19e8fb9713bbf398e65f48a629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 16:06:51 +0000

    Add package notionary/0.2.1/notionary-0.2.1.tar.gz

---
+++ packages/notionary/notionary-0.2.1.tar.gz/notionary-0.2.1/notionary/models/notion_database_response.py
@@ -0,0 +1,63 @@
+from pydantic import BaseModel
+from dataclasses import dataclass
+from typing import Optional, List, Dict, Any, Literal
+
+from notionary.models.notion_page_response import Icon
+
+
+@dataclass
+class TextContent:
+    content: str
+    link: Optional[str] = None
+
+
+@dataclass
+class TextContent:
+    content: str
+    link: Optional[str] = None
+
+
+@dataclass
+class RichText:
+    type: str
+    text: TextContent
+    plain_text: str
+    href: Optional[str]
+
+
+@dataclass
+class User:
+    object: str
+    id: str
+
+
+@dataclass
+class Parent:
+    type: Literal["page_id", "workspace"]
+    page_id: Optional[str] = None
+
+
+@dataclass
+class NotionDatabaserResponse(BaseModel):
+    """
+    Represents the response from the Notion API when retrieving a database.
+    """
+
+    object: Literal["database"]
+    id: str
+    cover: Optional[Any]
+    icon: Optional[Icon]
+    created_time: str
+    last_edited_time: str
+    created_by: User
+    last_edited_by: User
+    title: List[RichText]
+    description: List[Any]
+    is_inline: bool
+    properties: Dict[str, Any]
+    parent: Parent
+    url: str
+    public_url: Optional[str]
+    archived: bool
+    in_trash: bool
+    request_id: Optional[str] = None
