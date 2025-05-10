ca68cfa3e90077b9767c10c2fe2d7b953224a868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:20:11 +0000

    Add package fmu-dataio/2.11.2/fmu_dataio-2.11.2-py3-none-any.whl

---
+++ packages/fmu-dataio/fmu_dataio-2.11.2-py3-none-any.whl/fmu/dataio/_models/standard_results/__init__.py
@@ -0,0 +1,15 @@
+from .field_outline import FieldOutlineResult, FieldOutlineSchema
+from .inplace_volumes import InplaceVolumesResult, InplaceVolumesSchema
+from .structure_depth_fault_lines import (
+    StructureDepthFaultLinesResult,
+    StructureDepthFaultLinesSchema,
+)
+
+__all__ = [
+    "FieldOutlineResult",
+    "FieldOutlineSchema",
+    "InplaceVolumesResult",
+    "InplaceVolumesSchema",
+    "StructureDepthFaultLinesSchema",
+    "StructureDepthFaultLinesResult",
+]
