92b3e8036bade6cd400e26324e1609383544dab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:19:53 +0000

    Add package koshelevev1911/0.1.3/koshelevev1911-0.1.3.tar.gz

---
+++ packages/koshelevev1911/koshelevev1911-0.1.3.tar.gz/koshelevev1911-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='koshelevev1911',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[],
+    author='Кошелев Е. В.',
+    author_email='raygasada@gmail.com',
+    description='Пакет с функцией hello world',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ваш-профиль/hello_world_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
