1a2fc63057d53926952b6ea4d9471dd6d49a0e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 04:06:27 +0000

    Add package cpol-pyscf/0.1.10/cpol_pyscf-0.1.10.tar.gz

---
+++ packages/cpol-pyscf/cpol_pyscf-0.1.10.tar.gz/cpol_pyscf-0.1.10/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cpol_pyscf",
+    version="0.1.10",
+    author="",
+    author_email="mailanoopanair@gmail.com",
+    description="A package for calculating molecular moments and polarizabilities using PySCF",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pyscf",
+    ],
+)
+
