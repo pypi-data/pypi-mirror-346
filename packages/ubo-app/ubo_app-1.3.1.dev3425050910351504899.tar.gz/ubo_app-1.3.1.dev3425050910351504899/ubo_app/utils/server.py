0c89d126e844cbcf30d68eaef7c1f20c9c19ad62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:06:05 +0000

    Add package ubo-app/1.3.1.dev3425050910351504899/ubo_app-1.3.1.dev3425050910351504899.tar.gz

---
+++ packages/ubo-app/ubo_app-1.3.1.dev3425050910351504899.tar.gz/ubo_app-1.3.1.dev3425050910351504899/ubo_app/utils/server.py
@@ -0,0 +1,45 @@
+"""Module for sending commands to the system manager socket."""
+
+from __future__ import annotations
+
+import asyncio
+from typing import Literal, overload
+
+from ubo_app.constants import SERVER_SOCKET_PATH
+from ubo_app.logger import logger
+from ubo_app.utils import IS_RPI
+
+
+@overload
+async def send_command(*command: str) -> None: ...
+@overload
+async def send_command(*command: str, has_output: Literal[True]) -> str | None: ...
+async def send_command(*command_: str, has_output: bool = False) -> str | None:
+    """Send a command to the system manager socket."""
+    if not IS_RPI:
+        return None
+
+    command = ' '.join(command_)
+
+    try:
+        reader, writer = await asyncio.open_unix_connection(SERVER_SOCKET_PATH)
+
+        logger.debug('Sending command:', extra={'command': command})
+
+        response = None
+        writer.write(command.encode() + b'\0')
+        if has_output:
+            datagram = (await reader.readuntil(b'\0'))[:-1]
+            if datagram:
+                response = datagram.decode('utf-8')
+                logger.debug('Server response:', extra={'response': response})
+        writer.close()
+
+    except Exception:
+        logger.exception(
+            'Failed to send command to the server',
+            extra={'command': command},
+        )
+        raise
+    else:
+        return response
