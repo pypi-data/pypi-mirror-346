b38ac4f3484765a9a39b95df2e08fb4bf85e500c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:20:07 +0000

    Add package prism-logger/0.1.0/prism_logger-0.1.0.tar.gz

---
+++ packages/prism-logger/prism_logger-0.1.0.tar.gz/prism_logger-0.1.0/prism_logger/__init__.py
@@ -0,0 +1,9 @@
+from prism import logger
+from prism_logger.logger import (
+    BLANK,
+    DEFAULT_STYLE,
+    Logger,
+    MessageStyle,
+)
+
+__all__ = ["BLANK", "DEFAULT_STYLE", "Logger", "MessageStyle", "logger"]
