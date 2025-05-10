db2d3a35c37abadeb14da547f7bf361b8331346d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 16:06:56 +0000

    Add package freegpts/1.1.0/freegpts-1.1.0-py3-none-any.whl

---
+++ packages/freegpts/freegpts-1.1.0-py3-none-any.whl/freegpts/gpt4.py
@@ -0,0 +1,7 @@
+import requests
+
+def answer(question, role="user"):
+	dictToSend = {"model": "gpt-4", "request": {"messages": [{"role": role, "content": question}]}}
+	res = requests.post('https://api.onlysq.ru/ai/v2', json=dictToSend)
+	response = res.json()
+	return response["choices"][0]["message"]["content"]
