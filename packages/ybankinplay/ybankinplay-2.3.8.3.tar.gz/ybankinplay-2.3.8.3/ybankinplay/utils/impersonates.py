8eac3cb490fcd899574765c5d742a736593e8404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:20:22 +0000

    Add package ybankinplay/2.3.8.3/ybankinplay-2.3.8.3.tar.gz

---
+++ packages/ybankinplay/ybankinplay-2.3.8.3.tar.gz/ybankinplay-2.3.8.3/ybankinplay/utils/impersonates.py
@@ -0,0 +1,9 @@
+IMPERSONATE= [
+    "chrome99", "chrome100", "chrome101", "chrome104", "chrome107", "chrome110",
+    "chrome116", "chrome119", "chrome120", "chrome123", "chrome124", "chrome131",
+    "chrome133a", "chrome132", "chrome134", "chrome135", "chrome136"
+    "chrome99_android", "chrome131_android", "chrome132_android", "chrome133_android",
+    "chrome134_android", "chrome135_android",
+    "safari15_3", "safari15_5", "safari17_0", "safari18_0", "safari17_2_ios", "safari18_0_ios",
+     "edge99", "edge101", "edge133", "edge135"
+]
