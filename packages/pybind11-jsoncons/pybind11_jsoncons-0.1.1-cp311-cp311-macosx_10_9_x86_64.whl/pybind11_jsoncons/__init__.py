18a64bec253069be94bfea9da1a062f33ce57cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 05:05:34 +0000

    Add package pybind11-jsoncons/0.1.1/pybind11_jsoncons-0.1.1-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/pybind11-jsoncons/pybind11_jsoncons-0.1.1-cp311-cp311-macosx_10_9_x86_64.whl/pybind11_jsoncons/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from ._core import (
+    Json,
+    JsonQuery,
+    JsonQueryRepl,
+    __doc__,
+    __version__,
+    msgpack_decode,
+    msgpack_encode,
+)
+
+__all__ = [
+    "__doc__",
+    "__version__",
+    "JsonQuery",
+    "JsonQueryRepl",
+    "Json",
+    "msgpack_decode",
+    "msgpack_encode",
+]
