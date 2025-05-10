e9bd47df007d1c460cf7b8c6b41a25b2f2c78e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:08 +0000

    Add package SLBPPG/0.1/slbppg-0.1.tar.gz

---
+++ packages/SLBPPG/slbppg-0.1.tar.gz/slbppg-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='SLBPPG',
+      version='0.1',
+      author='Ilxss',
+      description='Predict stress index by ppg',
+      packages=find_packages(),
+      author_email='i.grishanow@gmail.com',
+      install_requires=['neurokit2==0.2.10', 'catboost', 'numpy', 'pandas', 'scipy', 'opencv-python'],
+      python_requires='>=3.7'
+      )
