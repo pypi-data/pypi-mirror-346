5c6a5c36a4fae5088001c3686ed94a25baacbeb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:20:03 +0000

    Add package augmentedsocialscientist/2.2.2/augmentedsocialscientist-2.2.2.tar.gz

---
+++ packages/augmentedsocialscientist/augmentedsocialscientist-2.2.2.tar.gz/augmentedsocialscientist-2.2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+environment = [
+    'scikit-learn',
+    'tqdm',
+    'numpy',
+    'scipy',
+    'torch',
+    'transformers',
+    'sentencepiece',
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(name='AugmentedSocialScientist',
+      author='Rubing Shen',
+      license='MIT',
+      author_email='shenrubing1996@gmail.com',
+      url='https://github.com/rubingshen/AugmentedSocialScientist',
+      download_url='https://github.com/rubingshen/AugmentedSocialScientist/archive/refs/tags/v2.2.2.tar.gz',
+      version='2.2.2',
+      description='A Package to Easily Train Bert-Like Models for Text Classification',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=['AugmentedSocialScientist'],
+      zip_safe=False,
+      install_requires=environment)
