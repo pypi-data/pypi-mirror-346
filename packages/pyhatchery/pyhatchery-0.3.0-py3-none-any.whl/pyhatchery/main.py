b650586f3b0dd6141f135d9bd44baa249a1bef7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:47 +0000

    Add package pyhatchery/0.3.0/pyhatchery-0.3.0-py3-none-any.whl

---
+++ packages/pyhatchery/pyhatchery-0.3.0-py3-none-any.whl/pyhatchery/main.py
@@ -0,0 +1,12 @@
+"""Main module for the pyhatchery package."""
+
+from pyhatchery import __version__
+
+
+def main():
+    """Main function to execute the script."""
+    print(f"Hello from pyhatchery! Version {__version__}.")
+
+
+if __name__ == "__main__":
+    main()
