a7453bcf92e415c7d4c9b16275d699282da3fdf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:19:46 +0000

    Add package wurzel/0.9.0/wurzel-0.9.0-py3-none-any.whl

---
+++ packages/wurzel/wurzel-0.9.0-py3-none-any.whl/wurzel/cli/cmd_run.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2025 Deutsche Telekom AG (opensource@telekom.de)
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+from typing import Type
+
+from wurzel.step import TypedStep
+
+
+def main(
+    step: Type[TypedStep],
+    output_path: Path,
+    input_folders: set[Path],
+    executor_str_value: str,
+    encapsulate_env=True,
+):
+    """Execute"""
+    executor = executor_str_value
+    with executor(dont_encapsulate=not encapsulate_env) as ex:
+        ex(step, input_folders, output_path)
