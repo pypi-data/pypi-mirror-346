cf4ff2eb9b1fa5d5e39074bf13221b9b367a4fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:19:56 +0000

    Add package solas-ai/0.5.1/solas_ai-0.5.1-cp38-cp38-win_amd64.whl

---
+++ packages/solas-ai/solas_ai-0.5.1-cp38-cp38-win_amd64.whl/solas_shared/exceptions/__init__.pyi
@@ -0,0 +1,4 @@
+from ._license_invalid_error import LicenseInvalidError as LicenseInvalidError
+from ._license_missing_feature_error import LicenseMissingFeatureError as LicenseMissingFeatureError
+
+__all__ = ['LicenseInvalidError', 'LicenseMissingFeatureError']
