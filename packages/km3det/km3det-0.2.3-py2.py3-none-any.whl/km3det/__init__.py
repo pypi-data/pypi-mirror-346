1763b6512e3228a08c8b74d98414f835f69cb3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 04:05:43 +0000

    Add package km3det/0.2.3/km3det-0.2.3-py2.py3-none-any.whl

---
+++ packages/km3det/km3det-0.2.3-py2.py3-none-any.whl/km3det/__init__.py
@@ -0,0 +1,11 @@
+try:
+    from importlib.metadata import version as get_version
+
+    version = get_version(__name__)
+except ImportError:
+    from pkg_resources import get_distribution
+
+    version = get_distribution(__name__).version
+
+from .detector import Detector
+from .pmt_parameters import PMT_parameters
