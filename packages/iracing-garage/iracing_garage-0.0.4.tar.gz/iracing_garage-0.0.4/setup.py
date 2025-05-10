d22d2a7e593cf95e2a30e36c26d53625d3e39a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:22 +0000

    Add package iracing-garage/0.0.4/iracing_garage-0.0.4.tar.gz

---
+++ packages/iracing-garage/iracing_garage-0.0.4.tar.gz/iracing_garage-0.0.4/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+VERSION = "0.0.4"
+DESCRIPTION = "Simple iRacing API Wrapper"
+LONG_DESCRIPTION = "A Python app to get data from the iRacing API endpoints. Helps you get stats, race results, and more! 🏎️ 🏁"
+
+# Setting up
+setup(
+    name="iracing_garage",
+    version=VERSION,
+    author="Dang Khoi Vo",
+    author_email="<vo.dangkh@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=["requests", "requests_toolbelt"],
+    keywords=[
+        "python",
+        "api",
+        "racing",
+        "iracing",
+        "api-wrappers",
+        "iracing-api",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
