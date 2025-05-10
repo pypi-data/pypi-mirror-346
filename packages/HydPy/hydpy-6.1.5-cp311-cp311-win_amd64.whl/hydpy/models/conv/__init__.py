db7a400db543bc554d7c7f8c380fcda12e4786f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 05:06:18 +0000

    Add package HydPy/6.1.5/hydpy-6.1.5-cp311-cp311-win_amd64.whl

---
+++ packages/HydPy/hydpy-6.1.5-cp311-cp311-win_amd64.whl/hydpy/models/conv/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""The |conv.DOCNAME.complete| model family allows connecting different kinds of models
+providing output and requiring input that does not fit immediately.
+"""
+# import...
+# ...from HydPy
+from hydpy.exe.modelimports import *
+from hydpy.models.conv.conv_model import Model
+
+tester = Tester()
+cythonizer = Cythonizer()
