5fe74bc3f210a3a00812c3c3bda079a2756e5850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 14:06:11 +0000

    Add package gazooresearchutils/1.8.1/GazooResearchUtils-1.8.1.tar.gz

---
+++ packages/gazooresearchutils/GazooResearchUtils-1.8.1.tar.gz/GazooResearchUtils-1.8.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GazooResearchUtils",
+    version="1.8.1",
+    description="Analysis of GazooResearch Data",
+    package_dir={"":"app"},
+    packages=find_packages(where="app"),
+    url="https://gazooresearch.com/",
+    author='Andrew Lim MD, Megan Lim MD, Christopher Lim MD, Robert Lim MD',
+    install_requires=["lifelines >= 0.28.0", "pandas>=2.1.0"],
+    python_requires=">=3.10"
+)
