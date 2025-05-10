1335f6ee1f806764da519594bc11426ef8dbd82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:20:25 +0000

    Add package airouter/0.1.36/airouter-0.1.36-py3-none-any.whl

---
+++ packages/airouter/airouter-0.1.36-py3-none-any.whl/airouter/providers/azure_openai_provider.py
@@ -0,0 +1,25 @@
+import os
+import decouple
+from airouter.providers.openai_provider import OpenAIProvider
+
+PROVIDER_CONFIGURED = False
+
+try:
+  import openai
+  os.environ['AZURE_OPENAI_ENDPOINT'] = decouple.config("AZURE_OPENAI_ENDPOINT")
+  os.environ['AZURE_OPENAI_API_KEY'] = decouple.config("AZURE_OPENAI_API_KEY")
+  os.environ['OPENAI_API_VERSION'] = decouple.config("OPENAI_API_VERSION", default="2023-09-01-preview") or \
+                                     decouple.config("AZURE_OPENAI_API_VERSION", default="2023-09-01-preview")
+  PROVIDER_CONFIGURED = True
+except:
+  pass
+
+
+class AzureOpenAIProvider(OpenAIProvider):
+  def __init__(self, **kwargs):
+    super(OpenAIProvider, self).__init__(**kwargs)
+    return
+
+  @property
+  def openai_client(self):
+    return openai.AzureOpenAI(azure_deployment=self.model.replace(".", ""))
