63ee4cf069f8b3801b0f4aa8961b28da244ef2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:06:21 +0000

    Add package ckanext-selfinfo/1.1.14/ckanext_selfinfo-1.1.14-py3-none-any.whl

---
+++ packages/ckanext-selfinfo/ckanext_selfinfo-1.1.14-py3-none-any.whl/ckanext/selfinfo/handlers.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import logging
+import json
+
+from ckan.lib.redis import connect_to_redis, Redis
+import ckan.plugins.toolkit as tk
+
+from .config import selfinfo_get_errors_limit
+from .utils import get_redis_key
+
+
+class SelfinfoErrorHandler(logging.Handler):
+    """Custom handler to store exceptions."""
+
+    def emit(self, record: logging.LogRecord):
+        if record.levelno >= logging.ERROR:
+            redis: Redis = connect_to_redis()
+            log_message = self.format(record)
+            redis_key = get_redis_key("errors")
+
+            if not redis.exists(redis_key):
+                redis.set(redis_key, json.dumps([]))
+            raw = redis.get(redis_key)
+            if not raw:
+                errors = []
+            else:
+                errors = raw.decode("utf-8")  # type: ignore
+                if not isinstance(errors, list):  # Ensure errors is a list
+                    errors = []
+
+            errors_limit = selfinfo_get_errors_limit()
+            if len(errors) >= errors_limit:
+                start_key = len(errors) - errors_limit + 1
+                errors = errors[start_key:]
+            current_url = None
+            try:
+                current_url = tk.h.full_current_url()
+            except (AttributeError, RuntimeError):
+                pass
+
+            errors.append(
+                {
+                    "error": log_message,
+                    "error_url": current_url if current_url else "Missing URL",
+                }
+            )
+            redis.set(redis_key, json.dumps(errors))
