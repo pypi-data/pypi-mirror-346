5aa1f4fa440c524f870eee04d1f54663fe0ed06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:26 +0000

    Add package cube-solver/1.0.1/cube_solver-1.0.1-py3-none-any.whl

---
+++ packages/cube-solver/cube_solver-1.0.1-py3-none-any.whl/cube_solver/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Cube Solver."""
+
+__author__ = """Dave Barragan"""
+__email__ = 'itsdaveba@gmail.com'
+__version__ = '1.0.1'
+
+from cube_solver.cube import Cube
+from cube_solver.solver import Solver
+
+__all__ = ["Cube", "Solver"]
