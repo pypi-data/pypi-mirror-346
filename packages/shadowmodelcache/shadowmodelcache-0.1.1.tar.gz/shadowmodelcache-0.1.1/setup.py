03bdba8d9cfd0a57fa827f33f0c4a9ba24353082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 16:06:54 +0000

    Add package shadowmodelcache/0.1.1/shadowmodelcache-0.1.1.tar.gz

---
+++ packages/shadowmodelcache/shadowmodelcache-0.1.1.tar.gz/shadowmodelcache-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shadowmodelcache',
+    version='0.1.1',
+    description='High-performance django model cache library.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/saveychauhan/ShadowModelCache',
+    author='Sawan Chauhan',
+    author_email='sav.ey@live.co.uk',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'django>=3.2',
+        'django-redis'
+    ],
+    python_requires='>=3.6',
+)
