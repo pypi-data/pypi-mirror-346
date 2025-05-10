63ce697fe5839247a24f8c225297a31fafdc2e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 14:06:02 +0000

    Add package pyTSPA-toolbox/0.1.1/pytspa_toolbox-0.1.1-py3-none-any.whl

---
+++ packages/pyTSPA-toolbox/pytspa_toolbox-0.1.1-py3-none-any.whl/pyTSPA/__init__.py
@@ -0,0 +1,34 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pyTSPA_toolbox")
+except PackageNotFoundError:
+    __version__ = "0.1.1"
+
+from .data import load_match_data, clean_data, data_profiling
+from .metrics import result_stats, team_performance, get_all_teams, each_team_performance, win_percentage, each_win_percentage, pythagorean_expectation, each_pythagorean_expectation, logistic_regression_prediction, predict_match_outcome, season_half_prediction
+from .visualization import plot_result_distribution, plot_team_results, plot_league_points_table, plot_goal_difference_distribution, plot_win_percentage_comparison, plot_pythagorean_expectation
+
+__all__ = [
+    "load_match_data",
+    "clean_data",
+    "data_profiling",
+    "result_stats",
+    "team_performance",
+    "get_all_teams",
+    "each_team_performance",
+    "win_percentage",
+    "each_win_percentage",
+    "pythagorean_expectation",
+    "each_pythagorean_expectation",
+    "logistic_regression_prediction",
+    "predict_match_outcome",
+    "season_half_prediction",
+    "plot_result_distribution",
+    "plot_team_results",
+    "plot_league_points_table",
+    "plot_goal_difference_distribution",
+    "plot_win_percentage_comparison",
+    "plot_pythagorean_expectation",
+    "__version__",
+]
