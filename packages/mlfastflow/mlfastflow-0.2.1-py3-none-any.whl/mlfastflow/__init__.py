b43faeb92067389193fa392a865fe8201595acbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:05:26 +0000

    Add package mlfastflow/0.2.1/mlfastflow-0.2.1-py3-none-any.whl

---
+++ packages/mlfastflow/mlfastflow-0.2.1-py3-none-any.whl/mlfastflow/__init__.py
@@ -0,0 +1,32 @@
+"""MLFastFlow - packages for fast dataflow and workflow processing."""
+
+__version__ = "0.2.1"
+
+# Import core components
+from mlfastflow.core import Flow
+
+# Import sourcing functionality
+import mlfastflow.sourcing as sourcing
+from mlfastflow.sourcing import Sourcing
+ 
+# Import BigQueryClient
+import mlfastflow.bigqueryclient as bigqueryclient
+from mlfastflow.bigqueryclient import BigQueryClient
+
+# Import utils module (functions accessible via utils.function_name)
+import mlfastflow.utils as utils
+
+# Import utility functions directly
+from mlfastflow.utils import concat_files, profile
+
+# Make these classes and modules available at the package level
+__all__ = [
+    'Flow',
+    'bigqueryclient',  # module
+    'BigQueryClient',  # class
+    'sourcing',        # module
+    'Sourcing',        # class
+    'utils',           # module
+    'concat_files',    # function
+    'profile',         # function
+]
