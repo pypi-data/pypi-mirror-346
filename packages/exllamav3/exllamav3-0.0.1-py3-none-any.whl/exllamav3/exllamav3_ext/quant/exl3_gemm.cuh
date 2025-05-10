922c227fc02e518429886e53008fc1e2f312c9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:06:38 +0000

    Add package exllamav3/0.0.1/exllamav3-0.0.1-py3-none-any.whl

---
+++ packages/exllamav3/exllamav3-0.0.1-py3-none-any.whl/exllamav3/exllamav3_ext/quant/exl3_gemm.cuh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <ATen/Tensor.h>
+
+int exl3_gemm
+(
+    const at::Tensor& A,
+    const at::Tensor& B,
+    at::Tensor& C,
+    const c10::optional<at::Tensor>& suh,
+    const c10::optional<at::Tensor>& A_had,
+    const c10::optional<at::Tensor>& svh,
+    int force_shape_idx
+);
