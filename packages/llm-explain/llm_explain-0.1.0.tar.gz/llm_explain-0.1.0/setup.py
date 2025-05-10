600f40fda38f5fae57370bc66807ade08ca1f56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 14:06:31 +0000

    Add package llm-explain/0.1.0/llm_explain-0.1.0.tar.gz

---
+++ packages/llm-explain/llm_explain-0.1.0.tar.gz/llm_explain-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llm_explain",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "pydantic",
+        "requests",
+        "numpy",
+        "scikit-learn",
+        "tqdm"
+    ],
+    author="Ruiqi Zhong",
+    author_email="ruiqizhong1997@gmail.com",
+    description="A package for using LLMs to explain datasets",
+    license="MIT",
+    keywords="llm, explain, dataset, explanation",
+    url="https://github.com/ruiqi-zhong/llm_explain",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.9",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
