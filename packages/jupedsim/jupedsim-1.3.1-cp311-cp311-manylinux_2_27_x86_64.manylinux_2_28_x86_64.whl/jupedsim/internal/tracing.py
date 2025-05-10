0550f40aa06fc9cf5aa70d7409a2310ad13a5ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:05:52 +0000

    Add package jupedsim/1.3.1/jupedsim-1.3.1-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/jupedsim/jupedsim-1.3.1-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/jupedsim/internal/tracing.py
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+import jupedsim.native as py_jps
+
+
+class Trace:
+    """
+    .. important::
+
+        This is indented for internal usage. We will not guarantee that this API will
+        stable and available in any release. It might be changed on any update, regardless of
+        a major/minor/patch update.
+    """
+
+    def __init__(self) -> None:
+        self._obj = py_jps.Trace
+
+    @property
+    def iteration_duration(self) -> float:
+        """Time for one simulation iteration in us.
+
+        Returns:
+             Time for one simulation iteration in us
+        """
+        return self._obj.iteration_duration
+
+    @property
+    def operational_level_duration(self) -> float:
+        """Time for one simulation iteration in the operational level in us.
+
+        Returns:
+             Time for one simulation iteration in the operational level in us
+        """
+
+        return self._obj.operational_level_duration
+
+    def __str__(self) -> str:
+        return self._obj.__repr__()
