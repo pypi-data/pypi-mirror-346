00dad57949364849fc00257f70b16df91ea9c5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 04:06:21 +0000

    Add package terra-classic-data-analysis-sdk/0.1.4/terra_classic_data_analysis_sdk-0.1.4.tar.gz

---
+++ packages/terra-classic-data-analysis-sdk/terra_classic_data_analysis_sdk-0.1.4.tar.gz/terra_classic_data_analysis_sdk-0.1.4/terra_classic_sdk/core/wasm/__init__.py
@@ -0,0 +1,19 @@
+from .msgs import (
+    MsgClearAdmin,
+    MsgExecuteContract,
+    MsgInstantiateContract,
+    MsgMigrateContract,
+    MsgStoreCode,
+    MsgUpdateAdmin,
+    MsgInstantiateContract2,
+)
+
+__all__ = [
+    "MsgStoreCode",
+    "MsgInstantiateContract",
+    "MsgExecuteContract",
+    "MsgMigrateContract",
+    "MsgUpdateAdmin",
+    "MsgClearAdmin",
+    "MsgInstantiateContract2",
+]
