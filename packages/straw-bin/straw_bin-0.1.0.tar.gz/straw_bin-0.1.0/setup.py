88b9fd1eeea3ffc9cf4206f7082e1fd70afd68c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:20:33 +0000

    Add package straw-bin/0.1.0/straw_bin-0.1.0.tar.gz

---
+++ packages/straw-bin/straw_bin-0.1.0.tar.gz/straw_bin-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='straw-bin',  
+    version='0.1.0',
+    author='Straw Hat',
+    description='A simple package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
