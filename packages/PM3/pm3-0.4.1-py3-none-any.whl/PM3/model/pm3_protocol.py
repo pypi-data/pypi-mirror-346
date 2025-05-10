edf7bc9044c606cd2b5ae5dc50fce17537fe59e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:19:54 +0000

    Add package PM3/0.4.1/pm3-0.4.1-py3-none-any.whl

---
+++ packages/PM3/pm3-0.4.1-py3-none-any.whl/PM3/model/pm3_protocol.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Union, Optional
+from collections import namedtuple
+
+# id or Name schema
+ION = namedtuple('Id_or_Name', 'type, data, proc')
+
+# Messaggio di ritorno simulato di psutil.wait
+alive_gone = namedtuple('AliveGone', 'pid')
+
+class RetMsg(BaseModel):
+    msg: str
+    err: bool = False
+    warn: bool = False
+    payload: Optional[Union[list, dict]] = None
+
+class KillMsg(BaseModel):
+    msg: str = ''
+    err: bool = False
+    warn: bool = False
+    gone: list = []
+    alive: list = []
