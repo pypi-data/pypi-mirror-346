db34800046235513846a4f74e2a6b6e6f3be1877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:19:55 +0000

    Add package inspire-dictdiffer/0.0.0/inspire-dictdiffer-0.0.0.tar.gz

---
+++ packages/inspire-dictdiffer/inspire-dictdiffer-0.0.0.tar.gz/inspire-dictdiffer-0.0.0/setup.py
@@ -0,0 +1,98 @@
+# This file is part of Dictdiffer.
+#
+# Copyright (C) 2013 Fatih Erikli.
+# Copyright (C) 2014, 2015, 2016 CERN.
+# Copyright (C) 2017, 2019 ETH Zurich, Swiss Data Science Center, Jiri Kuncar.
+#
+# Dictdiffer is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Dictdiffer is a library that helps you to diff and patch dictionaries."""
+
+from __future__ import absolute_import, print_function
+
+import os
+import re
+
+from setuptools import find_packages, setup
+
+readme = "Dictdiffer is a helper module that helps you to diff and patch dictionaries."
+history = open('CHANGES').read()
+
+tests_require = [
+    'coverage>=4.0',
+    'mock>=1.3.0',
+    'pydocstyle>=1.0.0',
+    'pytest-cache>=1.0',
+    'pytest-cov==2.6.1',
+    'pytest-pep8>=1.0.6',
+    'pytest~=4.6; python_version == "2.7"',
+    'pytest~=6.0,>=6.2.5; python_version >= "3"'
+]
+
+extras_require = {
+    'numpy': [
+        'numpy>=1.13.0;python_version<"3.7"',
+        'numpy>=1.15.0;python_version<"3.8"',
+        'numpy>=1.18.0;python_version<"3.9"',
+        'numpy>=1.20.0;python_version>="3.9"',
+    ],
+    'tests': tests_require,
+}
+
+extras_require['all'] = []
+for key, reqs in extras_require.items():
+    if ':' == key[0]:
+        continue
+    extras_require['all'].extend(reqs)
+
+packages = find_packages()
+
+version_template = """\
+# -*- coding: utf-8 -*-
+# Do not change the format of this next line. Doing so risks breaking
+# setup.py and docs/conf.py
+\"\"\"Version information for dictdiffer package.\"\"\"
+
+__version__ = {version!r}
+"""
+
+setup(
+    name='inspire-dictdiffer',
+    use_scm_version={
+        'local_scheme': 'dirty-tag',
+        'write_to': os.path.join('dictdiffer', 'version.py'),
+        'write_to_template': version_template,
+    },
+    description=__doc__,
+    long_description=readme,
+    author='Invenio Collaboration',
+    author_email='info@inveniosoftware.org',
+    url='https://github.com/inveniosoftware/dictdiffer',
+    project_urls={
+        'Changelog': (
+            'https://github.com/inveniosoftware/dictdiffer'
+            '/blob/master/CHANGES'
+        ),
+        'Docs': 'https://dictdiffer.rtfd.io/',
+    },
+    packages=['dictdiffer'],
+    zip_safe=False,
+    extras_require=extras_require,
+    tests_require=tests_require,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+    ],
+)
