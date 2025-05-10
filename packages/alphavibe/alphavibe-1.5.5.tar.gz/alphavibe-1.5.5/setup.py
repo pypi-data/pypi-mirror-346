ee78bcf0130b120edc65d304c9de2a425e1f59ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 14:06:22 +0000

    Add package alphavibe/1.5.5/alphavibe-1.5.5.tar.gz

---
+++ packages/alphavibe/alphavibe-1.5.5.tar.gz/alphavibe-1.5.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alphavibe',
+    version='1.5.5',
+    description='Gen Alpha programming language: Vibe-based Python for Zoomers',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Louis the Sigma',
+    author_email='developerlouis7923@gmail.com',
+    url='https://github.com/PotatoInfinity/genalpha-lang',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Education',
+        'Topic :: Software Development :: Interpreters'
+    ],
+    python_requires='>=3.6',
+)
