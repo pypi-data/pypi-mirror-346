506a620f00ef11ebebb993c7e111aa4600143978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 04:06:13 +0000

    Add package pyglossary/5.0.10/pyglossary-5.0.10.tar.gz

---
+++ packages/pyglossary/pyglossary-5.0.10.tar.gz/pyglossary-5.0.10/pyglossary/plugins/cc_kedict/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# mypy: ignore-errors
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+	from pyglossary.option import Option
+
+from .reader import Reader
+
+__all__ = [
+	"Reader",
+	"description",
+	"enable",
+	"extensionCreate",
+	"extensions",
+	"kind",
+	"lname",
+	"name",
+	"optionsProp",
+	"singleFile",
+	"website",
+	"wiki",
+]
+
+enable = True
+lname = "cc_kedict"
+name = "cc-kedict"
+description = "cc-kedict"
+extensions = ()
+extensionCreate = ""
+singleFile = True
+kind = "text"
+wiki = ""
+website = (
+	"https://github.com/mhagiwara/cc-kedict",
+	"@mhagiwara/cc-kedict",
+)
+optionsProp: dict[str, Option] = {}
