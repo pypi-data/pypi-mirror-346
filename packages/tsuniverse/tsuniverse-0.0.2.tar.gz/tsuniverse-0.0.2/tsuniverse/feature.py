f2d4f3cded6576c3ab8252d5fb0399df3399c858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:13 +0000

    Add package tsuniverse/0.0.2/tsuniverse-0.0.2.tar.gz

---
+++ packages/tsuniverse/tsuniverse-0.0.2.tar.gz/tsuniverse-0.0.2/tsuniverse/feature.py
@@ -0,0 +1,14 @@
+"""The base class for a feature."""
+
+from typing import NotRequired, TypedDict
+
+
+class Feature(TypedDict):
+    """A description of a feature to use."""
+
+    predictor: str
+    predictor_transform: str
+    predictand: str
+    predictand_transform: str
+    lag: NotRequired[float]
+    correlation: NotRequired[float]
---
+++ packages/tsuniverse/tsuniverse-0.0.2.tar.gz/tsuniverse-0.0.2/tsuniverse/process.py
@@ -0,0 +1,30 @@
+"""The main process function."""
+
+from multiprocessing import Pool
+from typing import Iterator
+
+import pandas as pd
+
+from .feature import Feature
+from .pearson_process import pearson_process
+from .transforms import TRANSFORMS
+
+
+def process(
+    df: pd.DataFrame,
+    predictands: list[str],
+    max_window: int,
+    max_process_features: int = 10,
+) -> Iterator[list[Feature]]:
+    """Process the dataframe for tsuniverse features."""
+    with Pool() as p:
+        for predictand in predictands:
+            for transform_name in TRANSFORMS:
+                features = sorted(
+                    list(
+                        pearson_process(df, predictand, max_window, p, transform_name)
+                    ),
+                    key=lambda x: abs(x["correlation"] if "correlation" in x else 0.0),
+                    reverse=True,
+                )[:max_process_features]
+                yield features
