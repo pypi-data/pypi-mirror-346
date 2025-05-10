e0df8c14933328fa4ee28cbef923afb56c2825e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:06:30 +0000

    Add package handybox/0.3.0/handybox-0.3.0.tar.gz

---
+++ packages/handybox/handybox-0.3.0.tar.gz/handybox-0.3.0/handybox/misctools.py
@@ -0,0 +1,16 @@
+import os
+import uuid
+
+def uniqid(prefix=''):
+    return f"{f'{prefix}-' if bool(prefix) else ''}{uuid.uuid4().hex[:13]}"
+
+def createDir(path: str) -> None:
+    """
+    Create a directory (and any necessary parent directories).
+    If the directory already exists, does nothing.
+    """
+    try:
+        os.makedirs(path, exist_ok=True)
+        print(f"Directory created (or already existed): {path}")
+    except Exception as e:
+        print(f"Failed to create directory {path}: {e}")
