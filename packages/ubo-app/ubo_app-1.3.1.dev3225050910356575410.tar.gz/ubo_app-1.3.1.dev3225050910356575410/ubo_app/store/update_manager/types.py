e48064f76fb0b3e05422a0763e44c3bde22dc3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:06:23 +0000

    Add package ubo-app/1.3.1.dev3225050910356575410/ubo_app-1.3.1.dev3225050910356575410.tar.gz

---
+++ packages/ubo-app/ubo_app-1.3.1.dev3225050910356575410.tar.gz/ubo_app-1.3.1.dev3225050910356575410/ubo_app/store/update_manager/types.py
@@ -0,0 +1,58 @@
+# ruff: noqa: D100, D101, D102, D103, D104, D107
+from __future__ import annotations
+
+from enum import StrEnum, auto
+
+from immutable import Immutable
+from redux import BaseAction, BaseEvent
+
+UPDATE_MANAGER_NOTIFICATION_ID = 'ubo:update_manager'
+UPDATE_MANAGER_SECOND_PHASE_NOTIFICATION_ID = 'ubo:update_manager:phase-2'
+
+
+class UpdateManagerAction(BaseAction): ...
+
+
+class UpdateManagerSetVersionsAction(UpdateManagerAction):
+    flash_notification: bool
+    current_version: str
+    base_image_variant: str
+    latest_version: str
+    recent_versions: list[str] | None = None
+
+
+class UpdateManagerSetStatusAction(UpdateManagerAction):
+    status: UpdateStatus
+
+
+class UpdateManagerSetUpdateServiceStatusAction(UpdateManagerAction):
+    is_active: bool
+
+
+class UpdateManagerEvent(BaseEvent): ...
+
+
+class UpdateManagerCheckEvent(UpdateManagerEvent): ...
+
+
+class UpdateManagerUpdateEvent(UpdateManagerEvent): ...
+
+
+class UpdateStatus(StrEnum):
+    """Update status enum."""
+
+    CHECKING = auto()
+    FAILED_TO_CHECK = auto()
+    UP_TO_DATE = auto()
+    OUTDATED = auto()
+    UPDATING = auto()
+
+
+class UpdateManagerState(Immutable):
+    """Version store."""
+
+    current_version: str | None = None
+    base_image_variant: str | None = None
+    latest_version: str | None = None
+    update_status: UpdateStatus = UpdateStatus.CHECKING
+    is_update_service_active: bool = False
