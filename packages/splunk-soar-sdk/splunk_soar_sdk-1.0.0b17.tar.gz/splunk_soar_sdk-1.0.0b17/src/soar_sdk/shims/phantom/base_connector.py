3a3eedd1a34985cc92e96937521e31d9af1ee931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 14:06:32 +0000

    Add package splunk-soar-sdk/1.0.0b17/splunk_soar_sdk-1.0.0b17.tar.gz

---
+++ packages/splunk-soar-sdk/splunk_soar_sdk-1.0.0b17.tar.gz/splunk_soar_sdk-1.0.0b17/src/soar_sdk/shims/phantom/base_connector.py
@@ -0,0 +1,114 @@
+try:
+    from phantom.base_connector import BaseConnector
+
+    _soar_is_available = True
+except ImportError:
+    _soar_is_available = False
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING or not _soar_is_available:
+    import json
+    import abc
+
+    from soar_sdk.shims.phantom.action_result import ActionResult
+    from soar_sdk.shims.phantom.connector_result import ConnectorResult
+
+    from typing import Union, Any
+    from contextlib import suppress
+
+    class BaseConnector:  # type: ignore[no-redef]
+        def __init__(self) -> None:
+            self.action_results: list[ActionResult] = []
+            self.__conn_result: ConnectorResult
+            self.__conn_result = ConnectorResult()
+
+        @staticmethod
+        def _get_phantom_base_url() -> str:
+            return "https://localhost:9999/"
+
+        def send_progress(
+            self,
+            progress_str_const: str,
+            *unnamed_format_args: object,
+            **named_format_args: object,
+        ) -> None:
+            with suppress(IndexError, KeyError, ValueError):
+                progress_str_const = progress_str_const.format(
+                    *unnamed_format_args, **named_format_args
+                )
+
+            print(progress_str_const)
+
+        def save_progress(
+            self,
+            progress_str_const: str,
+            *unnamed_format_args: object,
+            **named_format_args: object,
+        ) -> None:
+            with suppress(IndexError, KeyError, ValueError):
+                progress_str_const = progress_str_const.format(
+                    *unnamed_format_args, **named_format_args
+                )
+
+            print(progress_str_const)
+
+        def error_print(
+            self,
+            _tag: str,
+            _dump_object: Union[str, list, dict, ActionResult, Exception] = "",
+        ) -> None:
+            print(_tag, _dump_object)
+
+        def debug_print(
+            self,
+            _tag: str,
+            _dump_object: Union[str, list, dict, ActionResult, Exception] = "",
+        ) -> None:
+            print(_tag, _dump_object)
+
+        def get_action_results(self) -> list[ActionResult]:
+            return self.action_results
+
+        def add_action_result(self, action_result: ActionResult) -> ActionResult:
+            self.action_results.append(action_result)
+            return action_result
+
+        def get_action_identifier(self) -> str:
+            return self.action_identifier
+
+        @abc.abstractmethod
+        def handle_action(self, param: dict[str, Any]) -> None:
+            pass
+
+        def _handle_action(self, in_json: str, handle: int) -> str:
+            input_object = json.loads(in_json)
+
+            self.action_identifier = input_object.get("identifier", "")
+            self.config = input_object.get("config", {})
+            param_array = input_object.get("parameters") or [{}]
+            for param in param_array:
+                self.handle_action(param)
+
+            return in_json
+
+        def get_config(self) -> dict:
+            return self.config
+
+        def save_state(self, state: dict) -> None:
+            self.state = state
+
+        def load_state(self) -> dict:
+            return self.state
+
+        def _set_csrf_info(self, token: str, referer: str) -> None:
+            pass
+
+        def finalize(self) -> bool:
+            return True
+
+        def initialize(self) -> bool:
+            return True
+
+
+__all__ = ["BaseConnector"]
---
+++ packages/splunk-soar-sdk/splunk_soar_sdk-1.0.0b17.tar.gz/splunk_soar_sdk-1.0.0b17/tests/example_app/src/app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from soar_sdk.abstract import SOARClient
+from soar_sdk.app import App
+from soar_sdk.asset import AssetField, BaseAsset
+from soar_sdk.params import Params
+from soar_sdk.action_results import ActionOutput
+from soar_sdk.logging import getLogger
+
+logger = getLogger()
+
+
+class Asset(BaseAsset):
+    base_url: str
+    api_key: str = AssetField(sensitive=True, description="API key for authentication")
+    key_header: str = AssetField(
+        default="Authorization",
+        value_list=["Authorization", "X-API-Key"],
+        description="Header for API key authentication",
+    )
+
+
+app = App(asset_cls=Asset, name="example_app")
+
+
+@app.test_connectivity()
+def test_connectivity(client: SOARClient, asset: Asset) -> None:
+    logger.info(f"testing connectivity against {asset.base_url}")
+
+
+class ReverseStringParams(Params):
+    input_string: str
+
+
+class ReverseStringOutput(ActionOutput):
+    reversed_string: str
+
+
+@app.action(action_type="test", verbose="Reverses a string.")
+def reverse_string(
+    param: ReverseStringParams, client: SOARClient
+) -> ReverseStringOutput:
+    logger.debug("params: %s", param)
+    reversed_string = param.input_string[::-1]
+    logger.debug("reversed_string %s", reversed_string)
+    return ReverseStringOutput(reversed_string=reversed_string)
+
+
+if __name__ == "__main__":
+    app.cli()
