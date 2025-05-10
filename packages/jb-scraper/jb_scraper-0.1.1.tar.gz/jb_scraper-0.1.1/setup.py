0f66524985787c4e2a64517db82522f122a60724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:32 +0000

    Add package jb-scraper/0.1.1/jb_scraper-0.1.1.tar.gz

---
+++ packages/jb-scraper/jb_scraper-0.1.1.tar.gz/jb_scraper-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages  # type: ignore
+
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="jb-scraper",
+    version="0.1.1",
+    author="Lucas Aquino",
+    author_email="lc.aquinodeoliveira@gmail.com",
+    description="JobScraper, um pacote de raspagem de vagas na internet.",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ClausAlaerth/jobscraper-package",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.12",
+)
