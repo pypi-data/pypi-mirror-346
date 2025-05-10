64bb53cfc4824edc8567bd5618011edd7d62a6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 05:05:55 +0000

    Add package notebook-nanoservice/0.1.0/notebook_nanoservice-0.1.0.tar.gz

---
+++ packages/notebook-nanoservice/notebook_nanoservice-0.1.0.tar.gz/notebook_nanoservice-0.1.0/setup.py
@@ -0,0 +1,14 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="notebook_nanoservice",
+    version="0.1.0",
+    packages=find_packages(where="."),
+    author="Your Name",
+    author_email="danmar@microsoft@example.com",
+    description="Nanoservices for notebooks",
+    url="https://github.com/microsoft/notebook-nanoservice"
+)
