23f49c246e7095b2fa6cda9ed7da260fc161961e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:33 +0000

    Add package pmd-django/2.0.8/pmd_django-2.0.8.tar.gz

---
+++ packages/pmd-django/pmd_django-2.0.8.tar.gz/pmd_django-2.0.8/testapp/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class TestModel(models.Model):
+    status = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "test_model"
+        app_label = 'testapp'
+
+class TestRelatedModel(models.Model):
+    name = models.TextField()
+    related = models.ForeignKey(TestModel, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "test_related_model"
+        app_label = 'testapp'
+
+class UniqueModel(models.Model):
+    code = models.TextField(unique=True)
+    related = models.ForeignKey("testapp.TestRelatedModel", on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        db_table = "test_unique_model"
+        app_label = 'testapp'
