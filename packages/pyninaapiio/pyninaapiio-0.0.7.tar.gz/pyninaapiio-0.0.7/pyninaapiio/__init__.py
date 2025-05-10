a75bd12235f1e3132153bb507feaf0c2232f2722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 03:19:51 +0000

    Add package pyninaapiio/0.0.7/pyninaapiio-0.0.7.tar.gz

---
+++ packages/pyninaapiio/pyninaapiio-0.0.7.tar.gz/pyninaapiio-0.0.7/pyninaapiio/__init__.py
@@ -0,0 +1,10 @@
+"""A client library for accessing Advanced API"""
+
+from pyninaapiio.client import AuthenticatedClient, Client
+from pyninaapiio.nina import NinaAPI
+
+__all__ = (
+    "AuthenticatedClient",
+    "Client",
+    "NinaAPI",
+)
