1c6e2cd4c7fbf3bc25ebdc2fa826288eacaa7668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 05:05:56 +0000

    Add package pygame-nevui/0.1.2/pygame_nevui-0.1.2.tar.gz

---
+++ packages/pygame-nevui/pygame_nevui-0.1.2.tar.gz/pygame_nevui-0.1.2/pygame_nevui/__init__.py
@@ -0,0 +1,8 @@
+from .color import *  # Импортируем все из colors.py (Color, Color_Type, Theme, Color.RED и т.д.)
+from .style import *   # Импортируем все из style.py (Style, Align, default_style, Theme.DARK и т.д.)
+from .widgets import *
+from .layouts import *
+from .menu import *
+from .window import *
+from .utils import *
+from .animations import *
