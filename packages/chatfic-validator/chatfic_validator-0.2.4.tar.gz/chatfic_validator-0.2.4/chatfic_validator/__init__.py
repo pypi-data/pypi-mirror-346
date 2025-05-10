7916ebedd49c2684190a4c45f3011bb8e233d398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 14:06:25 +0000

    Add package chatfic-validator/0.2.4/chatfic_validator-0.2.4.tar.gz

---
+++ packages/chatfic-validator/chatfic_validator-0.2.4.tar.gz/chatfic_validator-0.2.4/chatfic_validator/__init__.py
@@ -0,0 +1,8 @@
+from .validator import ChatficValidator, ChatficFormat
+from .result import ChatficValidationResult
+
+__all__ = [
+    'ChatficValidator',
+    'ChatficValidationResult',
+    'ChatficFormat'
+]
