a6ed25e36c454858de9040cb7bf31dbb7d1ac591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 16:06:54 +0000

    Add package pmoschos-art-gen/0.1.0/pmoschos_art_gen-0.1.0-py3-none-any.whl

---
+++ packages/pmoschos-art-gen/pmoschos_art_gen-0.1.0-py3-none-any.whl/pmoschos_art_gen/__init__.py
@@ -0,0 +1,9 @@
+"""
+pmoschos_art_gen - Rainbow ASCII Art Generator
+
+A simple library to generate colorful ASCII art with rainbow colors.
+"""
+
+from .ascii_generator import generate_rainbow_ascii, render_ascii_lines, center_ascii_output
+
+__version__ = "0.1.0"
