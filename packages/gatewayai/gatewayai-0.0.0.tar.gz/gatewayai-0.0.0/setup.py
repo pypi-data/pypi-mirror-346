5961f3c949ec2915e6897153b20a3b64dfb10fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 04:06:16 +0000

    Add package gatewayai/0.0.0/gatewayai-0.0.0.tar.gz

---
+++ packages/gatewayai/gatewayai-0.0.0.tar.gz/gatewayai-0.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='gatewayai',
+      version='0.0.0',
+      description='WIP',
+      cmdclass={'test': PyTest})
