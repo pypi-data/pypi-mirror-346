828b303edaf9b87293a26b21c91ce7004416e129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 04:06:21 +0000

    Add package cpol-pyscf/0.1.8/cpol_pyscf-0.1.8-py3-none-any.whl

---
+++ packages/cpol-pyscf/cpol_pyscf-0.1.8-py3-none-any.whl/cpol_pyscf/__init__.py
@@ -0,0 +1,10 @@
+"""
+cpol_pyscf - A package for calculating molecular moments and polarizabilities using PySCF
+"""
+
+from . import momtensor
+from . import poltensor
+from . import qdist
+from . import PySCFmerger
+
+__version__ = "0.1.7"
