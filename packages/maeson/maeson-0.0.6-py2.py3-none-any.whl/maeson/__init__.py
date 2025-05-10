c1b077b197962ae10ec49af08ad2bbc84b08fdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 14:06:50 +0000

    Add package maeson/0.0.6/maeson-0.0.6-py2.py3-none-any.whl

---
+++ packages/maeson/maeson-0.0.6-py2.py3-none-any.whl/maeson/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for maeson."""
+
+__author__ = """Dryver Finch"""
+__email__ = "dryver2206@gmail.com"
+__version__ = "0.0.6"
+
+# Import the function from its module
+from .maeson import *
