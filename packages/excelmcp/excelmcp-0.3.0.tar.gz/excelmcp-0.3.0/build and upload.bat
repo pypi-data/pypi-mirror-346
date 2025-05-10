a018f72f5147539e9bf7b1e1dd27ad6fa68fa4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:06:18 +0000

    Add package excelmcp/0.3.0/excelmcp-0.3.0.tar.gz

---
+++ packages/excelmcp/excelmcp-0.3.0.tar.gz/excelmcp-0.3.0/build and upload.bat
@@ -0,0 +1,3 @@
+uv build --no-sources
+uv build --no-sources --package ExcelMCP
+uv publish --token pypi-AgEIcHlwaS5vcmcCJGRmOGY1YzFhLTEyMTMtNDg3MS05Yzg0LTNjYjg2NzIyMTlmNAACKlszLCIzMTY3ZDdhYS0xOWQ1LTRlM2UtYTRiZC02OTE0NTMzN2VmYjUiXQAABiB8t90RJlg00nBSUAnu2xkEaArXVruvoOwnjpHvJ44SQw
