f7050325cb142f2942484975249ef8dd518959bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:42 +0000

    Add package ctftestsowwy/0.0.3/ctftestsowwy-0.0.3.tar.gz

---
+++ packages/ctftestsowwy/ctftestsowwy-0.0.3.tar.gz/ctftestsowwy-0.0.3/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.egg_info import egg_info
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        develop.run(self)
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        print('HIIII HELOOO')
+
+def RunCommand():
+    print('HELOOOOO\n'*88   )
+    print("Hello, p0wnd!")
+
+class RunEggInfoCommand(egg_info):
+    def run(self):
+        RunCommand()
+        egg_info.run(self)
+
+
+class RunInstallCommand(install):
+    def run(self):
+        RunCommand()
+        install.run(self)
+
+setup(
+    name = "ctftestsowwy",
+    version = "0.0.3",
+    license = "MIT",
+    packages=find_packages(),
+    cmdclass={
+        'install' : RunInstallCommand,
+        'egg_info': RunEggInfoCommand
+    },
+)   
