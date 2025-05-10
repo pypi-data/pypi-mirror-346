0fc64a6c81126b48b53065943cec5e8ab92d2226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 15:06:23 +0000

    Add package maths-add/0.0.1.post3/maths_add-0.0.1.post3-py3-none-any.whl

---
+++ packages/maths-add/maths_add-0.0.1.post3-py3-none-any.whl/maths_add/special_numbers.py
@@ -0,0 +1,34 @@
+#encoding:utf-8
+import math
+from maths_add.except_error import decorate
+
+class Pi(object):
+	def __init__(self):
+		pass
+	def get_num(self):
+		return math.pi
+
+class E(object):
+	def __init__(self):
+		pass
+	def get_num(self):
+		return math.e
+
+class Tau(object):
+	def __init__(self):
+		pass
+	def get_num(self):
+		return math.tau
+
+class Inf(object):
+	def __init__(self):
+		pass
+	def get_num(self):
+		return math.inf
+
+class Nan(object):
+	def __init__(self):
+		pass
+	def get_num(self):
+		return math.nan
+
