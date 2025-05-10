51aac59cda3fd3bfacca0736d0064eafe1162680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-10 06:06:16 +0000

    Add package bluer-amr/5.4.1/bluer_amr-5.4.1.tar.gz

---
+++ packages/bluer-amr/bluer_amr-5.4.1.tar.gz/bluer_amr-5.4.1/bluer_AMR/__init__.py
@@ -0,0 +1,17 @@
+NAME = "bluer_AMR"
+
+ICON = "🔬"
+
+DESCRIPTION = f"{ICON} meta-analysis of clinical outcomes of antibiotics for cholera treatment and percentage of antibiotic resistance."
+
+VERSION = "5.4.1"
+
+REPO_NAME = "bluer-AMR"
+
+MARQUEE = "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true"
+
+ALIAS = "@AMR"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
