ad02fe13b6691a336dcb477315fdb3729a15fd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 04:08:23 +0000

    Add package petcmd/0.4.4/petcmd-0.4.4-py3-none-any.whl

---
+++ packages/petcmd/petcmd-0.4.4-py3-none-any.whl/petcmd/command.py
@@ -0,0 +1,34 @@
+import re
+from typing import Callable, Iterable
+
+from petcmd.utils import get_signature
+
+class Command:
+
+	def __init__(self, cmds: str | tuple[str, ...], func: Callable, shell_complete: Iterable | Callable | str = None):
+		if isinstance(cmds, str):
+			self.cmds = (cmds,)
+		else:
+			self.cmds = cmds
+		self.func = func
+		self.shell_complete = shell_complete
+		self.aliases = self.__generate_aliases()
+
+	def match(self, cmd: str | tuple[str, ...]) -> bool:
+		if isinstance(cmd, str):
+			return cmd in self.cmds
+		return any(c in cmd for c in self.cmds)
+
+	def __generate_aliases(self) -> dict[str, str]:
+		"""Returns {alias: argument}"""
+		positionals, keyword, *_ = get_signature(self.func)
+		aliases = {}
+		for arg in [*positionals, *keyword]:
+			aliases[arg] = arg
+			aliases[arg.replace('_', '-')] = arg
+			first_letter = re.search(r"[a-zA-Z]", arg).group(0)
+			if first_letter.lower() not in aliases:
+				aliases[first_letter.lower()] = arg
+			elif first_letter.upper() not in aliases:
+				aliases[first_letter.upper()] = arg
+		return aliases
