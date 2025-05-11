808e37066232a9c69e19fe20eab7236d2d9091fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 04:08:12 +0000

    Add package vlc-gesture-control/1.1.1/vlc_gesture_control-1.1.1-py3-none-any.whl

---
+++ packages/vlc-gesture-control/vlc_gesture_control-1.1.1-py3-none-any.whl/vlc_gesture_control/__init__.py
@@ -0,0 +1,12 @@
+"""VLC Gesture Control - Control VLC media player using hand gestures."""
+
+try:
+    from importlib.metadata import version
+
+    __version__ = version("vlc-gesture-control")
+except ImportError:
+    __version__ = "1.1.1"  # Fallback version
+
+
+__author__ = "VLC Gesture Control Contributors"
+__license__ = "MIT"
