d6b0bc8a1370da9b5ffe44a6e072786a3defcaab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 14:05:46 +0000

    Add package withpi/1.43.1/withpi-1.43.1.tar.gz

---
+++ packages/withpi/withpi-1.43.1.tar.gz/withpi-1.43.1/src/withpi/resources/search/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .search import (
+    SearchResource,
+    AsyncSearchResource,
+    SearchResourceWithRawResponse,
+    AsyncSearchResourceWithRawResponse,
+    SearchResourceWithStreamingResponse,
+    AsyncSearchResourceWithStreamingResponse,
+)
+from .query_fanout import (
+    QueryFanoutResource,
+    AsyncQueryFanoutResource,
+    QueryFanoutResourceWithRawResponse,
+    AsyncQueryFanoutResourceWithRawResponse,
+    QueryFanoutResourceWithStreamingResponse,
+    AsyncQueryFanoutResourceWithStreamingResponse,
+)
+from .query_classifier import (
+    QueryClassifierResource,
+    AsyncQueryClassifierResource,
+    QueryClassifierResourceWithRawResponse,
+    AsyncQueryClassifierResourceWithRawResponse,
+    QueryClassifierResourceWithStreamingResponse,
+    AsyncQueryClassifierResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "QueryFanoutResource",
+    "AsyncQueryFanoutResource",
+    "QueryFanoutResourceWithRawResponse",
+    "AsyncQueryFanoutResourceWithRawResponse",
+    "QueryFanoutResourceWithStreamingResponse",
+    "AsyncQueryFanoutResourceWithStreamingResponse",
+    "QueryClassifierResource",
+    "AsyncQueryClassifierResource",
+    "QueryClassifierResourceWithRawResponse",
+    "AsyncQueryClassifierResourceWithRawResponse",
+    "QueryClassifierResourceWithStreamingResponse",
+    "AsyncQueryClassifierResourceWithStreamingResponse",
+    "SearchResource",
+    "AsyncSearchResource",
+    "SearchResourceWithRawResponse",
+    "AsyncSearchResourceWithRawResponse",
+    "SearchResourceWithStreamingResponse",
+    "AsyncSearchResourceWithStreamingResponse",
+]
