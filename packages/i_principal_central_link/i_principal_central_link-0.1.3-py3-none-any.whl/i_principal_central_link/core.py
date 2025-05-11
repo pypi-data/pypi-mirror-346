0aebf0004a941d77c1676039bc947aabe12ae005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 14:05:49 +0000

    Add package i_principal_central_link/0.1.3/i_principal_central_link-0.1.3-py3-none-any.whl

---
+++ packages/i_principal_central_link/i_principal_central_link-0.1.3-py3-none-any.whl/i_principal_central_link/core.py
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def i_principal_central_link():
+
+
+
+    global i
+
+
+    i = {}
+
+
+    i["link"] = r"github.com/IPrincipalCentral/i_principal_central"
+
+
+
+    print(i["link"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
