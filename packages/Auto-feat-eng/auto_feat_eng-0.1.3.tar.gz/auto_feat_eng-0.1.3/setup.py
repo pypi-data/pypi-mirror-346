8c8c703f44fac2d6e62d60370f2fc7c4a8dbf1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 04:08:47 +0000

    Add package Auto-feat-eng/0.1.3/auto_feat_eng-0.1.3.tar.gz

---
+++ packages/Auto-feat-eng/auto_feat_eng-0.1.3.tar.gz/auto_feat_eng-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Auto_feat_eng',
+    packages=find_packages(include=['mypythonlib']),
+    version='0.1.3',
+    description='This library is used to do feature engineering work automatic',
+    author='Khemraj Mangal',
+    install_requires=["pandas", "numpy", "scikit-learn"],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
