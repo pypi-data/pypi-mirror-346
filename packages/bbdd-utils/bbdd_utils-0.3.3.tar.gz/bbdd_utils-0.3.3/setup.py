4ad3b29c65f31e1e6e30abb53ac68ca114eaa7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 03:26:01 +0000

    Add package bbdd-utils/0.3.3/bbdd_utils-0.3.3.tar.gz

---
+++ packages/bbdd-utils/bbdd_utils-0.3.3.tar.gz/bbdd_utils-0.3.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bbdd_utils',
+    version='0.3.3',
+    packages=find_packages(),
+    install_requires=[],
+    author='Luis_J_Domingo',
+    description='actualizacion mysql y sqlite3',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    # url='https://github.com/tuusuario/milibreria',  # si usas GitHub
+)
