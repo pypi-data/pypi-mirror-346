87201546165debd84160ee76839b7afdaabe6b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 03:25:54 +0000

    Add package frankyu/202505010.2/frankyu-202505010.2-py3-none-any.whl

---
+++ packages/frankyu/frankyu-202505010.2-py3-none-any.whl/f_other/b半成品/a飞机导弹列表workbook_20250510145743707399.py
@@ -0,0 +1,9 @@
+aaa = "飞机导弹列表workbook_20250510145743707399"
+
+bbb = r'https://d.docs.live.net/9122e41a29eea899/sb_yufengguang/xls/%E9%A3%9E%E6%9C%BA%E5%AF%BC%E5%BC%B9%E5%88%97%E8%A1%A8workbook_20250510145743707399.xlsx'
+
+import f_excel.over.open_and_process_excel as op
+
+ccc = op.open_and_process_excel(bbb)
+
+input()
