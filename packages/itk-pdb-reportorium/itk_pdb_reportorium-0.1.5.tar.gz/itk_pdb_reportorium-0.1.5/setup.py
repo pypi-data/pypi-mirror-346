47f29a58790f8a48785464c0575629719e9183c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 05:07:01 +0000

    Add package itk-pdb-reportorium/0.1.5/itk_pdb_reportorium-0.1.5.tar.gz

---
+++ packages/itk-pdb-reportorium/itk_pdb_reportorium-0.1.5.tar.gz/itk_pdb_reportorium-0.1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="itk-pdb-reportorium",
+    version="0.1.5",
+    packages=find_packages(where=".") + ["metadata"],
+    include_package_data=True,
+    description="This package is specialized for the module-qc-statistical-tools (MQST) and aims to send the output MQST reports to EOS using the SendFileToEOS.py script.",
+    author="kenneth wraight,Omar Istaitia, Doyeong Kim, Dimitris Varouchas",
+    author_email='kenneth.wraight@glasgow.ac.uk, omar.istaitia@cern.ch, doyeong.kim@cern.ch, dimitris.varouchas@cern.ch',    install_requires=[
+        "randomname",
+        "scp",
+        "paramiko",
+    ],
+)
+
