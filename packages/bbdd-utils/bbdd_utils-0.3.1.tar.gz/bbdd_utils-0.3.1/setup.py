f1d7041af9e4e2ce0df6fce779184752d9eed92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 03:25:35 +0000

    Add package bbdd-utils/0.3.1/bbdd_utils-0.3.1.tar.gz

---
+++ packages/bbdd-utils/bbdd_utils-0.3.1.tar.gz/bbdd_utils-0.3.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bbdd_utils',
+    version='0.3.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Luis_J_Domingo',
+    description='libreria para la gestion de bases de datos',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    # url='https://github.com/tuusuario/milibreria',  # si usas GitHub
+)
