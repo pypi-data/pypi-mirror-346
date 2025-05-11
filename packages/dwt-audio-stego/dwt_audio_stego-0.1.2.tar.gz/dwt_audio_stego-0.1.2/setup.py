302a3b2cdde501e0574b30821b3a5773bf8f569a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 03:25:57 +0000

    Add package dwt-audio-stego/0.1.2/dwt_audio_stego-0.1.2.tar.gz

---
+++ packages/dwt-audio-stego/dwt_audio_stego-0.1.2.tar.gz/dwt_audio_stego-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dwt-audio-stego',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'soundfile',
+        'PyWavelets',
+        'librosa',
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dwt-audio-stego = dwt_audio_stego.main:cli',
+        ],
+    },
+    author='HVT',
+    description='Audio steganography using DWT',
+    license='MIT',
+    python_requires='>=3.6',
+)
