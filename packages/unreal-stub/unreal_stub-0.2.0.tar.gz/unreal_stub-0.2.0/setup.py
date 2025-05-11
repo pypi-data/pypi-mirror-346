decaf775e387c2fee30348b551aea21c4402782f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 03:25:30 +0000

    Add package unreal-stub/0.2.0/unreal_stub-0.2.0.tar.gz

---
+++ packages/unreal-stub/unreal_stub-0.2.0.tar.gz/unreal_stub-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text(encoding="utf-8")
+
+
+setup(
+    name='unreal_stub',
+    version='0.2.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages()
+)
