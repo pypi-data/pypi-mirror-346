464b2d233cc575fbbf809641d726272900139163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 04:08:56 +0000

    Add package fitfile/1.1.10/fitfile-1.1.10.tar.gz

---
+++ packages/fitfile/fitfile-1.1.10.tar.gz/fitfile-1.1.10/fitfile/version_info.py
@@ -0,0 +1,18 @@
+"""Version information for the application."""
+
+__author__ = "Tom Goetz"
+__copyright__ = "Copyright Tom Goetz"
+__license__ = "GPL"
+
+
+python_required = (3, 0, 0)
+python_tested = (3, 11, 4)
+version_info = (1, 1, 10)
+
+
+def version_string():
+    """Return a version string for a version tuple."""
+    return '.'.join(str(digit) for digit in version_info)
+
+
+__version__ = version_string()
