e0819feb33938a1fb10e4ef199b3ad76a990abbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 04:08:32 +0000

    Add package gPhotosAlbum-JPEGcompression/2.2.2/gphotosalbum_jpegcompression-2.2.2.tar.gz

---
+++ packages/gPhotosAlbum-JPEGcompression/gphotosalbum_jpegcompression-2.2.2.tar.gz/gphotosalbum_jpegcompression-2.2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gPhotosAlbum_JPEGcompression',
+    version='2.2.2',
+    author='Abhishek Venkatachalam',
+    author_email='abhishek.venkatachalam06@gmail.com',
+    description='Takes Google Photos Album (visible to anyone with url) and produces a zip file with compressed JPEGS.',
+    long_description=(
+        "For more information about the author, visit [LinkedIn](https://www.linkedin.com/in/abhishek-venkatachalam-62121049/).\n\n"
+        + open('README.md').read()
+    ),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pillow',
+        'tqdm',
+        'requests',
+        'selenium',
+        'psutil'
+    ],
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
