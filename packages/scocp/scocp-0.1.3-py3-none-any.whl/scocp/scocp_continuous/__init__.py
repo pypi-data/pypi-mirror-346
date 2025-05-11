ae4b51741ae4edb694ea11245bcd2d38d916f680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 05:07:08 +0000

    Add package scocp/0.1.3/scocp-0.1.3-py3-none-any.whl

---
+++ packages/scocp/scocp-0.1.3-py3-none-any.whl/scocp/scocp_continuous/__init__.py
@@ -0,0 +1,17 @@
+"""SOCP with continuous control"""
+
+from ._scocp_continuous import (
+    ContinuousControlSCOCP,
+)
+from ._rdv_fixed import (
+    FixedTimeContinuousRdv,
+    FixedTimeContinuousRdvLogMass,
+)
+from ._rdv_free import (
+    FreeTimeContinuousRdv,
+    FreeTimeContinuousRdvLogMass,
+)
+from ._rdv_free_moving import (
+    FreeTimeContinuousMovingTargetRdvLogMass,
+    FreeTimeContinuousMovingTargetRdvMass,
+)
