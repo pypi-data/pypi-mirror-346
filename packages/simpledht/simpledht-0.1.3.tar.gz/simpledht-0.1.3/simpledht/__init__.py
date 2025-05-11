fc1ef65ab850e037cfd85c6ecfe4e4da78082e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 03:25:17 +0000

    Add package simpledht/0.1.3/simpledht-0.1.3.tar.gz

---
+++ packages/simpledht/simpledht-0.1.3.tar.gz/simpledht-0.1.3/simpledht/__init__.py
@@ -0,0 +1,28 @@
+"""
+SimpleDHT - A simple distributed hash table implementation
+
+Example usage:
+    from simpledht import DHTNode
+
+    # Create a new node
+    node = DHTNode(host='0.0.0.0', port=5000)
+    
+    # Start the node
+    node.start()
+    
+    # Store a value
+    node.put('mykey', 'myvalue')
+    
+    # Retrieve a value
+    value = node.get('mykey')
+    
+    # Connect to another node
+    node.bootstrap('other_node_ip:5000')
+"""
+
+__version__ = "0.1.3"
+
+from .dht_node import DHTNode
+from .cli import main as cli_main
+
+__all__ = ["DHTNode", "cli_main"] 
