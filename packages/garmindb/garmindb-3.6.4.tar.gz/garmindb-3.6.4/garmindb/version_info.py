23ffa51aedc86efa006820e91fb33f4b5fd1d772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 04:08:14 +0000

    Add package garmindb/3.6.4/garmindb-3.6.4.tar.gz

---
+++ packages/garmindb/garmindb-3.6.4.tar.gz/garmindb-3.6.4/garmindb/version_info.py
@@ -0,0 +1,18 @@
+"""Version information for the application."""
+
+__author__ = "Tom Goetz"
+__copyright__ = "Copyright Tom Goetz"
+__license__ = "GPL"
+
+
+python_required = (3, 0, 0)
+dev_python_required = (3, 9, 0)
+python_tested = (3, 11, 4)
+
+version_info = (3, 6, 4)
+prerelease = False
+
+
+def version_string():
+    """Return a version string for a version tuple."""
+    return '.'.join(str(digit) for digit in version_info)
