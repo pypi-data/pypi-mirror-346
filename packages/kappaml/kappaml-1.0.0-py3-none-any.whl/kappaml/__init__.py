21e8b07c5f0e3f7c8c982b884ebfa99eae0665c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 04:08:05 +0000

    Add package kappaml/1.0.0/kappaml-1.0.0-py3-none-any.whl

---
+++ packages/kappaml/kappaml-1.0.0-py3-none-any.whl/kappaml/__init__.py
@@ -0,0 +1,17 @@
+"""
+KappaML - A Python package for machine learning model evaluation and monitoring.
+"""
+
+from .kappaml import (
+    evaluate_model,
+    evaluate_model_with_metrics,
+    evaluate_model_with_plots,
+    evaluate_model_with_metrics_and_plots,
+    evaluate_model_with_metrics_and_plots_and_save,
+    evaluate_model_with_metrics_and_save,
+    evaluate_model_with_plots_and_save,
+    evaluate_model_and_save,
+)
+
+__version__ = "0.1.0"
+__author__ = "Alex Imbrea" 
